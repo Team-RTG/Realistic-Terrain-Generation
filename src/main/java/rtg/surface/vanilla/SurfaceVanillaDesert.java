6c28a2e29231fe653b2af35c5f7371dbe1fbaff1
Ezoteric <gemeenz@gmail.com>
2015-09-16 22:48:30 +0200

    Update SurfaceVanillaDesert.java
    changed desert terrain to more realistic (needs tweaking)

--- src/main/java/rtg/surface/vanilla/SurfaceVanillaDesert.java
+++ src/main/java/rtg/surface/vanilla/SurfaceVanillaDesert.java
@@ -14,28 +14,32 @@ import net.minecraft.world.biome.BiomeGenBase;
 
 public class SurfaceVanillaDesert extends SurfaceBase
 {
-	private Block cliffBlock1;
-	private Block cliffBlock2;
-	private Block bottomBlock;
-	
-	public SurfaceVanillaDesert(Block top, Block filler, Block bottom, Block cliff1, Block cliff2)
+	public SurfaceVanillaDesert(Block top, Block fill) 
 	{
-		super(top, filler);
-		
-		bottomBlock = bottom; 
-		cliffBlock1 = cliff1;
-		cliffBlock2 = cliff2;
+		super(top, fill);
 	}
 	
 	@Override
 	public void paintTerrain(Block[] blocks, byte[] metadata, int i, int j, int x, int y, int depth, World world, Random rand, PerlinNoise perlin, CellNoise cell, float[] noise, float river, BiomeGenBase[] base)
 	{
-		float c = CliffCalculator.calc(x, y, noise);
-		boolean cliff = c > 2.8f ? true : false;
-
+		boolean water = false;
+		boolean riverPaint = false;
+		boolean grass = false;
+		
+		if(river > 0.05f && river + (perlin.noise2(i / 10f, j / 10f) * 0.1f) > 0.86f)
+		{
+			riverPaint = true;
+			
+			if(perlin.noise2(i / 12f, j / 12f) > 0.25f)
+			{
+				grass = true;
+			}
+		}
+		
+		Block b;
 		for(int k = 255; k > -1; k--)
 		{
-			Block b = blocks[(y * 16 + x) * 256 + k];
+			b = blocks[(y * 16 + x) * 256 + k];
             if(b == Blocks.air)
             {
             	depth = -1;
@@ -44,33 +48,25 @@ public class SurfaceVanillaDesert extends SurfaceBase
             {
             	depth++;
 
-            	if(cliff)
+            	if(riverPaint)
             	{
-            		if(depth > -1 && depth < 2)
+            		if(grass && depth < 4)
             		{
-            			blocks[(y * 16 + x) * 256 + k] = rand.nextInt(3) == 0 ? cliffBlock2 : cliffBlock1; 
+    	        		blocks[(y * 16 + x) * 256 + k] = Blocks.grass;
             		}
-            		else if (depth < 10)
+            		else if(depth == 0)
             		{
-            			blocks[(y * 16 + x) * 256 + k] = cliffBlock1;
+    	        		blocks[(y * 16 + x) * 256 + k] = rand.nextInt(2) == 0 ? Blocks.sand : Blocks.sandstone;
             		}
             	}
-            	else if(depth < 6)
-            	{
-	        		if(depth == 0 && k > 61)
-	        		{
-	        			blocks[(y * 16 + x) * 256 + k] = topBlock;
-	        		}
-	        		else if(depth < 4)
-	        		{
-	        			blocks[(y * 16 + x) * 256 + k] = fillerBlock;
-	        		}
-	        		else
-	        		{
-	        			blocks[(y * 16 + x) * 256 + k] = bottomBlock;
-	        		}
-            	}
+        		else if(depth > -1 && depth < 9)
+        		{
+        			blocks[(y * 16 + x) * 256 + k] = Blocks.sand;
+            		if(depth == 0 && k > 61 && k < 254);
+        		}
             }
+
+            
 		}
 	}
 }

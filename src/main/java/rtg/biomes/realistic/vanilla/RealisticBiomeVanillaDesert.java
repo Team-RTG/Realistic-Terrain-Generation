1ca77f23f59ca36bb25b750c581ffa2a153d6f64
Ezoteric <gemeenz@gmail.com>
2015-09-16 22:46:12 +0200

    Update RealisticBiomeVanillaDesert.java
    Changed desert terrain to more realistic (still needs some tweaking)

--- src/main/java/rtg/biomes/realistic/vanilla/RealisticBiomeVanillaDesert.java
+++ src/main/java/rtg/biomes/realistic/vanilla/RealisticBiomeVanillaDesert.java
@@ -1,12 +1,28 @@
 package rtg.biomes.realistic.vanilla;
 
+import java.util.Random;
+
 import rtg.biomes.vanilla.VanillaBiomes;
 import rtg.biomes.realistic.RealisticBiomeBase;
 import rtg.coast.vanilla.CoastVanillaDesert;
+import rtg.deco.DecoBlob;
+import rtg.deco.DecoCacti;
+import rtg.deco.DecoFlowers;
+import rtg.deco.DecoGrass;
+import rtg.deco.trees.DecoSavannah;
 import rtg.surface.vanilla.SurfaceVanillaDesert;
 import rtg.terrain.vanilla.TerrainVanillaDesert;
+import rtg.util.CellNoise;
+import rtg.util.PerlinNoise;
 import net.minecraft.init.Blocks;
+import net.minecraft.world.World;
 import net.minecraft.world.biome.BiomeGenBase;
+import net.minecraft.world.gen.feature.WorldGenDeadBush;
+import net.minecraft.world.gen.feature.WorldGenDesertWells;
+import net.minecraft.world.gen.feature.WorldGenPumpkin;
+import net.minecraft.world.gen.feature.WorldGenReed;
+import net.minecraft.world.gen.feature.WorldGenShrub;
+import net.minecraft.world.gen.feature.WorldGenerator;
 
 public class RealisticBiomeVanillaDesert extends RealisticBiomeVanilla
 {	
@@ -16,8 +32,59 @@ public class RealisticBiomeVanillaDesert extends RealisticBiomeVanilla
 			BiomeGenBase.desert,
 			VanillaBiomes.vanillaRiverOasis,
 			new CoastVanillaDesert(),
-			new TerrainVanillaDesert(150f, 50f, 0f),
-			new SurfaceVanillaDesert(BiomeGenBase.desert.topBlock, BiomeGenBase.desert.fillerBlock, Blocks.sandstone, Blocks.stone, Blocks.cobblestone)
+			new TerrainVanillaDesert(),
+			new SurfaceVanillaDesert(BiomeGenBase.desert.topBlock, BiomeGenBase.desert.fillerBlock)
 		);
 	}	
+	@Override
+    public void rDecorate(World world, Random rand, int chunkX, int chunkY, PerlinNoise perlin, CellNoise cell, float strength, float river)
+    {
+
+		if(rand.nextInt((int)(4f / strength)) == 0)
+	       {
+	           int k = chunkX + rand.nextInt(360) + 8;
+	           int l = chunkY + rand.nextInt(360) + 8;
+	           WorldGenDesertWells worldgendesertwells = new WorldGenDesertWells();
+	           worldgendesertwells.generate(world, rand, k, world.getHeightValue(k, l) + 1, l);
+	        }	
+		if(river > 0.7f)
+		{
+			if(river > 0.86f)
+			{
+				for(int b33 = 0; b33 < 10f * strength; b33++)
+				{
+					int j6 = chunkX + rand.nextInt(16) + 8;
+					int k10 = chunkY + rand.nextInt(16) + 8;
+					int z52 = world.getHeightValue(j6, k10);
+	
+					if(z52 < 100f || (z52 < 120f && rand.nextInt(10) == 0))
+					{
+						WorldGenerator worldgenerator = rand.nextInt(4) != 0 ? new WorldGenShrub(0, 0) : new DecoSavannah(1);
+						worldgenerator.setScale(1.0D, 1.0D, 1.0D);
+						worldgenerator.generate(world, rand, j6, z52, k10);
+					}
+				}
+			}
+			
+			for(int k18 = 0; k18 < 12f * strength; k18++)
+			{
+				int k21 = chunkX + rand.nextInt(16) + 8;
+				int j23 = rand.nextInt(160);
+				int k24 = chunkY + rand.nextInt(16) + 8;
+				if(j23 < 120f)
+				{
+					(new DecoCacti(false)).generate(world, rand, k21, j23, k24);
+				}
+			}
+		}
+		
+		
+		for(int i15 = 0; i15 < 1f * strength; i15++)
+		{
+			int i17 = chunkX + rand.nextInt(16) + 8;
+			int i20 = 64 + rand.nextInt(64);
+			int l22 = chunkY + rand.nextInt(16) + 8;
+			(new WorldGenDeadBush(Blocks.deadbush)).generate(world, rand, i17, i20, l22);
+		}
+    }
 }

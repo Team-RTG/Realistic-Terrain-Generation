445028962bd06bbb635b6bce433e88408c254f02
Jason <gemeenz@gmail.com>
2015-09-19 16:07:38 +0200

    Update RealisticBiomeVanillaBeach.java
    Beach pimped with palms

--- src/main/java/rtg/biomes/realistic/vanilla/RealisticBiomeVanillaBeach.java
+++ src/main/java/rtg/biomes/realistic/vanilla/RealisticBiomeVanillaBeach.java
@@ -1,12 +1,19 @@
 package rtg.biomes.realistic.vanilla;
 
+import java.util.Random;
+
 import rtg.biomes.vanilla.VanillaBiomes;
 import rtg.biomes.vanilla.VanillaBiomes.Climate;
 import rtg.biomes.realistic.RealisticBiomeBase;
+import rtg.deco.trees.DecoPalm;
 import rtg.surface.vanilla.SurfaceVanillaBeach;
 import rtg.terrain.vanilla.TerrainVanillaBeach;
+import rtg.util.CellNoise;
+import rtg.util.PerlinNoise;
 import net.minecraft.init.Blocks;
+import net.minecraft.world.World;
 import net.minecraft.world.biome.BiomeGenBase;
+import net.minecraft.world.gen.feature.WorldGenerator;
 
 public class RealisticBiomeVanillaBeach extends RealisticBiomeVanilla
 {	
@@ -18,5 +25,24 @@ public class RealisticBiomeVanillaBeach extends RealisticBiomeVanilla
 			new TerrainVanillaBeach(),
 			new SurfaceVanillaBeach(BiomeGenBase.beach.topBlock, BiomeGenBase.beach.fillerBlock, BiomeGenBase.beach.topBlock, BiomeGenBase.beach.fillerBlock, (byte)0, 1)
 		);
-	}	
+	}
+	
+	    public void rDecorate(World world, Random rand, int chunkX, int chunkY, PerlinNoise perlin, CellNoise cell, float strength, float river)
+	    {
+	    	
+	    	if(rand.nextInt((int)(2f / strength)) == 0)
+			{
+				int j6 = chunkX + rand.nextInt(16) + 8;
+				int k10 = chunkY + rand.nextInt(16) + 8;
+				int z52 = world.getHeightValue(j6, k10);
+
+				if(z52 < 80)
+				{
+					WorldGenerator worldgenerator = new DecoPalm();
+					worldgenerator.setScale(1.0D, 1.0D, 1.0D);
+					worldgenerator.generate(world, rand, j6, z52, k10);
+				}
+			}
+	}
+	
 }

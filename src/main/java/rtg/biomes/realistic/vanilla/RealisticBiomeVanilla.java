92a32178b3c10538a637a46e4c9a5a2083422720
WhichOnesPink <whichonespink@hokecraft.co.uk>
2015-09-22 02:03:30 +0100

    Removed unused code for realistic vanilla biomes.

--- src/main/java/rtg/biomes/realistic/vanilla/RealisticBiomeVanilla.java
+++ src/main/java/rtg/biomes/realistic/vanilla/RealisticBiomeVanilla.java
@@ -8,18 +8,8 @@ import rtg.terrain.TerrainBase;
 
 public class RealisticBiomeVanilla extends RealisticBiomeSupport
 {	
-	public BiomeGenBase vanillaBiomeBase;
-	public BiomeGenBase vanillaRiver;
-	public TerrainBase vanillaTerrain;
-	public SurfaceBase vanillaSurface;
-
 	public RealisticBiomeVanilla(BiomeGenBase b, BiomeGenBase riverbiome, TerrainBase t, SurfaceBase s)
 	{
 		super(b, riverbiome, t, s);
-		
-		vanillaBiomeBase = b;
-		vanillaRiver = riverbiome;
-		vanillaTerrain = t;
-		vanillaSurface = s;
 	}	
 }

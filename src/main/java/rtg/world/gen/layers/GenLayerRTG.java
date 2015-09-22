10a36cb6a13caabd8a0aa85c1b842bbaa5318431
Jason <gemeenz@gmail.com>
2015-09-22 16:37:48 +0200

    Create GenLayerRTG.java

---
+++ src/main/java/rtg/world/gen/layers/GenLayerRTG.java
@@ -0,0 +1,106 @@
+package rtg.world.gen.layers;
+
+import java.util.concurrent.Callable;
+
+import net.minecraft.world.WorldType;
+import net.minecraft.world.gen.layer.GenLayer;
+import net.minecraft.world.gen.layer.GenLayerAddIsland;
+import net.minecraft.world.gen.layer.GenLayerAddMushroomIsland;
+import net.minecraft.world.gen.layer.GenLayerAddSnow;
+import net.minecraft.world.gen.layer.GenLayerDeepOcean;
+import net.minecraft.world.gen.layer.GenLayerEdge;
+import net.minecraft.world.gen.layer.GenLayerFuzzyZoom;
+import net.minecraft.world.gen.layer.GenLayerHills;
+import net.minecraft.world.gen.layer.GenLayerIsland;
+import net.minecraft.world.gen.layer.GenLayerRareBiome;
+import net.minecraft.world.gen.layer.GenLayerRemoveTooMuchOcean;
+import net.minecraft.world.gen.layer.GenLayerRiver;
+import net.minecraft.world.gen.layer.GenLayerRiverInit;
+import net.minecraft.world.gen.layer.GenLayerRiverMix;
+import net.minecraft.world.gen.layer.GenLayerShore;
+import net.minecraft.world.gen.layer.GenLayerSmooth;
+import net.minecraft.world.gen.layer.GenLayerVoronoiZoom;
+import net.minecraft.world.gen.layer.GenLayerZoom;
+import rtg.config.*;
+import rtg.world.gen.layers.*;
+
+public abstract class GenLayerRTG extends GenLayer
+{
+	public GenLayerRTG(long par1) 
+	{
+		super(par1);
+	}
+
+    public static GenLayer[] initializeAllBiomeGenerators(long seed, WorldType par2WorldType)
+    {
+        boolean flag = false;
+        GenLayer genLayerCreateLand = ConfigRTG.oceanFiller ? new GenLayerIsland(1L) : new GenLayerCreateLand(1L);
+        GenLayerFuzzyZoom genlayerfuzzyzoom = new GenLayerFuzzyZoom(2000L, genLayerCreateLand);
+        GenLayerAddIsland genlayeraddisland = new GenLayerAddIsland(1L, genlayerfuzzyzoom);
+        GenLayerZoom genlayerzoom = new GenLayerZoom(2001L, genlayeraddisland);
+        genlayeraddisland = new GenLayerAddIsland(2L, genlayerzoom);
+        genlayeraddisland = new GenLayerAddIsland(50L, genlayeraddisland);
+        genlayeraddisland = new GenLayerAddIsland(70L, genlayeraddisland);
+        GenLayerRemoveTooMuchOcean genlayerremovetoomuchocean = new GenLayerRemoveTooMuchOcean(2L, genlayeraddisland);
+        GenLayerAddSnow genlayeraddsnow = new GenLayerAddSnow(2L, ConfigRTG.oceanFiller ? genlayerremovetoomuchocean : genlayeraddisland);
+        genlayeraddisland = new GenLayerAddIsland(3L, genlayeraddsnow);
+        GenLayerEdge genlayeredge = new GenLayerEdge(2L, genlayeraddisland, GenLayerEdge.Mode.COOL_WARM);
+        genlayeredge = new GenLayerEdge(2L, genlayeredge, GenLayerEdge.Mode.HEAT_ICE);
+        genlayeredge = new GenLayerEdge(3L, genlayeredge, GenLayerEdge.Mode.SPECIAL);
+        genlayerzoom = new GenLayerZoom(2002L, genlayeredge);
+        genlayerzoom = new GenLayerZoom(2003L, genlayerzoom);
+        genlayeraddisland = new GenLayerAddIsland(4L, genlayerzoom);
+        RTGGenLayerAddMushroomIsland RTGGenLayerAddMushroomIsland = new RTGGenLayerAddMushroomIsland(5L, genlayeraddisland);
+        GenLayerDeepOcean genlayerdeepocean = new GenLayerDeepOcean(4L, RTGGenLayerAddMushroomIsland);
+        GenLayer genlayer2 = GenLayerZoom.magnify(1000L, genlayerdeepocean, 0);
+        byte b0 = 4;
+
+        if (flag)
+        {
+            b0 = 4;
+        }
+        b0 = getModdedBiomeSize(par2WorldType, b0);
+
+        GenLayer genlayer = GenLayerZoom.magnify(1000L, genlayer2, 0);
+        GenLayerRiverInit genlayerriverinit = new GenLayerRiverInit(100L, genlayer);
+        Object object = par2WorldType.getBiomeLayer(seed, genlayer2);
+
+        GenLayer genlayer1 = GenLayerZoom.magnify(1000L, genlayerriverinit, 2);
+        GenLayerHills genlayerhills = new GenLayerHills(1000L, (GenLayer)object, genlayer1);
+        GenLayerMutationRTG genlayermutation = new GenLayerMutationRTG(1000L, (GenLayer)object, genlayer1);
+        //  Can be usefull for addon and def for makin the volcano a rare spawn
+        //  GenLayerSpecialBiome genlayerspecialbiome = new GenLayerSpecialBiome(1500L, genlayermutation);
+        //
+        
+        genlayer = GenLayerZoom.magnify(1000L, genlayerriverinit, 2);
+        genlayer = GenLayerZoom.magnify(1000L, genlayer, b0);
+        GenLayerRiver genlayerriver = new GenLayerRiver(1L, genlayer);
+        GenLayerSmooth genlayersmooth = new GenLayerSmooth(1000L, genlayerriver);
+        object = new GenLayerRareBiome(1001L, genlayerhills);
+     //   object = new GenLayerRareBiome(1001L, genlayerspecialbiome);
+        
+        for (int j = 0; j < b0; ++j)
+        {
+            object = new GenLayerZoom((long)(1000 + j), (GenLayer)object);
+
+            if (j == 0)
+            {
+                object = new GenLayerAddIsland(3L, (GenLayer)object);
+            }
+
+            if (j == 1)
+            {
+              //  object = new GenLayerShoreBOP(1000L, (GenLayer)object);
+                object = new GenLayerShore(1000L, (GenLayer)object);
+            }
+        }
+
+        GenLayerSmooth genlayersmooth1 = new GenLayerSmooth(1000L, (GenLayer)object);
+       // GenLayerRiverMixBOP genlayerrivermix = new GenLayerRiverMixBOP(100L, genlayersmooth1, genlayersmooth);
+        GenLayerRiverMix genlayerrivermix = new GenLayerRiverMix(100L, genlayersmooth1, genlayersmooth);
+        GenLayerVoronoiZoom genlayervoronoizoom = new GenLayerVoronoiZoom(10L, genlayerrivermix);
+        genlayerrivermix.initWorldGenSeed(seed);
+        genlayervoronoizoom.initWorldGenSeed(seed);
+        return new GenLayer[] {genlayerrivermix, genlayervoronoizoom, genlayerrivermix};
+    }
+}

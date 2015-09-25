1a26a49838a74bbd7513ed59818192c6d8dea3ad
Jason <gemeenz@gmail.com>
2015-09-25 23:32:13 +0200

    e
    e

---
+++ src/main/java/rtg/config/BiomeGenRTG.java
@@ -0,0 +1,53 @@
+package rtg.config;
+
+import java.io.File;
+
+import net.minecraftforge.common.config.Configuration;
+
+import org.apache.logging.log4j.Level;
+
+import cpw.mods.fml.common.FMLLog;
+
+public class BiomeGenRTG
+{
+	public static Configuration config;
+	
+	//Special
+	public static boolean oceanGen = false;
+	public static boolean frozenOceanGen = false;
+	public static boolean deepOceanGen = false;
+	public static boolean mushroomIslandGen = false;
+	public static boolean mesaPlateauFGen = false;
+	public static boolean mesaPlateauGen = false;
+	public static boolean jungleGen = false;
+	public static boolean megaTaigaGen = false;
+
+	public static void init(File configFile)
+	{
+		config = new Configuration(configFile);
+
+		try
+		{
+			config.load();
+
+			oceanGen = config.get("Special Biomes To Generate", "Ocean", true).getBoolean(true);
+			frozenOceanGen = config.get("Special Biomes To Generate", "Frozen Ocean", true).getBoolean(true);
+			deepOceanGen = config.get("Special Biomes To Generate", "Deep Ocean", true).getBoolean(true);
+			mushroomIslandGen = config.get("Special Biomes To Generate", "Mushroom Island", true).getBoolean(true);
+			mesaPlateauFGen = config.get("Special Biomes To Generate", "Mesa Plateau F", true).getBoolean(true);
+			mesaPlateauGen = config.get("Special Biomes To Generate", "Mesa Plateau", true).getBoolean(true);
+			jungleGen = config.get("Special Biomes To Generate", "Jungle", true).getBoolean(true);
+			megaTaigaGen = config.get("Special Biomes To Generate", "Mega Taiga", true).getBoolean(true);
+		}
+		catch (Exception e)
+		{
+			FMLLog.log(Level.ERROR, e, "Biomes O Plenty has had a problem loading its configuration");
+		}
+		finally
+		{
+			if (config.hasChanged()) {
+				config.save();
+			}
+		}
+	}
+}
---
+++ src/main/java/rtg/world/gen/layers/GenLayerSpecialBiome.java
@@ -0,0 +1,79 @@
+package rtg.world.gen.layers;
+
+import java.util.List;
+
+import net.minecraft.world.biome.BiomeGenBase;
+import net.minecraft.world.gen.layer.GenLayer;
+import net.minecraft.world.gen.layer.IntCache;
+import net.minecraftforge.common.BiomeManager.BiomeEntry;
+
+import org.apache.commons.lang3.tuple.Pair;
+
+//import biomesoplenty.common.biome.BOPSubBiome;
+//import biomesoplenty.common.world.BOPBiomeManager;
+//import biomesoplenty.common.world.noise.SimplexNoise;
+/*
+public class GenLayerSpecialBiome extends GenLayer
+{
+	private final int OFFSET_RANGE = 500000;
+	
+	private Pair<Integer, Integer>[] offsets = new Pair[BiomeGenBase.getBiomeGenArray().length];
+	
+	public GenLayerSpecialBiome(long seed, GenLayer parent) 
+	{
+		super(seed);
+		
+		this.parent = parent;
+		
+		this.setOffsets();
+	}
+	
+    @Override
+	public int[] getInts(int x, int z, int width, int length)
+    {
+        int[] inputBiomeIDs = this.parent.getInts(x - 1, z - 1, width + 2, length + 2);
+        int[] outputBiomeIDs = IntCache.getIntCache(width * length);
+
+        for (int xi = 0; xi < width; ++xi) 
+        {
+        	for (int zi = 0; zi < length; ++zi) 
+        	{
+        		this.initChunkSeed(xi + x, zi + z);
+        		int currentBiomeId = inputBiomeIDs[xi + 1 + (zi + 1) * (width + 2)];
+        		
+        		List<BiomeEntry> currentSubBiomes = BOPBiomeManager.overworldSubBiomes[currentBiomeId];
+        		BOPSubBiome selectedSubBiome = currentSubBiomes != null ? (BOPSubBiome)currentSubBiomes.get(this.nextInt(currentSubBiomes.size())).biome : null;
+        		
+        		if (selectedSubBiome != null)
+        		{
+        			Pair<Integer, Integer> offset = getOffset(selectedSubBiome);
+        			
+        			if (SimplexNoise.noise((xi + x + offset.getLeft()) * selectedSubBiome.zoom, (zi + z + offset.getRight()) * selectedSubBiome.zoom) > selectedSubBiome.threshold)  
+        			{
+        				outputBiomeIDs[xi + zi * width] = selectedSubBiome.biomeID;
+        			}
+        			else outputBiomeIDs[xi + zi * width] = currentBiomeId;
+        		}
+        		else
+        		{
+        			outputBiomeIDs[xi + zi * width] = currentBiomeId;
+        		}
+        	}
+        }
+
+        return outputBiomeIDs;
+    }
+    
+    private Pair<Integer, Integer> getOffset(BiomeGenBase biome)
+    {
+    	return offsets[biome.biomeID];
+    }
+
+    private void setOffsets()
+    {
+    	for (int i = 0; i< offsets.length; i++)
+    	{
+    		offsets[i] = Pair.of(this.nextInt(OFFSET_RANGE) - (OFFSET_RANGE / 2), this.nextInt(OFFSET_RANGE) - (OFFSET_RANGE / 2));
+    	}
+    }
+} */

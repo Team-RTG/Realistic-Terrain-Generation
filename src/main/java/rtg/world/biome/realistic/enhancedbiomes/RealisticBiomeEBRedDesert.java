19e8d5c1dc605b8622cd1e1470846d60ec0388a2
WhichOnesPink <whichonespink@hokecraft.co.uk>
2015-10-11 22:06:38 +0100

    Improved EB Red Desert.

--- src/main/java/rtg/world/biome/realistic/enhancedbiomes/RealisticBiomeEBRedDesert.java
+++ src/main/java/rtg/world/biome/realistic/enhancedbiomes/RealisticBiomeEBRedDesert.java
@@ -1,25 +1,25 @@
 package rtg.world.biome.realistic.enhancedbiomes;
 
-import net.minecraft.init.Blocks;
-import net.minecraft.world.biome.BiomeGenBase;
 import rtg.config.ConfigEB;
 import rtg.world.biome.BiomeBase;
-import rtg.world.biome.BiomeBase.BiomeCategory;
 import rtg.world.gen.surface.enhancedbiomes.SurfaceEBRedDesert;
 import rtg.world.gen.terrain.enhancedbiomes.TerrainEBRedDesert;
 
+import net.minecraft.world.biome.BiomeGenBase;
+
 public class RealisticBiomeEBRedDesert extends RealisticBiomeEBBase
-{	
-	public RealisticBiomeEBRedDesert(BiomeGenBase ebBiome)
-	{
-		super(
-			ebBiome, BiomeBase.climatizedBiome(BiomeGenBase.river, Climate.OASIS),
-			new TerrainEBRedDesert(),
-			new SurfaceEBRedDesert(ebBiome.topBlock, ebBiome.fillerBlock, Blocks.sand, Blocks.sand, Blocks.sand)
-		);
-		
-		this.setRealisticBiomeName("EB Red Desert");
-		this.biomeCategory = BiomeCategory.HOT;
-		this.biomeWeight = ConfigEB.weightEBRedDesert;
-	}
+{
+    
+    public RealisticBiomeEBRedDesert(BiomeGenBase ebBiome)
+    {
+    
+        super(
+            ebBiome, BiomeBase.climatizedBiome(BiomeGenBase.river, Climate.OASIS),
+            new TerrainEBRedDesert(),
+            new SurfaceEBRedDesert());
+        
+        this.setRealisticBiomeName("EB Red Desert");
+        this.biomeCategory = BiomeCategory.HOT;
+        this.biomeWeight = ConfigEB.weightEBRedDesert;
+    }
 }
--- src/main/java/rtg/world/gen/surface/enhancedbiomes/SurfaceEBRedDesert.java
+++ src/main/java/rtg/world/gen/surface/enhancedbiomes/SurfaceEBRedDesert.java
@@ -6,6 +6,7 @@ import rtg.util.CellNoise;
 import rtg.util.CliffCalculator;
 import rtg.util.OpenSimplexNoise;
 import rtg.world.gen.surface.SurfaceBase;
+
 import net.minecraft.block.Block;
 import net.minecraft.init.Blocks;
 import net.minecraft.world.World;
@@ -13,63 +14,65 @@ import net.minecraft.world.biome.BiomeGenBase;
 
 public class SurfaceEBRedDesert extends SurfaceBase
 {
-	private Block cliffBlock1;
-	private Block cliffBlock2;
-	private Block bottomBlock;
-	
-	public SurfaceEBRedDesert(Block top, Block filler, Block bottom, Block cliff1, Block cliff2)
-	{
-		super(top, filler);
-		
-		bottomBlock = bottom; 
-		cliffBlock1 = cliff1;
-		cliffBlock2 = cliff2;
-	}
-	
-	@Override
-	public void paintTerrain(Block[] blocks, byte[] metadata, int i, int j, int x, int y, int depth, World world, Random rand, OpenSimplexNoise simplex, CellNoise cell, float[] noise, float river, BiomeGenBase[] base)
-	{
-		float c = CliffCalculator.calc(x, y, noise);
-		boolean cliff = c > 2.8f ? true : false;
-
-		for(int k = 255; k > -1; k--)
-		{
-			Block b = blocks[(y * 16 + x) * 256 + k];
-            if(b == Blocks.air)
+    
+    private Block cliffBlock1;
+    private Block cliffBlock2;
+    private Block bottomBlock;
+    
+    public SurfaceEBRedDesert()
+    {
+    
+        super(Blocks.sand, Blocks.sand);
+        
+        bottomBlock = Blocks.sandstone;
+        cliffBlock1 = Blocks.stained_hardened_clay;
+    }
+    
+    @Override
+    public void paintTerrain(Block[] blocks, byte[] metadata, int i, int j, int x, int y, int depth, World world, Random rand,
+        OpenSimplexNoise simplex, CellNoise cell, float[] noise, float river, BiomeGenBase[] base)
+    {
+    
+        float c = CliffCalculator.calc(x, y, noise);
+        boolean cliff = c > 1.4f ? true : false;
+        
+        for (int k = 255; k > -1; k--)
+        {
+            Block b = blocks[(y * 16 + x) * 256 + k];
+            if (b == Blocks.air)
             {
-            	depth = -1;
+                depth = -1;
             }
-            else if(b == Blocks.stone)
+            else if (b == Blocks.stone)
             {
-            	depth++;
-
-            	if(cliff)
-            	{
-            		if(depth > -1 && depth < 2)
-            		{
-            			blocks[(y * 16 + x) * 256 + k] = rand.nextInt(3) == 0 ? cliffBlock2 : cliffBlock1; 
-            		}
-            		else if (depth < 10)
-            		{
-            			blocks[(y * 16 + x) * 256 + k] = cliffBlock1;
-            		}
-            	}
-            	else if(depth < 6)
-            	{
-	        		if(depth == 0 && k > 61)
-	        		{
-	        			blocks[(y * 16 + x) * 256 + k] = topBlock;
-	        		}
-	        		else if(depth < 4)
-	        		{
-	        			blocks[(y * 16 + x) * 256 + k] = fillerBlock;
-	        		}
-	        		else
-	        		{
-	        			blocks[(y * 16 + x) * 256 + k] = bottomBlock;
-	        		}
-            	}
+                depth++;
+                
+                if (cliff)
+                {
+                    if (depth < 6)
+                    {
+                        blocks[(y * 16 + x) * 256 + k] = cliffBlock1;
+                        metadata[(y * 16 + x) * 256 + k] = (byte) 14;
+                    }
+                }
+                else if (depth < 6)
+                {
+                    if (depth == 0 && k > 61)
+                    {
+                        blocks[(y * 16 + x) * 256 + k] = topBlock;
+                        metadata[(y * 16 + x) * 256 + k] = (byte) 1;
+                    }
+                    else if (depth < 4)
+                    {
+                        blocks[(y * 16 + x) * 256 + k] = fillerBlock;
+                        metadata[(y * 16 + x) * 256 + k] = (byte) 1;
+                    }
+                    else
+                    {
+                        blocks[(y * 16 + x) * 256 + k] = bottomBlock;
+                    }
+                }
             }
-		}
-	}
+        }
+    }
 }

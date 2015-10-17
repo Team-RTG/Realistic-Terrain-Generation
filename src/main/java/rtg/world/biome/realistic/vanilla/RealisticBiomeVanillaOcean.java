bfcebf23cab12c3f6077ba7b386bcb526d706f0f
WhichOnesPink <whichonespink@hokecraft.co.uk>
2015-10-17 16:23:20 +0100

    Improved ocean biomes.

--- src/main/java/rtg/world/biome/realistic/vanilla/RealisticBiomeVanillaOcean.java
+++ src/main/java/rtg/world/biome/realistic/vanilla/RealisticBiomeVanillaOcean.java
@@ -1,29 +1,31 @@
 package rtg.world.biome.realistic.vanilla;
 
-import net.minecraft.block.Block;
-import net.minecraft.init.Blocks;
-import net.minecraft.world.biome.BiomeGenBase;
 import rtg.config.ConfigRTG;
 import rtg.world.biome.BiomeBase;
 import rtg.world.gen.surface.vanilla.SurfaceVanillaOcean;
 import rtg.world.gen.terrain.vanilla.TerrainVanillaOcean;
 
+import net.minecraft.block.Block;
+import net.minecraft.init.Blocks;
+import net.minecraft.world.biome.BiomeGenBase;
+
 public class RealisticBiomeVanillaOcean extends RealisticBiomeVanillaBase
-{	
-	public static Block topBlock = BiomeGenBase.ocean.topBlock;
-	public static Block fillerBlock = BiomeGenBase.ocean.fillerBlock;
-	
-	public RealisticBiomeVanillaOcean()
-	{
-		super(
-			BiomeGenBase.ocean,
-			BiomeBase.climatizedBiome(BiomeGenBase.river, Climate.TEMPERATE),
-			new TerrainVanillaOcean(),
-			new SurfaceVanillaOcean(Blocks.sand, Blocks.sand, (byte)0, 0)
-		);
-		
-		this.setRealisticBiomeName("Vanilla Ocean");
-		this.biomeCategory = BiomeCategory.WET;
-		this.biomeWeight = ConfigRTG.weightVanillaOcean;
-	}
+{
+    
+    public static Block topBlock = BiomeGenBase.ocean.topBlock;
+    public static Block fillerBlock = BiomeGenBase.ocean.fillerBlock;
+    
+    public RealisticBiomeVanillaOcean()
+    {
+    
+        super(
+            BiomeGenBase.ocean,
+            BiomeBase.climatizedBiome(BiomeGenBase.ocean, Climate.WET),
+            new TerrainVanillaOcean(),
+            new SurfaceVanillaOcean(Blocks.sand, Blocks.sand, Blocks.gravel, 20f, 0.2f));
+        
+        this.setRealisticBiomeName("Vanilla Ocean");
+        this.biomeCategory = BiomeCategory.WET;
+        this.biomeWeight = ConfigRTG.weightVanillaOcean;
+    }
 }
--- src/main/java/rtg/world/gen/terrain/vanilla/TerrainVanillaDeepOcean.java
+++ src/main/java/rtg/world/gen/terrain/vanilla/TerrainVanillaDeepOcean.java
@@ -6,13 +6,19 @@ import rtg.world.gen.terrain.TerrainBase;
 
 public class TerrainVanillaDeepOcean extends TerrainBase
 {
-	public TerrainVanillaDeepOcean()
-	{
-		
-	}
+    
+    public TerrainVanillaDeepOcean()
+    {
+        
+    }
 
-	public float generateNoise(OpenSimplexNoise simplex, CellNoise cell, int x, int y, float ocean, float border, float river)
-	{
-		return 45f;
-	}
+    public float generateNoise(OpenSimplexNoise simplex, CellNoise cell, int x, int y, float ocean, float border, float river)
+    {
+        float h = simplex.noise2(x / 300f, y / 300f) * 40f * river;
+        h = h > 3f ? 3f : h; 
+        h += simplex.noise2(x / 50f, y / 50f) * (12f - h) * 0.4f;
+        h += simplex.noise2(x / 15f, y / 15f) * (12f - h) * 0.15f;
+        
+        return 20f + h;
+    }
 }

14c5cc36a540f2e9f0478ee5bc0c920cd1e0f73e
WhichOnesPink <whichonespink@hokecraft.co.uk>
2015-10-19 15:41:01 +0100

    Added support for Ars Magica's Witchwood Forest biome.

---
+++ src/main/java/rtg/world/biome/realistic/arsmagica/RealisticBiomeAMWitchwoodForest.java
@@ -0,0 +1,26 @@
+package rtg.world.biome.realistic.arsmagica;
+
+import rtg.config.ConfigAM;
+import rtg.world.biome.BiomeBase;
+import rtg.world.gen.surface.arsmagica.SurfaceAMWitchwoodForest;
+import rtg.world.gen.terrain.arsmagica.TerrainAMWitchwoodForest;
+
+import net.minecraft.init.Blocks;
+import net.minecraft.world.biome.BiomeGenBase;
+
+public class RealisticBiomeAMWitchwoodForest extends RealisticBiomeAMBase
+{
+    
+    public RealisticBiomeAMWitchwoodForest(BiomeGenBase amBiome)
+    {
+    
+        super(
+            amBiome, BiomeBase.climatizedBiome(BiomeGenBase.river, Climate.TEMPERATE),
+            new TerrainAMWitchwoodForest(),
+            new SurfaceAMWitchwoodForest(amBiome.topBlock, amBiome.fillerBlock, Blocks.stone, Blocks.cobblestone));
+        
+        this.setRealisticBiomeName("Ars Magica Witchwood Forest");
+        this.biomeCategory = BiomeCategory.SMALL;
+        this.biomeWeight = ConfigAM.weightAMWitchwoodForest;
+    }
+}

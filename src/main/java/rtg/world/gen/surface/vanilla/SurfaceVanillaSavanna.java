af3cf4f00c0b5780e5536cc8cfcb2459d09c1daa
WhichOnesPink <whichonespink@hokecraft.co.uk>
2015-10-22 00:42:52 +0100

    Improved vanilla Savanna & Savanna Plateau biomes.

--- src/main/java/rtg/world/gen/surface/vanilla/SurfaceVanillaSavanna.java
+++ src/main/java/rtg/world/gen/surface/vanilla/SurfaceVanillaSavanna.java
@@ -14,57 +14,73 @@ import net.minecraft.world.biome.BiomeGenBase;
 
 public class SurfaceVanillaSavanna extends SurfaceBase
 {
-    private float valley;
-    private boolean dirt;
-    private boolean mix;
     
-    public SurfaceVanillaSavanna(Block top, Block fill, float valleySize, boolean d, boolean m) 
+    private Block mixBlock;
+    private Block cliffBlock1;
+    private Block cliffBlock2;
+    private float width;
+    private float height;
+    
+    public SurfaceVanillaSavanna(Block top, Block filler, Block mix, Block cliff1, Block cliff2, float mixWidth, float mixHeight)
     {
-        super(top, fill);
+    
+        super(top, filler);
         
-        valley = valleySize;
-        dirt = d;
-        mix = m;
+        mixBlock = mix;
+        cliffBlock1 = cliff1;
+        cliffBlock2 = cliff2;
+        
+        width = mixWidth;
+        height = mixHeight;
     }
     
     @Override
-    public void paintTerrain(Block[] blocks, byte[] metadata, int i, int j, int x, int y, int depth, World world, Random rand, OpenSimplexNoise simplex, CellNoise cell, float[] noise, float river, BiomeGenBase[] base)
+    public void paintTerrain(Block[] blocks, byte[] metadata, int i, int j, int x, int y, int depth, World world, Random rand,
+        OpenSimplexNoise simplex, CellNoise cell, float[] noise, float river, BiomeGenBase[] base)
     {
-        float h = (simplex.noise2(i / valley, j / valley) + 0.25f) * 65f;
-        h = h < 1f ? 1f : h;
-        float m = simplex.noise2(i / 12f, j / 12f);
+    
+        float c = CliffCalculator.calc(x, y, noise);
+        boolean cliff = c > 1.4f ? true : false;
         
-        Block b;
-        for(int k = 255; k > -1; k--)
+        for (int k = 255; k > -1; k--)
         {
-            b = blocks[(y * 16 + x) * 256 + k];
-            if(b == Blocks.air)
+            Block b = blocks[(y * 16 + x) * 256 + k];
+            if (b == Blocks.air)
             {
                 depth = -1;
             }
-            else if(b == Blocks.stone)
+            else if (b == Blocks.stone)
             {
                 depth++;
                 
-                if(depth == 0)
+                if (cliff)
                 {
-                    if(k > 90f + simplex.noise2(i / 24f, j / 24f) * 10f - h || (m < -0.28f && mix))
+                    if (depth > -1 && depth < 2)
                     {
-                        blocks[(y * 16 + x) * 256 + k] = topBlock;
+                        blocks[(y * 16 + x) * 256 + k] = rand.nextInt(3) == 0 ? cliffBlock2 : cliffBlock1;
                     }
-                    else if(dirt && m < 0.22f || k < 62)
+                    else if (depth < 10)
                     {
-                        blocks[(y * 16 + x) * 256 + k] = fillerBlock;
-                        metadata[(y * 16 + x) * 256 + k] = 1;
-                    }
-                    else
-                    {
-                        blocks[(y * 16 + x) * 256 + k] = topBlock;
+                        blocks[(y * 16 + x) * 256 + k] = cliffBlock1;
                     }
                 }
-                else if(depth < 6)
+                else
                 {
-                    blocks[(y * 16 + x) * 256 + k] = fillerBlock;
+                    if (depth == 0 && k > 61)
+                    {
+                        if (simplex.noise2(i / width, j / width) > height) // > 0.27f, i / 12f
+                        {
+                            blocks[(y * 16 + x) * 256 + k] = mixBlock;
+                        }
+                        else
+                        {
+                            blocks[(y * 16 + x) * 256 + k] = topBlock;
+                        }
+                    }
+                    else if (depth < 4)
+                    {
+                        blocks[(y * 16 + x) * 256 + k] = fillerBlock;
+                    }
                 }
             }
         }

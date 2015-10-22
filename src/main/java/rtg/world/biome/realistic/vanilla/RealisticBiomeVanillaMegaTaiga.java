348bf79ae53f237e33bddbf19cbbbe0361f98a87
WhichOnesPink <whichonespink@hokecraft.co.uk>
2015-10-22 23:21:20 +0100

    Improved Mega Taiga & Mega Taiga Hills biomes.

--- src/main/java/rtg/world/biome/realistic/vanilla/RealisticBiomeVanillaMegaTaiga.java
+++ src/main/java/rtg/world/biome/realistic/vanilla/RealisticBiomeVanillaMegaTaiga.java
@@ -1,30 +1,155 @@
 package rtg.world.biome.realistic.vanilla;
 
+import java.util.Random;
+
 import rtg.config.vanilla.ConfigVanilla;
+import rtg.util.CellNoise;
+import rtg.util.OpenSimplexNoise;
 import rtg.world.biome.BiomeBase;
+import rtg.world.gen.feature.WorldGenBlob;
+import rtg.world.gen.feature.WorldGenGrass;
+import rtg.world.gen.feature.WorldGenLog;
+import rtg.world.gen.feature.WorldGenWildWheat;
+import rtg.world.gen.feature.tree.WorldGenTreePineSmall;
+import rtg.world.gen.feature.tree.WorldGenTreeShrub;
+import rtg.world.gen.feature.tree.WorldGenTreeSprucePineBig;
+import rtg.world.gen.feature.tree.WorldGenTreeSpruceSmall;
 import rtg.world.gen.surface.vanilla.SurfaceVanillaMegaTaiga;
 import rtg.world.gen.terrain.vanilla.TerrainVanillaMegaTaiga;
 
 import net.minecraft.block.Block;
 import net.minecraft.init.Blocks;
+import net.minecraft.world.World;
 import net.minecraft.world.biome.BiomeGenBase;
+import net.minecraft.world.gen.feature.WorldGenFlowers;
+import net.minecraft.world.gen.feature.WorldGenLakes;
+import net.minecraft.world.gen.feature.WorldGenPumpkin;
+import net.minecraft.world.gen.feature.WorldGenerator;
 
 public class RealisticBiomeVanillaMegaTaiga extends RealisticBiomeVanillaBase
-{	
-	public static Block topBlock = BiomeGenBase.megaTaiga.topBlock;
-	public static Block fillerBlock = BiomeGenBase.megaTaiga.fillerBlock;
-	
-	public RealisticBiomeVanillaMegaTaiga()
-	{
-		super(
-			BiomeGenBase.megaTaiga,
-			BiomeBase.climatizedBiome(BiomeGenBase.river, Climate.COLD),
-			new TerrainVanillaMegaTaiga(230f, 120f, 90f),
-			new SurfaceVanillaMegaTaiga(topBlock, fillerBlock, true, Blocks.sand, 0.2f)
-		);
-		
-		this.setRealisticBiomeName("Vanilla Mega Taiga");
-		this.biomeCategory = BiomeCategory.COLD;
-		this.biomeWeight = ConfigVanilla.weightVanillaMegaTaiga;
-	}	
+{
+    
+    public static Block topBlock = BiomeGenBase.megaTaiga.topBlock;
+    public static Block fillerBlock = BiomeGenBase.megaTaiga.fillerBlock;
+    
+    public RealisticBiomeVanillaMegaTaiga()
+    {
+    
+        super(
+            BiomeGenBase.megaTaiga,
+            BiomeBase.climatizedBiome(BiomeGenBase.river, Climate.COLD),
+            new TerrainVanillaMegaTaiga(),
+            new SurfaceVanillaMegaTaiga(topBlock, fillerBlock));
+        
+        this.setRealisticBiomeName("Vanilla Mega Taiga");
+        this.biomeCategory = BiomeCategory.COLD;
+        this.biomeWeight = ConfigVanilla.weightVanillaMegaTaiga;
+    }
+    
+    @Override
+    public void rDecorate(World world, Random rand, int chunkX, int chunkY, OpenSimplexNoise simplex, CellNoise cell, float strength,
+        float river)
+    {
+    
+        if (rand.nextInt((int) (15f / strength)) == 0)
+        {
+            int i2 = chunkX + rand.nextInt(16) + 8;
+            int i8 = chunkY + rand.nextInt(16) + 8;
+            int l4 = world.getHeightValue(i2, i8);
+            if (l4 > 63)
+            {
+                (new WorldGenLakes(Blocks.water)).generate(world, rand, i2, l4, i8);
+            }
+        }
+        
+        // boulders
+        for (int l = 0; l < 3f * strength; ++l)
+        {
+            int i1 = chunkX + rand.nextInt(16) + 8;
+            int j1 = chunkY + rand.nextInt(16) + 8;
+            int k1 = world.getHeightValue(i1, j1);
+            if (k1 < 95 && (k1 < 64 || rand.nextInt(7) == 0))
+            {
+                (new WorldGenBlob(Blocks.mossy_cobblestone, 0)).generate(world, rand, i1, k1, j1);
+            }
+        }
+        
+        // trees
+        float l = simplex.noise2(chunkX / 100f, chunkY / 100f) * 6f + 0.8f;
+        for (int b1 = 0; b1 < l * 4f * strength; b1++)
+        {
+            int j6 = chunkX + rand.nextInt(16) + 8;
+            int k10 = chunkY + rand.nextInt(16) + 8;
+            int z52 = world.getHeightValue(j6, k10);
+            
+            WorldGenerator worldgenerator =
+                rand.nextInt(4) == 0 ? new WorldGenTreeSpruceSmall(1 + rand.nextInt(2)) : rand.nextInt(6) == 0 ? new WorldGenTreePineSmall(
+                    1 + rand.nextInt(3), 4 + rand.nextInt(4)) : new WorldGenTreeSprucePineBig(4 + rand.nextInt(6), 12 + rand.nextInt(10));
+            worldgenerator.setScale(1.0D, 1.0D, 1.0D);
+            worldgenerator.generate(world, rand, j6, z52, k10);
+        }
+        
+        if (l > 0f && rand.nextInt(6) == 0)
+        {
+            int x22 = chunkX + rand.nextInt(16) + 8;
+            int z22 = chunkY + rand.nextInt(16) + 8;
+            int y22 = world.getHeightValue(x22, z22);
+            (new WorldGenLog(1, 3 + rand.nextInt(4), false)).generate(world, rand, x22, y22, z22);
+        }
+        
+        for (int b = 0; b < 2f * strength; b++)
+        {
+            int i1 = chunkX + rand.nextInt(16) + 8;
+            int j1 = chunkY + rand.nextInt(16) + 8;
+            int k1 = world.getHeightValue(i1, j1);
+            if (rand.nextInt(10) == 0)
+            {
+                (new WorldGenTreeShrub(rand.nextInt(5) + 4, rand.nextInt(2), rand.nextInt(2))).generate(world, rand, i1, k1, j1);
+            }
+            else
+            {
+                (new WorldGenTreeShrub(rand.nextInt(4) + 1, rand.nextInt(2), rand.nextInt(2))).generate(world, rand, i1, k1, j1);
+            }
+        }
+        
+        if (rand.nextInt((int) (3f / strength)) == 0)
+        {
+            int k15 = chunkX + rand.nextInt(16) + 8;
+            int k17 = rand.nextInt(64) + 64;
+            int k20 = chunkY + rand.nextInt(16) + 8;
+            
+            if (rand.nextBoolean())
+            {
+                (new WorldGenFlowers(Blocks.brown_mushroom)).generate(world, rand, k15, k17, k20);
+            }
+            else
+            {
+                (new WorldGenFlowers(Blocks.red_mushroom)).generate(world, rand, k15, k17, k20);
+            }
+        }
+        
+        if (rand.nextInt((int) (20f / strength)) == 0)
+        {
+            int j16 = chunkX + rand.nextInt(16) + 8;
+            int j18 = rand.nextInt(128);
+            int j21 = chunkY + rand.nextInt(16) + 8;
+            (new WorldGenPumpkin()).generate(world, rand, j16, j18, j21);
+        }
+        
+        if (rand.nextInt((int) (150f / strength)) == 0)
+        {
+            int k21 = chunkX + rand.nextInt(16) + 8;
+            int j23 = rand.nextInt(60) + 60;
+            int k24 = chunkY + rand.nextInt(16) + 8;
+            (new WorldGenWildWheat(rand.nextInt(3))).generate(world, rand, k21, j23, k24);
+        }
+        
+        for (int l14 = 0; l14 < 10f * strength; l14++)
+        {
+            int l19 = chunkX + rand.nextInt(16) + 8;
+            int k22 = rand.nextInt(128);
+            int j24 = chunkY + rand.nextInt(16) + 8;
+            (new WorldGenGrass(Blocks.tallgrass, 1)).generate(world, rand, l19, k22, j24);
+        }
+    }
 }
--- src/main/java/rtg/world/gen/surface/vanilla/SurfaceVanillaMegaTaiga.java
+++ src/main/java/rtg/world/gen/surface/vanilla/SurfaceVanillaMegaTaiga.java
@@ -6,6 +6,7 @@ import rtg.util.CellNoise;
 import rtg.util.CliffCalculator;
 import rtg.util.OpenSimplexNoise;
 import rtg.world.gen.surface.SurfaceBase;
+
 import net.minecraft.block.Block;
 import net.minecraft.init.Blocks;
 import net.minecraft.world.World;
@@ -13,127 +14,108 @@ import net.minecraft.world.biome.BiomeGenBase;
 
 public class SurfaceVanillaMegaTaiga extends SurfaceBase
 {
-	private boolean beach;
-	private Block beachBlock;
-	private float min;
-	
-	private float sCliff = 1.5f;
-	private float sHeight = 60f;
-	private float sStrength = 65f;
-	private float cCliff = 1.5f;
-	
-	public byte topByte = 0;
-	
-	public SurfaceVanillaMegaTaiga(Block top, Block fill, boolean genBeach, Block genBeachBlock, float minCliff) 
-	{
-		super(top, fill);
-		beach = genBeach;
-		beachBlock = genBeachBlock;
-		min = minCliff;
-	}
-	
-	public SurfaceVanillaMegaTaiga(Block top, Block fill, boolean genBeach, Block genBeachBlock, float minCliff, float stoneCliff, float stoneHeight, float stoneStrength, float clayCliff)
-	{
-		this(top, fill, genBeach, genBeachBlock, minCliff);
-		
-		sCliff = stoneCliff;
-		sHeight = stoneHeight;
-		sStrength = stoneStrength;
-		cCliff = clayCliff;
-	}
-	
-	@Override
-	public void paintTerrain(Block[] blocks, byte[] metadata, int i, int j, int x, int y, int depth, World world, Random rand, OpenSimplexNoise simplex, CellNoise cell, float[] noise, float river, BiomeGenBase[] base)
-	{
-		float c = CliffCalculator.calc(x, y, noise);
-		int cliff = 0;
-		boolean gravel = false;
-		
-    	Block b;
-		for(int k = 255; k > -1; k--)
-		{
-			b = blocks[(y * 16 + x) * 256 + k];
-            if(b == Blocks.air)
+    
+    public SurfaceVanillaMegaTaiga(Block top, Block fill)
+    {
+    
+        super(top, fill);
+    }
+    
+    @Override
+    public void paintTerrain(Block[] blocks, byte[] metadata, int i, int j, int x, int y, int depth, World world, Random rand,
+        OpenSimplexNoise simplex, CellNoise cell, float[] noise, float river, BiomeGenBase[] base)
+    {
+    
+        float p = simplex.noise2(i / 8f, j / 8f) * 0.5f;
+        float c = CliffCalculator.calc(x, y, noise);
+        int cliff = 0;
+        boolean gravel = false;
+        
+        Block b;
+        for (int k = 255; k > -1; k--)
+        {
+            b = blocks[(y * 16 + x) * 256 + k];
+            if (b == Blocks.air)
             {
-            	depth = -1;
+                depth = -1;
             }
-            else if(b == Blocks.stone)
+            else if (b == Blocks.stone)
             {
-            	depth++;
-            	
-            	if(depth == 0)
-            	{
-            		if(k < 63)
-            		{
-            			if(beach)
-            			{
-            				gravel = true;
-            			}
-            		}
-
-					float p = simplex.noise3(i / 8f, j / 8f, k / 8f) * 0.5f;
-        			if(c > min && c > sCliff - ((k - sHeight) / sStrength) + p)
-        			{
-        				cliff = 1;
-        			}
-            		if(c > cCliff)
-        			{
-        				cliff = 2;
-        			}
-            		
-            		if(cliff == 1)
-            		{
-            			blocks[(y * 16 + x) * 256 + k] = rand.nextInt(3) == 0 ? Blocks.cobblestone : Blocks.stone; 
-            		}
-            		else if(cliff == 2)
-            		{
-        				blocks[(y * 16 + x) * 256 + k] = Blocks.stained_hardened_clay; 
-        				metadata[(y * 16 + x) * 256 + k] = 9; 
-            		}
-            		else if(k < 63)
-            		{
-            			if(beach)
-            			{
-	            			blocks[(y * 16 + x) * 256 + k] = beachBlock;
-	            			gravel = true;
-            			}
-            			else if(k < 62)
-            			{
-                			blocks[(y * 16 + x) * 256 + k] = fillerBlock;
-            			}
-            			else
-            			{
-                			blocks[(y * 16 + x) * 256 + k] = topBlock;
-                			metadata[(y * 16 + x) * 256 + k] = topByte;
-            			}
-            		}
-            		else
-            		{
-            			blocks[(y * 16 + x) * 256 + k] = topBlock;
-            			metadata[(y * 16 + x) * 256 + k] = topByte;
-            		}
-            	}
-            	else if(depth < 6)
-        		{
-            		if(cliff == 1)
-            		{
-            			blocks[(y * 16 + x) * 256 + k] = Blocks.stone; 
-            		}
-            		else if(cliff == 2)
-            		{
-        				blocks[(y * 16 + x) * 256 + k] = Blocks.stained_hardened_clay; 
-        				metadata[(y * 16 + x) * 256 + k] = 9; 
-            		}
-            		else if(gravel)
-            		{
-            			blocks[(y * 16 + x) * 256 + k] = beachBlock;
-            		}
-            		else
-            		{
-            			blocks[(y * 16 + x) * 256 + k] = fillerBlock;
-            		}
-        		}
+                depth++;
+                
+                if (depth == 0)
+                {
+                    if (k < 63)
+                    {
+                        gravel = true;
+                    }
+                    
+                    if (c > 0.45f && c > 1.5f - ((k - 60f) / 65f) + p)
+                    {
+                        cliff = 1;
+                    }
+                    if (c > 1.5f)
+                    {
+                        cliff = 2;
+                    }
+                    if (k > 110 + (p * 4) && c < 0.3f + ((k - 100f) / 50f) + p)
+                    {
+                        cliff = 3;
+                    }
+                    
+                    if (cliff == 1)
+                    {
+                        blocks[(y * 16 + x) * 256 + k] = rand.nextInt(3) == 0 ? Blocks.cobblestone : Blocks.stone;
+                    }
+                    else if (cliff == 2)
+                    {
+                        blocks[(y * 16 + x) * 256 + k] = Blocks.stained_hardened_clay;
+                        metadata[(y * 16 + x) * 256 + k] = 9;
+                    }
+                    else if (cliff == 3)
+                    {
+                        blocks[(y * 16 + x) * 256 + k] = Blocks.snow;
+                    }
+                    else if (simplex.noise2(i / 50f, j / 50f) + p * 0.6f > 0.24f)
+                    {
+                        blocks[(y * 16 + x) * 256 + k] = Blocks.dirt;
+                        metadata[(y * 16 + x) * 256 + k] = 2;
+                    }
+                    else if (k < 63)
+                    {
+                        blocks[(y * 16 + x) * 256 + k] = Blocks.gravel;
+                        gravel = true;
+                    }
+                    else
+                    {
+                        blocks[(y * 16 + x) * 256 + k] = Blocks.grass;
+                    }
+                }
+                else if (depth < 6)
+                {
+                    if (cliff == 1)
+                    {
+                        blocks[(y * 16 + x) * 256 + k] = Blocks.stone;
+                    }
+                    else if (cliff == 2)
+                    {
+                        blocks[(y * 16 + x) * 256 + k] = Blocks.stained_hardened_clay;
+                        metadata[(y * 16 + x) * 256 + k] = 9;
+                    }
+                    else if (cliff == 3)
+                    {
+                        blocks[(y * 16 + x) * 256 + k] = Blocks.snow;
+                    }
+                    else if (gravel)
+                    {
+                        blocks[(y * 16 + x) * 256 + k] = Blocks.gravel;
+                    }
+                    else
+                    {
+                        blocks[(y * 16 + x) * 256 + k] = Blocks.dirt;
+                    }
+                }
             }
-		}
-	}
+        }
+    }
 }

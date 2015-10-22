56d4f458cf91c0f6b8824ea475e087bf96f526b4
WhichOnesPink <whichonespink@hokecraft.co.uk>
2015-10-22 18:00:47 +0100

    Improved vanilla Swampland biome.

--- src/main/java/rtg/world/biome/realistic/vanilla/RealisticBiomeVanillaSwampland.java
+++ src/main/java/rtg/world/biome/realistic/vanilla/RealisticBiomeVanillaSwampland.java
@@ -1,30 +1,110 @@
 package rtg.world.biome.realistic.vanilla;
 
+import java.util.Random;
+
 import rtg.config.vanilla.ConfigVanilla;
+import rtg.util.CellNoise;
+import rtg.util.OpenSimplexNoise;
 import rtg.world.biome.BiomeBase;
+import rtg.world.biome.realistic.RealisticBiomeBase;
+import rtg.world.gen.feature.WorldGenGrass;
+import rtg.world.gen.feature.WorldGenLog;
+import rtg.world.gen.feature.tree.WorldGenTreeDesertDead;
+import rtg.world.gen.feature.tree.WorldGenTreePineBig;
+import rtg.world.gen.feature.tree.WorldGenTreeShrub;
+import rtg.world.gen.feature.tree.WorldGenTreeWillow;
 import rtg.world.gen.surface.vanilla.SurfaceVanillaSwampland;
 import rtg.world.gen.terrain.vanilla.TerrainVanillaSwampland;
 
 import net.minecraft.block.Block;
 import net.minecraft.init.Blocks;
+import net.minecraft.world.World;
 import net.minecraft.world.biome.BiomeGenBase;
+import net.minecraft.world.gen.feature.WorldGenPumpkin;
+import net.minecraft.world.gen.feature.WorldGenSwamp;
+import net.minecraft.world.gen.feature.WorldGenerator;
 
 public class RealisticBiomeVanillaSwampland extends RealisticBiomeVanillaBase
-{	
-	public static Block topBlock = BiomeGenBase.swampland.topBlock;
-	public static Block fillerBlock = BiomeGenBase.swampland.fillerBlock;
-	
-	public RealisticBiomeVanillaSwampland()
-	{
-		super(
-			BiomeGenBase.swampland,
-			BiomeBase.climatizedBiome(BiomeGenBase.river, Climate.WET),
-			new TerrainVanillaSwampland(),
-			new SurfaceVanillaSwampland(topBlock, fillerBlock, Blocks.stone, Blocks.cobblestone)
-		);
-		
-		this.setRealisticBiomeName("Vanilla Swampland");
-		this.biomeCategory = BiomeCategory.WET;
-		this.biomeWeight = ConfigVanilla.weightVanillaSwampland;
-	}	
+{
+    
+    public static Block topBlock = BiomeGenBase.swampland.topBlock;
+    public static Block fillerBlock = BiomeGenBase.swampland.fillerBlock;
+    
+    public RealisticBiomeVanillaSwampland()
+    {
+    
+        super(
+            BiomeGenBase.swampland,
+            BiomeBase.climatizedBiome(BiomeGenBase.river, Climate.WET),
+            new TerrainVanillaSwampland(),
+            new SurfaceVanillaSwampland(topBlock, fillerBlock, Blocks.stone, Blocks.cobblestone));
+        
+        this.setRealisticBiomeName("Vanilla Swampland");
+        this.biomeCategory = BiomeCategory.WET;
+        this.biomeWeight = ConfigVanilla.weightVanillaSwampland;
+    }
+    
+    @Override
+    public void rDecorate(World world, Random rand, int chunkX, int chunkY, OpenSimplexNoise simplex, CellNoise cell, float strength,
+        float river)
+    {
+    
+        float l = simplex.noise2(chunkX / 80f, chunkY / 80f) * 60f - 15f;
+        for (int b1 = 0; b1 < l * strength; b1++)
+        {
+            if (rand.nextInt(2) == 0)
+            {
+                int j6 = chunkX + rand.nextInt(16) + 8;
+                int k10 = chunkY + rand.nextInt(16) + 8;
+                int z52 = world.getHeightValue(j6, k10);
+                
+                if (z52 < 110)
+                {
+                    WorldGenerator worldgenerator = new WorldGenTreeWillow();
+                    worldgenerator.setScale(1.0D, 1.0D, 1.0D);
+                    worldgenerator.generate(world, rand, j6, z52, k10);
+                }
+            }
+        }
+        
+        if (rand.nextInt((int) (4f / strength)) == 0)
+        {
+            int x22 = chunkX + rand.nextInt(16) + 8;
+            int z22 = chunkY + rand.nextInt(16) + 8;
+            int y22 = world.getHeightValue(x22, z22);
+            if (y22 < 100)
+            {
+                (new WorldGenLog(Blocks.log2, 1, Blocks.leaves2, -1, 3 + rand.nextInt(4))).generate(world, rand, x22, y22, z22);
+            }
+        }
+        
+        for (int f24 = 0; f24 < 3f * strength; f24++)
+        {
+            int i1 = chunkX + rand.nextInt(16) + 8;
+            int j1 = chunkY + rand.nextInt(16) + 8;
+            int k1 = world.getHeightValue(i1, j1);
+            if (k1 < 110)
+            {
+                (new WorldGenTreeShrub(rand.nextInt(4) + 1, 0, rand.nextInt(3))).generate(world, rand, i1, k1, j1);
+            }
+        }
+        
+        if (rand.nextInt((int) (50f / strength)) == 0)
+        {
+            int j16 = chunkX + rand.nextInt(16) + 8;
+            int j18 = rand.nextInt(100);
+            int j21 = chunkY + rand.nextInt(16) + 8;
+            (new WorldGenPumpkin()).generate(world, rand, j16, j18, j21);
+        }
+        
+        for (int l14 = 0; l14 < 12f * strength; l14++)
+        {
+            int l19 = chunkX + rand.nextInt(16) + 8;
+            int k22 = rand.nextInt(128);
+            int j24 = chunkY + rand.nextInt(16) + 8;
+            (new WorldGenGrass(Blocks.tallgrass, 1)).generate(world, rand, l19, k22, j24);
+        }
+        
+        RealisticBiomeBase.rDecorateSeedBiome(world, rand, chunkX, chunkY, simplex, cell, strength, river, baseBiome);
+    }
 }
--- src/main/java/rtg/world/gen/terrain/vanilla/TerrainVanillaSwampland.java
+++ src/main/java/rtg/world/gen/terrain/vanilla/TerrainVanillaSwampland.java
@@ -6,25 +6,28 @@ import rtg.world.gen.terrain.TerrainBase;
 
 public class TerrainVanillaSwampland extends TerrainBase
 {
-	public TerrainVanillaSwampland()
-	{
-	}
-	
-	@Override
-	public float generateNoise(OpenSimplexNoise simplex, CellNoise cell, int x, int y, float ocean, float border, float river)
-	{
-		float h = simplex.noise2(x / 130f, y / 130f) * 30f;
-		
-		h += simplex.noise2(x / 12f, y / 12f) * 2f;
-		h += simplex.noise2(x / 18f, y / 18f) * 4f;
-		
-		h = h < 4f ? 0f : h - 4f;
-		
-		if(h == 0f)
-		{
-			h += simplex.noise2(x / 20f, y / 20f) + simplex.noise2(x / 5f, y / 5f);
-		}
-		
-		return 62f + h;
-	}
+    
+    public TerrainVanillaSwampland()
+    {
+    
+    }
+    
+    @Override
+    public float generateNoise(OpenSimplexNoise simplex, CellNoise cell, int x, int y, float ocean, float border, float river)
+    {
+    
+        float h = simplex.noise2(x / 130f, y / 130f) * 30f;
+        
+        h += simplex.noise2(x / 12f, y / 12f) * 2f;
+        h += simplex.noise2(x / 18f, y / 18f) * 4f;
+        
+        h = h < 4f ? 0f : h - 4f;
+        
+        if (h == 0f)
+        {
+            h += simplex.noise2(x / 20f, y / 20f) + simplex.noise2(x / 5f, y / 5f);
+        }
+        
+        return 62f + h;
+    }
 }

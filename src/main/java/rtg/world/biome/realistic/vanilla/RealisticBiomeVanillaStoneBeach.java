65bedf9139a957fb33a7542bdfb045493ab40731
WhichOnesPink <whichonespink@hokecraft.co.uk>
2015-10-23 09:14:57 +0100

    Improved vanilla Stone Beach biome.

--- src/main/java/rtg/world/biome/realistic/vanilla/RealisticBiomeVanillaStoneBeach.java
+++ src/main/java/rtg/world/biome/realistic/vanilla/RealisticBiomeVanillaStoneBeach.java
@@ -6,6 +6,7 @@ import rtg.world.gen.surface.vanilla.SurfaceVanillaStoneBeach;
 import rtg.world.gen.terrain.vanilla.TerrainVanillaStoneBeach;
 
 import net.minecraft.block.Block;
+import net.minecraft.init.Blocks;
 import net.minecraft.world.biome.BiomeGenBase;
 
 public class RealisticBiomeVanillaStoneBeach extends RealisticBiomeVanillaBase
@@ -17,9 +18,9 @@ public class RealisticBiomeVanillaStoneBeach extends RealisticBiomeVanillaBase
 	{
 		super(
 			BiomeGenBase.stoneBeach,
-			BiomeBase.climatizedBiome(BiomeGenBase.river, Climate.OASIS),
+			BiomeBase.climatizedBiome(BiomeGenBase.river, Climate.WET),
 			new TerrainVanillaStoneBeach(0f, 100f, 63f, 80f),
-			new SurfaceVanillaStoneBeach(topBlock, fillerBlock, false, null, 1f, 1.5f, 85f, 20f, 4f)
+			new SurfaceVanillaStoneBeach(topBlock, fillerBlock, true, Blocks.gravel, 1f, 1.5f, 85f, 20f, 4f)
 		);
 		
 		this.setRealisticBiomeName("Vanilla Stone Beach");

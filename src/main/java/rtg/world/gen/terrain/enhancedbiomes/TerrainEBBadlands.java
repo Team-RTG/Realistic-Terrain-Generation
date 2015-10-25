0aeaa5687112c87d4f6958f67cf8957d4fb60d41
WhichOnesPink <whichonespink@hokecraft.co.uk>
2015-10-25 07:55:13 +0000

    Improved EB Badlands biome.

--- src/main/java/rtg/world/gen/terrain/enhancedbiomes/TerrainEBBadlands.java
+++ src/main/java/rtg/world/gen/terrain/enhancedbiomes/TerrainEBBadlands.java
@@ -6,22 +6,30 @@ import rtg.world.gen.terrain.TerrainBase;
 
 public class TerrainEBBadlands extends TerrainBase
 {
-	public TerrainEBBadlands()
-	{
-	}
-	
-	@Override
-	public float generateNoise(OpenSimplexNoise simplex, CellNoise cell, int x, int y, float ocean, float border, float river)
-	{
-		float st = (simplex.noise2(x / 160f, y / 160f) + 0.38f) * 35f * river;
-		st = st < 0.2f ? 0.2f : st;
-		
-		float h = simplex.noise2(x / 60f, y / 60f) * st * 2f;
-		h = h > 0f ? -h : h;
-		h += st;
-		h *= h / 50f;
-		h += st;
-		
-    	return 70f + h;
-	}
+    
+    public TerrainEBBadlands()
+    {
+    
+    }
+    
+    @Override
+    public float generateNoise(OpenSimplexNoise simplex, CellNoise cell, int x, int y, float ocean, float border, float river)
+    {
+    
+        float floNoise;
+        float st = (simplex.noise2(x / 160f, y / 160f) + 0.38f) * 10f * river;
+        st = st < 0.2f ? 0.2f : st;
+        
+        float h = simplex.noise2(x / 60f, y / 60f) * st * 2f;
+        h = h > 0f ? -h : h;
+        h += st;
+        h *= h / 80f;
+        h += st;
+        
+        floNoise = 62f + h;
+        
+        // FMLLog.log(Level.INFO, "floNoise = %f", floNoise);
+        
+        return floNoise;
+    }
 }

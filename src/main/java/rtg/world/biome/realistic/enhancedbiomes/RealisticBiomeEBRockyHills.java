ed8390885012005a9fb9e396abc135c1e7e7f7bb
WhichOnesPink <whichonespink@hokecraft.co.uk>
2015-10-26 11:52:56 +0000

    Improved EB Rocky Hills biome.

--- src/main/java/rtg/world/biome/realistic/enhancedbiomes/RealisticBiomeEBRockyHills.java
+++ src/main/java/rtg/world/biome/realistic/enhancedbiomes/RealisticBiomeEBRockyHills.java
@@ -1,25 +1,143 @@
 package rtg.world.biome.realistic.enhancedbiomes;
 
-import net.minecraft.world.biome.BiomeGenBase;
+import java.util.Random;
 
 import rtg.config.enhancedbiomes.ConfigEB;
+import rtg.util.CellNoise;
+import rtg.util.OpenSimplexNoise;
 import rtg.world.biome.BiomeBase;
-import rtg.world.biome.BiomeBase.BiomeCategory;
+import rtg.world.gen.feature.WorldGenFlowers;
+import rtg.world.gen.feature.WorldGenGrass;
+import rtg.world.gen.feature.tree.WorldGenTreePineEuro;
+import rtg.world.gen.surface.SurfaceBase;
+import rtg.world.gen.surface.SurfaceRiverOasis;
 import rtg.world.gen.surface.enhancedbiomes.SurfaceEBRockyHills;
 import rtg.world.gen.terrain.enhancedbiomes.TerrainEBRockyHills;
+import enhancedbiomes.EnhancedBiomesMod;
+import enhancedbiomes.blocks.EnhancedBiomesBlocks;
+import enhancedbiomes.helpers.TreeGen;
+
+import net.minecraft.block.Block;
+import net.minecraft.init.Blocks;
+import net.minecraft.world.World;
+import net.minecraft.world.biome.BiomeGenBase;
+import net.minecraft.world.gen.feature.WorldGenBlockBlob;
+import net.minecraft.world.gen.feature.WorldGenReed;
+import net.minecraft.world.gen.feature.WorldGenShrub;
+import net.minecraft.world.gen.feature.WorldGenerator;
 
 public class RealisticBiomeEBRockyHills extends RealisticBiomeEBBase
-{	
-	public RealisticBiomeEBRockyHills(BiomeGenBase ebBiome)
-	{
-		super(
-			ebBiome, BiomeBase.climatizedBiome(BiomeGenBase.river, Climate.TEMPERATE),
-			new TerrainEBRockyHills(80f),
-			new SurfaceEBRockyHills(ebBiome.topBlock, ebBiome.fillerBlock, 40f, false, false)
-		);
-		
-		this.setRealisticBiomeName("EB Rocky Hills");
-		this.biomeCategory = BiomeCategory.HOT;
-		this.biomeWeight = ConfigEB.weightEBRockyHills;
-	}
+{
+    
+    private static SurfaceBase surface = new SurfaceEBRockyHills(
+        (EnhancedBiomesMod.useNewStone == 1) ? EnhancedBiomesBlocks.stoneEB : Blocks.stone,
+        (EnhancedBiomesMod.useNewStone == 1) ? (byte) 3 : (byte) 0,
+        (EnhancedBiomesMod.useNewStone == 1) ? EnhancedBiomesBlocks.stoneEB : Blocks.stone,
+        (EnhancedBiomesMod.useNewStone == 1) ? (byte) 10 : (byte) 0,
+        false,
+        null,
+        0f,
+        1.5f,
+        60f,
+        65f,
+        1.5f,
+        (EnhancedBiomesMod.useNewStone == 1) ? EnhancedBiomesBlocks.stoneCobbleEB : Blocks.cobblestone,
+        (EnhancedBiomesMod.useNewStone == 1) ? (byte) 3 : (byte) 0,
+        (EnhancedBiomesMod.useNewStone == 1) ? EnhancedBiomesBlocks.stoneCobbleEB : Blocks.cobblestone,
+        (EnhancedBiomesMod.useNewStone == 1) ? (byte) 10 : (byte) 0,
+        0.08f
+        );
+    
+    private static SurfaceBase riverSurface = new SurfaceRiverOasis();
+    
+    public RealisticBiomeEBRockyHills(BiomeGenBase ebBiome)
+    {
+    
+        super(
+            ebBiome, BiomeBase.climatizedBiome(BiomeGenBase.river, Climate.TEMPERATE),
+            new TerrainEBRockyHills(230f, 120f, 0f),
+            surface);
+        
+        this.setRealisticBiomeName("EB Rocky Hills");
+        this.biomeCategory = BiomeCategory.HOT;
+        this.biomeWeight = ConfigEB.weightEBRockyHills;
+    }
+    
+    @Override
+    public void rDecorate(World world, Random rand, int chunkX, int chunkY, OpenSimplexNoise simplex, CellNoise cell, float strength,
+        float river)
+    {
+    
+        for (int i23 = 0; i23 < 1; i23++)
+        {
+            int i1 = chunkX + rand.nextInt(16) + 8;
+            int j1 = chunkY + rand.nextInt(16) + 8;
+            int k1 = world.getHeightValue(i1, j1);
+            
+            if (k1 < 80)
+            {
+                (new WorldGenBlockBlob(Blocks.cobblestone, 0)).generate(world, rand, i1, k1, j1);
+            }
+        }
+        
+        if (river > 0.7f)
+        {
+            if (river > 0.86f)
+            {
+                for (int b33 = 0; b33 < 10f * strength; b33++)
+                {
+                    int j6 = chunkX + rand.nextInt(16) + 8;
+                    int k10 = chunkY + rand.nextInt(16) + 8;
+                    int z52 = world.getHeightValue(j6, k10);
+                    
+                    if (z52 < 100f || (z52 < 120f && rand.nextInt(10) == 0))
+                    {
+                        WorldGenerator worldgenerator = rand.nextInt(4) != 0 ? new WorldGenShrub(0, 0) : TreeGen.eucalyptus_shrub(rand);
+                        worldgenerator.setScale(1.0D, 1.0D, 1.0D);
+                        worldgenerator.generate(world, rand, j6, z52, k10);
+                    }
+                }
+            }
+            
+            for (int f25 = 0; f25 < 2f * strength; f25++)
+            {
+                int i18 = chunkX + rand.nextInt(16) + 8;
+                int i23 = chunkY + rand.nextInt(16) + 8;
+                (new WorldGenReed()).generate(world, rand, i18, 60 + rand.nextInt(8), i23);
+            }
+            
+            for (int f23 = 0; f23 < 3; f23++)
+            {
+                int j15 = chunkX + rand.nextInt(16) + 8;
+                int j17 = rand.nextInt(128);
+                int j20 = chunkY + rand.nextInt(16) + 8;
+                (new WorldGenFlowers(new int[] {9, 9, 9, 9, 3, 3, 3, 3, 3, 2, 2, 2, 11, 11, 11})).generate(world, rand, j15, j17, j20);
+            }
+            
+            for (int l14 = 0; l14 < 15; l14++)
+            {
+                int l19 = chunkX + rand.nextInt(16) + 8;
+                int k22 = rand.nextInt(128);
+                int j24 = chunkY + rand.nextInt(16) + 8;
+                
+                if (rand.nextInt(3) == 0)
+                {
+                    (new WorldGenGrass(Blocks.double_plant, 2)).generate(world, rand, l19, k22, j24);
+                }
+                else
+                {
+                    (new WorldGenGrass(Blocks.tallgrass, 1)).generate(world, rand, l19, k22, j24);
+                }
+            }
+        }
+    }
+    
+    @Override
+    public void rReplace(Block[] blocks, byte[] metadata, int i, int j, int x, int y, int depth, World world, Random rand,
+        OpenSimplexNoise simplex, CellNoise cell, float[] noise, float river, BiomeGenBase[] base)
+    {
+    
+        surface.paintTerrain(blocks, metadata, i, j, x, y, depth, world, rand, simplex, cell, noise, river, base);
+        riverSurface.paintTerrain(blocks, metadata, i, j, x, y, depth, world, rand, simplex, cell, noise, river, base);
+    }
 }

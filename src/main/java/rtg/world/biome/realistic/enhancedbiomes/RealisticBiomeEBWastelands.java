e48515a1717111f9b290f086e51a62399668f912
WhichOnesPink <whichonespink@hokecraft.co.uk>
2015-10-27 17:54:26 +0000

    Improved EB Wastelands biome.

--- src/main/java/rtg/world/biome/realistic/enhancedbiomes/RealisticBiomeEBWastelands.java
+++ src/main/java/rtg/world/biome/realistic/enhancedbiomes/RealisticBiomeEBWastelands.java
@@ -1,25 +1,101 @@
 package rtg.world.biome.realistic.enhancedbiomes;
 
+import java.util.Random;
+
+import enhancedbiomes.EnhancedBiomesMod;
+import enhancedbiomes.blocks.EnhancedBiomesBlocks;
+import enhancedbiomes.world.gen.WorldGenSpikedBush;
+
+import net.minecraft.block.Block;
+import net.minecraft.init.Blocks;
+import net.minecraft.world.World;
 import net.minecraft.world.biome.BiomeGenBase;
+import net.minecraft.world.gen.feature.WorldGenDeadBush;
 
 import rtg.config.enhancedbiomes.ConfigEB;
+import rtg.util.CellNoise;
+import rtg.util.OpenSimplexNoise;
 import rtg.world.biome.BiomeBase;
 import rtg.world.biome.BiomeBase.BiomeCategory;
+import rtg.world.gen.feature.WorldGenBlob;
+import rtg.world.gen.feature.WorldGenFlowers;
+import rtg.world.gen.feature.WorldGenGrass;
+import rtg.world.gen.feature.tree.WorldGenTreeShrub;
+import rtg.world.gen.surface.enhancedbiomes.SurfaceEBSandstoneCanyon;
 import rtg.world.gen.surface.enhancedbiomes.SurfaceEBWastelands;
 import rtg.world.gen.terrain.enhancedbiomes.TerrainEBWastelands;
 
 public class RealisticBiomeEBWastelands extends RealisticBiomeEBBase
 {	
+    private static Block cobbleBlock = (EnhancedBiomesMod.useNewStone == 1) ? EnhancedBiomesBlocks.stoneCobbleEB : Blocks.cobblestone;
+    private static byte cobbleByte = (EnhancedBiomesMod.useNewStone == 1) ? (byte)11 : (byte)0;
+    
 	public RealisticBiomeEBWastelands(BiomeGenBase ebBiome)
 	{
 		super(
 			ebBiome, BiomeBase.climatizedBiome(BiomeGenBase.river, Climate.TEMPERATE),
 			new TerrainEBWastelands(),
-			new SurfaceEBWastelands(ebBiome.topBlock, ebBiome.fillerBlock)
+			new SurfaceEBSandstoneCanyon(
+                Blocks.gravel, //Block top 
+                (byte)0, //byte topByte
+                cobbleBlock, //Block filler, 
+                cobbleByte, //byte fillerByte
+                Blocks.dirt, //Block mixTop, 
+                (byte)0, //byte mixTopByte, 
+                cobbleBlock, //Block mixFill, 
+                cobbleByte, //byte mixFillByte, 
+                cobbleBlock, //Block cliff1, 
+                cobbleByte, //byte cliff1Byte, 
+                cobbleBlock, //Block cliff2, 
+                cobbleByte, //byte cliff2Byte, 
+                40f, //float mixWidth, 
+                -0.10f, //float mixHeight, 
+                10f, //float smallWidth, 
+                0.5f //float smallStrength
+            )
 		);
 		
 		this.setRealisticBiomeName("EB Wastelands");
 		this.biomeCategory = BiomeCategory.HOT;
 		this.biomeWeight = ConfigEB.weightEBWastelands;
 	}
+	
+    @Override
+    public void rDecorate(World world, Random rand, int chunkX, int chunkY, OpenSimplexNoise simplex, CellNoise cell, float strength,
+        float river)
+    {
+    
+        for (int l = 0; l < 3f * strength; ++l)
+        {
+            int i1 = chunkX + rand.nextInt(16) + 8;
+            int j1 = chunkY + rand.nextInt(16) + 8;
+            int k1 = world.getHeightValue(i1, j1);
+            
+            if (k1 < 95 && (k1 < 64 || rand.nextInt(8) == 0))
+            {
+                (new WorldGenBlob(cobbleBlock, rand.nextInt(1))).generate(world, rand, i1, k1, j1, true);
+            }
+        }
+        
+        float l = simplex.noise2(chunkX / 80f, chunkY / 80f) * 60f - 15f;
+
+        for (int i15 = 0; i15 < 1f * strength; i15++)
+        {
+            int i17 = chunkX + rand.nextInt(16) + 8;
+            int i20 = 64 + rand.nextInt(64);
+            int l22 = chunkY + rand.nextInt(16) + 8;
+            (new WorldGenDeadBush(Blocks.deadbush)).generate(world, rand, i17, i20, l22);
+        }
+        
+        for (int l14 = 0; l14 < 12f * strength; l14++)
+        {
+            int l19 = chunkX + rand.nextInt(16) + 8;
+            int k22 = rand.nextInt(128);
+            int j24 = chunkY + rand.nextInt(16) + 8;
+            
+            if (rand.nextInt(16) == 0) {
+                (new WorldGenGrass(Blocks.tallgrass, 1)).generate(world, rand, l19, k22, j24);
+            }
+        }
+    }
 }
--- src/main/java/rtg/world/gen/surface/enhancedbiomes/SurfaceEBWastelands.java
+++ src/main/java/rtg/world/gen/surface/enhancedbiomes/SurfaceEBWastelands.java
@@ -5,8 +5,8 @@ import java.util.Random;
 import rtg.util.CellNoise;
 import rtg.util.CliffCalculator;
 import rtg.util.OpenSimplexNoise;
-import rtg.util.SnowHeightCalculator;
-import rtg.world.gen.surface.SurfaceBase;
+import enhancedbiomes.blocks.EnhancedBiomesBlocks;
+
 import net.minecraft.block.Block;
 import net.minecraft.init.Blocks;
 import net.minecraft.world.World;
@@ -14,65 +14,131 @@ import net.minecraft.world.biome.BiomeGenBase;
 
 public class SurfaceEBWastelands extends SurfaceEBBase
 {
-	public SurfaceEBWastelands(Block top, Block fill) 
-	{
-		super(top, fill);
-	}
-	
-	@Override
-	public void paintTerrain(Block[] blocks, byte[] metadata, int i, int j, int x, int y, int depth, World world, Random rand, OpenSimplexNoise simplex, CellNoise cell, float[] noise, float river, BiomeGenBase[] base)
-	{
-		boolean water = false;
-		boolean riverPaint = false;
-		boolean grass = false;
-		
-		if(river > 0.05f && river + (simplex.noise2(i / 10f, j / 10f) * 0.1f) > 0.86f)
-		{
-			riverPaint = true;
-			
-			if(simplex.noise2(i / 12f, j / 12f) > 0.25f)
-			{
-				grass = true;
-			}
-		}
-		
-		Block b;
-		for(int k = 255; k > -1; k--)
-		{
-			b = blocks[(y * 16 + x) * 256 + k];
-            if(b == Blocks.air)
+    
+    private Block blockTop;
+    private byte byteTop;
+    private Block blockFiller;
+    private byte byteFiller;
+    private Block blockMixTop;
+    private byte byteMixTop;
+    private Block blockMixFiller;
+    private byte byteMixFiller;
+    private Block blockCliff1;
+    private byte byteCliff1;
+    private Block blockCliff2;
+    private byte byteCliff2;
+    private float floMixWidth;
+    private float floMixHeight;
+    private float floSmallWidth;
+    private float floSmallStrength;
+    
+    public SurfaceEBWastelands(Block top, byte topByte, Block filler, byte fillerByte, Block mixTop, byte mixTopByte, Block mixFiller,
+        byte mixFillerByte, Block cliff1, byte cliff1Byte, Block cliff2, byte cliff2Byte, float mixWidth, float mixHeight,
+        float smallWidth, float smallStrength)
+    {
+    
+        super(top, filler);
+        
+        blockTop = top;
+        byteTop = topByte;
+        blockFiller = filler;
+        byteFiller = fillerByte;
+        
+        blockMixTop = mixTop;
+        byteMixTop = mixTopByte;
+        blockMixFiller = mixFiller;
+        byteMixFiller = mixFillerByte;
+        
+        blockCliff1 = cliff1;
+        byteCliff1 = cliff1Byte;
+        
+        blockCliff2 = cliff2;
+        byteCliff2 = cliff2Byte;
+        
+        floMixWidth = mixWidth;
+        floMixHeight = mixHeight;
+        floSmallWidth = smallWidth;
+        floSmallStrength = smallStrength;
+    }
+    
+    @Override
+    public void paintTerrain(Block[] blocks, byte[] metadata, int i, int j, int x, int y, int depth, World world, Random rand,
+        OpenSimplexNoise simplex, CellNoise cell, float[] noise, float river, BiomeGenBase[] base)
+    {
+    
+        float c = CliffCalculator.calc(x, y, noise);
+        boolean cliff = c > 1.4f ? true : false;
+        boolean mix = false;
+        
+        for (int k = 255; k > -1; k--)
+        {
+            Block b = blocks[(y * 16 + x) * 256 + k];
+            if (b == Blocks.air)
             {
-            	depth = -1;
+                depth = -1;
             }
-            else if(b == Blocks.stone)
+            else if (b == Blocks.stone)
             {
-            	depth++;
-
-            	if(riverPaint)
-            	{
-            		if(grass && depth < 4)
-            		{
-    	        		blocks[(y * 16 + x) * 256 + k] = Blocks.dirt;
-            		}
-            		else if(depth == 0)
-            		{
-    	        		blocks[(y * 16 + x) * 256 + k] = rand.nextInt(2) == 0 ? Blocks.stone : Blocks.cobblestone;
-            		}
-            	}
-        		else if(depth > -1 && depth < 9)
-        		{
-        			blocks[(y * 16 + x) * 256 + k] = Blocks.snow;
-            		if(depth == 0 && k > 61 && k < 254)
-            		{
-            			SnowHeightCalculator.calc(x, y, k, blocks, metadata, noise);
-            		}
-        		}
+                depth++;
+                
+                if (shouldReplaceStone()) {
+                    blocks[(y * 16 + x) * 256 + k] = EnhancedBiomesBlocks.stoneEB;
+                    metadata[(y * 16 + x) * 256 + k] = (byte) 11;
+                }
+                
+                if (cliff)
+                {
+                    if (depth > -1 && depth < 2)
+                    {
+                        if (rand.nextInt(3) == 0) {
+                            blocks[(y * 16 + x) * 256 + k] = blockCliff2;
+                            metadata[(y * 16 + x) * 256 + k] = byteCliff2;
+                        }
+                        else {
+                            blocks[(y * 16 + x) * 256 + k] = blockCliff1;
+                            metadata[(y * 16 + x) * 256 + k] = byteCliff1;
+                        }
+                        
+                    }
+                    else if (depth < 10)
+                    {
+                        blocks[(y * 16 + x) * 256 + k] = blockCliff1;
+                        metadata[(y * 16 + x) * 256 + k] = byteCliff1;
+                    }
+                }
+                else
+                {
+                    if (depth == 0 && k > 61)
+                    {
+                        if (simplex.noise2(i / floMixWidth, j / floMixWidth) + simplex.noise2(i / floSmallWidth, j / floSmallWidth)
+                            * floSmallStrength > floMixHeight)
+                        {
+                            blocks[(y * 16 + x) * 256 + k] = blockMixTop;
+                            metadata[(y * 16 + x) * 256 + k] = byteMixTop;
+                            
+                            mix = true;
+                        }
+                        else
+                        {
+                            blocks[(y * 16 + x) * 256 + k] = topBlock;
+                            metadata[(y * 16 + x) * 256 + k] = byteTop;
+                        }
+                    }
+                    else if (depth < 4)
+                    {
+                        if (mix)
+                        {
+                            blocks[(y * 16 + x) * 256 + k] = blockMixFiller;
+                            metadata[(y * 16 + x) * 256 + k] = byteMixFiller;
+                        }
+                        else
+                        {
+                            blocks[(y * 16 + x) * 256 + k] = fillerBlock;
+                            metadata[(y * 16 + x) * 256 + k] = byteFiller;
+                        }
+                    }
+                }
             }
-            else if(!water && b == Blocks.water)
-            {
-    			blocks[(y * 16 + x) * 256 + k] = Blocks.ice;
-            	water = true;
-            }
-		}
-	}
+        }
+    }
 }

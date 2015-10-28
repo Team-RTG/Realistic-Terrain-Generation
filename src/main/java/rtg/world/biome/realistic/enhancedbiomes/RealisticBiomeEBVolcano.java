0ac23f37c98323ace2fe9062d146e0faae76a8a0
WhichOnesPink <whichonespink@hokecraft.co.uk>
2015-10-28 09:41:28 +0000

    Improved EB Volcano biome.

--- src/main/java/rtg/world/biome/realistic/enhancedbiomes/RealisticBiomeEBVolcano.java
+++ src/main/java/rtg/world/biome/realistic/enhancedbiomes/RealisticBiomeEBVolcano.java
@@ -1,25 +1,77 @@
 package rtg.world.biome.realistic.enhancedbiomes;
 
-import net.minecraft.world.biome.BiomeGenBase;
+import java.util.Random;
 
 import rtg.config.enhancedbiomes.ConfigEB;
+import rtg.util.CellNoise;
+import rtg.util.OpenSimplexNoise;
 import rtg.world.biome.BiomeBase;
-import rtg.world.biome.BiomeBase.BiomeCategory;
+import rtg.world.biome.WorldChunkManagerRTG;
+import rtg.world.gen.feature.WorldGenGrass;
+import rtg.world.gen.feature.WorldGenVolcano;
 import rtg.world.gen.surface.enhancedbiomes.SurfaceEBVolcano;
 import rtg.world.gen.terrain.enhancedbiomes.TerrainEBVolcano;
 
+import net.minecraft.block.Block;
+import net.minecraft.init.Blocks;
+import net.minecraft.world.World;
+import net.minecraft.world.biome.BiomeGenBase;
+
 public class RealisticBiomeEBVolcano extends RealisticBiomeEBBase
-{	
-	public RealisticBiomeEBVolcano(BiomeGenBase ebBiome)
-	{
-		super(
-			ebBiome, BiomeBase.climatizedBiome(BiomeGenBase.river, Climate.TEMPERATE),
-			new TerrainEBVolcano(),
-			new SurfaceEBVolcano(ebBiome.topBlock, ebBiome.fillerBlock, false, ebBiome.topBlock, 20f)
-		);
-		
-		this.setRealisticBiomeName("EB Volcano");
-		this.biomeCategory = BiomeCategory.HOT;
-		this.biomeWeight = ConfigEB.weightEBVolcano;
-	}
+{
+    
+    public RealisticBiomeEBVolcano(BiomeGenBase ebBiome)
+    {
+    
+        super(
+            ebBiome, BiomeBase.climatizedBiome(BiomeGenBase.river, Climate.HOT),
+            new TerrainEBVolcano(),
+            new SurfaceEBVolcano(Blocks.grass, Blocks.dirt, true, Blocks.gravel, 0f, 1.5f, 60f, 65f, 1.5f, Blocks.gravel, 0.08f));
+        
+        this.setRealisticBiomeName("EB Volcano");
+        this.biomeCategory = BiomeCategory.HOT;
+        this.biomeWeight = ConfigEB.weightEBVolcano;
+    }
+    
+    @Override
+    public void rDecorate(World world, Random rand, int chunkX, int chunkY, OpenSimplexNoise simplex, CellNoise cell, float strength,
+        float river)
+    {
+    
+        for (int l14 = 0; l14 < 15; l14++)
+        {
+            int l19 = chunkX + rand.nextInt(16) + 8;
+            int k22 = rand.nextInt(128);
+            int j24 = chunkY + rand.nextInt(16) + 8;
+            
+            if (rand.nextInt(3) == 0)
+            {
+                (new WorldGenGrass(Blocks.double_plant, 2)).generate(world, rand, l19, k22, j24);
+            }
+            else
+            {
+                (new WorldGenGrass(Blocks.tallgrass, 1)).generate(world, rand, l19, k22, j24);
+            }
+        }
+    }
+    
+    @Override
+    public void rMapGen(Block[] blocks, byte[] metadata, World world, WorldChunkManagerRTG cmr, Random mapRand, int baseX, int baseY,
+        int chunkX, int chunkY, OpenSimplexNoise simplex, CellNoise cell, float noise[])
+    {
+    
+        if (baseX % 4 == 0 && baseY % 4 == 0 && mapRand.nextInt(6) == 0)
+        {
+            float river = cmr.getRiverStrength(baseX * 16, baseY * 16) + 1f;
+            if (river > 0.98f && cmr.isBorderlessAt(baseX * 16, baseY * 16)
+                && cmr.getNoiseWithRiverOceanAt(baseX * 16, baseY * 16, river, cmr.getOceanValue(baseX * 16, baseY * 16)) > 110f)
+            {
+                long i1 = mapRand.nextLong() / 2L * 2L + 1L;
+                long j1 = mapRand.nextLong() / 2L * 2L + 1L;
+                mapRand.setSeed((long) chunkX * i1 + (long) chunkY * j1 ^ world.getSeed());
+                
+                WorldGenVolcano.build(blocks, metadata, world, mapRand, baseX, baseY, chunkX, chunkY, simplex, cell, noise);
+            }
+        }
+    }
 }

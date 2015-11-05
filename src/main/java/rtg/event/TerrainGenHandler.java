b23a29b46a17af01344f74d73a65e54fd6ef9d24
WhichOnesPink <whichonespink@hokecraft.co.uk>
2015-11-05 06:59:28 +0000

    Added debugging to TerrainGenHandler

--- src/main/java/rtg/event/TerrainGenHandler.java
+++ src/main/java/rtg/event/TerrainGenHandler.java
@@ -1,6 +1,11 @@
 package rtg.event;
 
+import org.apache.logging.log4j.Level;
+
+import rtg.config.rtg.ConfigRTG;
+import rtg.world.gen.structure.MapGenMineshaftRTG;
 import rtg.world.gen.structure.MapGenScatteredFeatureRTG;
+import cpw.mods.fml.common.FMLLog;
 import cpw.mods.fml.common.eventhandler.EventPriority;
 import cpw.mods.fml.common.eventhandler.SubscribeEvent;
 
@@ -15,10 +20,21 @@ public class TerrainGenHandler {
 
 	@SubscribeEvent(priority = EventPriority.LOWEST)
 	public void initMapGen(InitMapGenEvent event) {
-		if (event.type == InitMapGenEvent.EventType.CAVE) {
-			//event.newGen = new MapGenWrapper(event.newGen, new MapGenIronBlockCaves());
-		} else if (event.type == InitMapGenEvent.EventType.SCATTERED_FEATURE) {
+	    
+	    if (ConfigRTG.enableDebugging) {
+	        FMLLog.log(Level.INFO, "event type = %s", event.type.toString());
+	        FMLLog.log(Level.INFO, "event originalGen = %s", event.originalGen.toString());
+	    }
+	    
+		if (event.type == InitMapGenEvent.EventType.SCATTERED_FEATURE) {
 			event.newGen = new MapGenScatteredFeatureRTG();
 		}
+		else if (event.type == InitMapGenEvent.EventType.MINESHAFT) {
+		    //event.newGen = new MapGenMineshaftRTG();
+		}
+		
+        if (ConfigRTG.enableDebugging) {
+            FMLLog.log(Level.INFO, "event newGen = %s", event.newGen.toString());
+        }
 	}
 }

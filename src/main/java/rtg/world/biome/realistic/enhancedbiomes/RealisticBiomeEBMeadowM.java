8112803b456209bec759151130338e1db42608da
WhichOnesPink <whichonespink@hokecraft.co.uk>
2015-11-08 22:38:11 +0000

    Improved EB Meadow M biome.

--- src/main/java/rtg/world/biome/realistic/enhancedbiomes/RealisticBiomeEBMeadowM.java
+++ src/main/java/rtg/world/biome/realistic/enhancedbiomes/RealisticBiomeEBMeadowM.java
@@ -1,5 +1,9 @@
 package rtg.world.biome.realistic.enhancedbiomes;
 
+import enhancedbiomes.EnhancedBiomesMod;
+import enhancedbiomes.blocks.EnhancedBiomesBlocks;
+
+import net.minecraft.block.Block;
 import net.minecraft.init.Blocks;
 import net.minecraft.world.biome.BiomeGenBase;
 
@@ -10,13 +14,43 @@ import rtg.world.gen.surface.enhancedbiomes.SurfaceEBMeadowM;
 import rtg.world.gen.terrain.enhancedbiomes.TerrainEBMeadowM;
 
 public class RealisticBiomeEBMeadowM extends RealisticBiomeEBBase
-{	
+{
+    private static Block ebTopBlock = EnhancedBiomesMod.useNewGrass ? EnhancedBiomesBlocks.grassEB : Blocks.grass;
+    private static byte ebTopByte = EnhancedBiomesMod.useNewGrass ? (byte)6 : (byte)0;
+    private static Block ebFillBlock = EnhancedBiomesMod.useNewGrass ? EnhancedBiomesBlocks.dirtEB : Blocks.dirt;
+    private static byte ebFillByte = EnhancedBiomesMod.useNewGrass ? (byte)6 : (byte)0;
+    private static Block ebMixTopBlock = EnhancedBiomesMod.useNewGrass ? EnhancedBiomesBlocks.grassEB : Blocks.grass;
+    private static byte ebMixTopByte = EnhancedBiomesMod.useNewGrass ? (byte)6 : (byte)0;
+    private static Block ebMixFillBlock = EnhancedBiomesMod.useNewGrass ? EnhancedBiomesBlocks.dirtEB : Blocks.dirt;
+    private static byte ebMixFillByte = EnhancedBiomesMod.useNewGrass ? (byte)6 : (byte)0;
+    private static Block ebCliff1Block = Blocks.stone;
+    private static byte ebCliff1Byte = (byte)0;
+    private static Block ebCliff2Block = Blocks.cobblestone;
+    private static byte ebCliff2Byte = (byte)0;
+    
 	public RealisticBiomeEBMeadowM(BiomeGenBase ebBiome)
 	{
 		super(
 			ebBiome, BiomeBase.climatizedBiome(BiomeGenBase.river, Climate.TEMPERATE),
 			new TerrainEBMeadowM(),
-			new SurfaceEBMeadowM(ebBiome.topBlock, ebBiome.fillerBlock, Blocks.stone, Blocks.cobblestone)
+			new SurfaceEBMeadowM(
+                ebTopBlock, //Block top 
+                ebTopByte, //byte topByte
+                ebFillBlock, //Block filler, 
+                ebFillByte, //byte fillerByte
+                ebMixTopBlock, //Block mixTop, 
+                ebMixTopByte, //byte mixTopByte, 
+                ebMixFillBlock, //Block mixFill, 
+                ebMixFillByte, //byte mixFillByte, 
+                ebCliff1Block, //Block cliff1, 
+                ebCliff1Byte, //byte cliff1Byte, 
+                ebCliff2Block, //Block cliff2, 
+                ebCliff2Byte, //byte cliff2Byte, 
+                80f, //float mixWidth, 
+                -0.15f, //float mixHeight, 
+                10f, //float smallWidth, 
+                0.5f //float smallStrength
+            )
 		);
 		
 		this.setRealisticBiomeName("EB Meadow M");

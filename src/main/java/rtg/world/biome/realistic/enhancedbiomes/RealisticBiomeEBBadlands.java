15843abc72658ec2d1b42b168f28b5b88775c010
WhichOnesPink <whichonespink@hokecraft.co.uk>
2015-11-08 20:48:24 +0000

    Improved EB Badlands biome.

--- src/main/java/rtg/world/biome/realistic/enhancedbiomes/RealisticBiomeEBBadlands.java
+++ src/main/java/rtg/world/biome/realistic/enhancedbiomes/RealisticBiomeEBBadlands.java
@@ -1,5 +1,9 @@
 package rtg.world.biome.realistic.enhancedbiomes;
 
+import enhancedbiomes.EnhancedBiomesMod;
+import enhancedbiomes.blocks.EnhancedBiomesBlocks;
+
+import net.minecraft.block.Block;
 import net.minecraft.init.Blocks;
 import net.minecraft.world.biome.BiomeGenBase;
 
@@ -11,12 +15,42 @@ import rtg.world.gen.terrain.enhancedbiomes.TerrainEBBadlands;
 
 public class RealisticBiomeEBBadlands extends RealisticBiomeEBBase
 {	
+    private static Block ebTopBlock = EnhancedBiomesMod.useNewGrass ? EnhancedBiomesBlocks.grassEB : Blocks.grass;
+    private static byte ebTopByte = EnhancedBiomesMod.useNewGrass ? (byte)7 : (byte)0;
+    private static Block ebFillBlock = EnhancedBiomesMod.useNewGrass ? EnhancedBiomesBlocks.dirtEB : Blocks.dirt;
+    private static byte ebFillByte = EnhancedBiomesMod.useNewGrass ? (byte)7 : (byte)0;
+    private static Block ebMixTopBlock = EnhancedBiomesMod.useNewGrass ? EnhancedBiomesBlocks.grassEB : Blocks.grass;
+    private static byte ebMixTopByte = EnhancedBiomesMod.useNewGrass ? (byte)7 : (byte)0;
+    private static Block ebMixFillBlock = EnhancedBiomesMod.useNewGrass ? EnhancedBiomesBlocks.dirtEB : Blocks.dirt;
+    private static byte ebMixFillByte = EnhancedBiomesMod.useNewGrass ? (byte)7 : (byte)0;
+    private static Block ebCliff1Block = (EnhancedBiomesMod.useNewStone == 1) ? EnhancedBiomesBlocks.stoneEB : Blocks.sandstone;
+    private static byte ebCliff1Byte = (EnhancedBiomesMod.useNewStone == 1) ? (byte)2 : (byte)0;
+    private static Block ebCliff2Block = (EnhancedBiomesMod.useNewStone == 1) ? EnhancedBiomesBlocks.stoneCobbleEB : Blocks.sandstone;
+    private static byte ebCliff2Byte = (EnhancedBiomesMod.useNewStone == 1) ? (byte)2 : (byte)0;
+    
 	public RealisticBiomeEBBadlands(BiomeGenBase ebBiome)
 	{
 		super(
 			ebBiome, BiomeBase.climatizedBiome(BiomeGenBase.river, Climate.HOT),
 			new TerrainEBBadlands(),
-			new SurfaceEBBadlands(ebBiome.topBlock, ebBiome.fillerBlock, false, null, 0f, 1.5f, 60f, 65f, 1.5f, ebBiome.topBlock, 0.15f)
+			new SurfaceEBBadlands(
+			    ebTopBlock, //Block top 
+			    ebTopByte, //byte topByte
+			    ebFillBlock, //Block filler, 
+			    ebFillByte, //byte fillerByte
+			    ebMixTopBlock, //Block mixTop, 
+			    ebMixTopByte, //byte mixTopByte, 
+			    ebMixFillBlock, //Block mixFill, 
+			    ebMixFillByte, //byte mixFillByte, 
+			    ebCliff1Block, //Block cliff1, 
+			    ebCliff1Byte, //byte cliff1Byte, 
+			    ebCliff2Block, //Block cliff2, 
+			    ebCliff2Byte, //byte cliff2Byte, 
+                80f, //float mixWidth, 
+                -0.15f, //float mixHeight, 
+                10f, //float smallWidth, 
+                0.5f //float smallStrength
+            )
 		);
 		
 		this.setRealisticBiomeName("EB Badlands");

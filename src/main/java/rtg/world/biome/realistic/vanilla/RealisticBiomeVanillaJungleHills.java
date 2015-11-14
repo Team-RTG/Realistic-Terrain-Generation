3a828503e22e577951c6fbeec5d3d139836f62ac
WhichOnesPink <whichonespink@hokecraft.co.uk>
2015-11-14 18:38:38 +0000

    Improved vanilla Jungle biome trinity.

--- src/main/java/rtg/world/biome/realistic/vanilla/RealisticBiomeVanillaJungleHills.java
+++ src/main/java/rtg/world/biome/realistic/vanilla/RealisticBiomeVanillaJungleHills.java
@@ -6,7 +6,9 @@ import rtg.config.vanilla.ConfigVanilla;
 import rtg.util.CellNoise;
 import rtg.util.OpenSimplexNoise;
 import rtg.world.biome.BiomeBase;
-import rtg.world.gen.feature.WorldGenGrass;
+import rtg.world.biome.realistic.RealisticBiomeBase;
+import rtg.world.gen.feature.tree.WorldGenTreeJungleFat;
+import rtg.world.gen.feature.tree.WorldGenTreeJungleTall;
 import rtg.world.gen.surface.vanilla.SurfaceVanillaJungleHills;
 import rtg.world.gen.terrain.vanilla.TerrainVanillaJungleHills;
 
@@ -14,9 +16,7 @@ import net.minecraft.block.Block;
 import net.minecraft.init.Blocks;
 import net.minecraft.world.World;
 import net.minecraft.world.biome.BiomeGenBase;
-import net.minecraft.world.gen.feature.WorldGenMegaJungle;
-import net.minecraft.world.gen.feature.WorldGenShrub;
-import net.minecraft.world.gen.feature.WorldGenTrees;
+import net.minecraft.world.gen.feature.WorldGenLakes;
 import net.minecraft.world.gen.feature.WorldGenerator;
 
 public class RealisticBiomeVanillaJungleHills extends RealisticBiomeVanillaBase
@@ -31,7 +31,7 @@ public class RealisticBiomeVanillaJungleHills extends RealisticBiomeVanillaBase
         super(
             BiomeGenBase.jungleHills,
             BiomeBase.climatizedBiome(BiomeGenBase.river, Climate.WET),
-            new TerrainVanillaJungleHills(230f, 120f, 50f),
+            new TerrainVanillaJungleHills(),
             new SurfaceVanillaJungleHills(Blocks.grass, Blocks.dirt, false, null, 1f, 1.5f, 60f, 65f, 1.5f));
         
         this.setRealisticBiomeName("Vanilla Jungle Hills");
@@ -44,36 +44,32 @@ public class RealisticBiomeVanillaJungleHills extends RealisticBiomeVanillaBase
         float river)
     {
     
-        for (int b33 = 0; b33 < 2; b33++)
+        if (rand.nextInt((int) (16f / strength)) == 0)
+        {
+            int i2 = chunkX + rand.nextInt(16) + 8;
+            int i8 = chunkY + rand.nextInt(16) + 8;
+            int l4 = world.getHeightValue(i2, i8);
+            
+            if (l4 > 63)
+            {
+                (new WorldGenLakes(Blocks.water)).generate(world, rand, i2, l4, i8);
+            }
+        }
+        
+        for (int b33 = 0; b33 < 3; b33++)
         {
             int j6 = chunkX + rand.nextInt(16) + 8;
             int k10 = chunkY + rand.nextInt(16) + 8;
             int z52 = world.getHeightValue(j6, k10);
             
-            if (z52 < 100 || rand.nextInt(3) == 0)
+            if (z52 < 100 && rand.nextInt(4) == 0)
             {
-                WorldGenerator worldgenerator =
-                    rand.nextInt(5) == 0 ? new WorldGenShrub(0, 0) : rand.nextInt(3) == 0 ? new WorldGenMegaJungle(false, 5, 10, 3, 3)
-                        : new WorldGenTrees(false, 3 + rand.nextInt(5), 3, 3, true);
+                WorldGenerator worldgenerator = new WorldGenTreeJungleTall(Blocks.log, 3, Blocks.leaves, 3, 10 + rand.nextInt(10), 2 + rand.nextInt(2), 13f, 4, 0.32f, 0.1f);
                 worldgenerator.setScale(1.0D, 1.0D, 1.0D);
                 worldgenerator.generate(world, rand, j6, z52, k10);
             }
         }
         
-        for (int l14 = 0; l14 < 15; l14++)
-        {
-            int l19 = chunkX + rand.nextInt(16) + 8;
-            int k22 = rand.nextInt(128);
-            int j24 = chunkY + rand.nextInt(16) + 8;
-            
-            if (rand.nextInt(6) == 0)
-            {
-                (new WorldGenGrass(Blocks.double_plant, 2)).generate(world, rand, l19, k22, j24);
-            }
-            else
-            {
-                (new WorldGenGrass(Blocks.tallgrass, 1)).generate(world, rand, l19, k22, j24);
-            }
-        }
+        RealisticBiomeBase.rDecorateSeedBiome(world, rand, chunkX, chunkY, simplex, cell, strength, river, baseBiome);
     }
 }
--- src/main/java/rtg/world/biome/realistic/vanilla/RealisticBiomeVanillaJungleM.java
+++ src/main/java/rtg/world/biome/realistic/vanilla/RealisticBiomeVanillaJungleM.java
@@ -1,13 +1,22 @@
 package rtg.world.biome.realistic.vanilla;
 
+import java.util.Random;
+
 import rtg.config.vanilla.ConfigVanilla;
+import rtg.util.CellNoise;
+import rtg.util.OpenSimplexNoise;
 import rtg.world.biome.BiomeBase;
+import rtg.world.biome.realistic.RealisticBiomeBase;
+import rtg.world.gen.feature.tree.WorldGenTreeJungleTall;
 import rtg.world.gen.surface.vanilla.SurfaceVanillaJungleM;
 import rtg.world.gen.terrain.vanilla.TerrainVanillaJungleM;
 
 import net.minecraft.block.Block;
 import net.minecraft.init.Blocks;
+import net.minecraft.world.World;
 import net.minecraft.world.biome.BiomeGenBase;
+import net.minecraft.world.gen.feature.WorldGenLakes;
+import net.minecraft.world.gen.feature.WorldGenerator;
 
 public class RealisticBiomeVanillaJungleM extends RealisticBiomeVanillaBase
 {	
@@ -29,5 +38,39 @@ public class RealisticBiomeVanillaJungleM extends RealisticBiomeVanillaBase
 		this.setRealisticBiomeName("Vanilla Jungle M");
 		this.biomeSize = BiomeSize.NORMAL;
 		this.biomeWeight = ConfigVanilla.weightVanillaJungleM;
-	}	
+	}
+	
+    @Override
+    public void rDecorate(World world, Random rand, int chunkX, int chunkY, OpenSimplexNoise simplex, CellNoise cell, float strength,
+        float river)
+    {
+    
+        if (rand.nextInt((int) (16f / strength)) == 0)
+        {
+            int i2 = chunkX + rand.nextInt(16) + 8;
+            int i8 = chunkY + rand.nextInt(16) + 8;
+            int l4 = world.getHeightValue(i2, i8);
+            
+            if (l4 > 63)
+            {
+                (new WorldGenLakes(Blocks.water)).generate(world, rand, i2, l4, i8);
+            }
+        }
+        
+        for (int b33 = 0; b33 < 3; b33++)
+        {
+            int j6 = chunkX + rand.nextInt(16) + 8;
+            int k10 = chunkY + rand.nextInt(16) + 8;
+            int z52 = world.getHeightValue(j6, k10);
+            
+            if (z52 < 100 && rand.nextInt(4) == 0)
+            {
+                WorldGenerator worldgenerator = new WorldGenTreeJungleTall(Blocks.log, 3, Blocks.leaves, 3, 10 + rand.nextInt(10), 2 + rand.nextInt(2), 13f, 4, 0.32f, 0.1f);
+                worldgenerator.setScale(1.0D, 1.0D, 1.0D);
+                worldgenerator.generate(world, rand, j6, z52, k10);
+            }
+        }
+        
+        RealisticBiomeBase.rDecorateSeedBiome(world, rand, chunkX, chunkY, simplex, cell, strength, river, baseBiome);
+    }
 }

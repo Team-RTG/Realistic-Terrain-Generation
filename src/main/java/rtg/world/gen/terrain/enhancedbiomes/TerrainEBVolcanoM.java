26666a2f182a1539d6945524479053e78d6d6cb0
WhichOnesPink <whichonespink@hokecraft.co.uk>
2015-11-15 15:47:40 +0000

    Improved EB Volcano M biome.

--- src/main/java/rtg/world/gen/terrain/enhancedbiomes/TerrainEBVolcanoM.java
+++ src/main/java/rtg/world/gen/terrain/enhancedbiomes/TerrainEBVolcanoM.java
@@ -13,27 +13,23 @@ public class TerrainEBVolcanoM extends TerrainBase
 	@Override
 	public float generateNoise(OpenSimplexNoise simplex, CellNoise cell, int x, int y, float border, float river)
 	{
-		float b = (12f + (simplex.noise2(x / 300f, y / 300f) * 6f));
-		float h = cell.noise(x / 200D, y / 200D, 1D) * b * river;
-		h *= h * 1.5f;
-		h = h > 155f ? 155f : h;
-		
-		if(h > 2f)
-		{
-			float d = (h - 2f) / 2f > 8f ? 8f : (h - 2f) / 2f;
-			h += simplex.noise2(x / 30f, y / 30f) * d;
-			h += simplex.noise2(x / 50f, y / 50f) * d * 0.5f;
+        float st = 15f - ((cell.noise(x / 500D, y / 500D, 1D) * 42f) + (simplex.noise2(x / 30f, y / 30f) * 2f));
+        
+        st = st < 0f ? 0f : st;
 
-			if(h > 35f)
-			{
-				float d2 = (h - 35f) / 1.5f > 30f ? 30f : (h - 35f) / 1.5f;
-				h += cell.noise(x / 25D, y / 25D, 1D) * d2;
-			}
-		}
-		
-		h += simplex.noise2(x / 18f, y / 18f) * 3;
-		h += simplex.noise2(x / 8f, y / 8f) * 2;
-				
-		return 45f + h + (b * 2);
+        float h = st;
+        h = h < 0f ? 0f : h;
+        h += (h * 0.4f) * ((h * 0.4f) * 2f);
+        
+        if(h > 10f)
+        {
+            float d2 = (h - 10f) / 1.5f > 30f ? 30f : (h - 10f) / 1.5f;
+            h += cell.noise(x / 25D, y / 25D, 1D) * d2;
+        }
+        
+        h += simplex.noise2(x / 18f, y / 18f) * 3;
+        h += simplex.noise2(x / 8f, y / 8f) * 2;
+        
+        return 65f + h * border;
 	}
 }

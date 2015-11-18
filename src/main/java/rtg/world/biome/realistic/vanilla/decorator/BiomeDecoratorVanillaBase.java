caa748bbdd9e8e5019e1e9e11e5ccd6d8a0bf750
WhichOnesPink <whichonespink@hokecraft.co.uk>
2015-11-18 22:59:25 +0000

    Added BiomeDecoratorVanillaBase.

---
+++ src/main/java/rtg/world/biome/realistic/vanilla/decorator/BiomeDecoratorVanillaBase.java
@@ -0,0 +1,12 @@
+package rtg.world.biome.realistic.vanilla.decorator;
+
+import rtg.world.biome.BiomeDecoratorRTG;
+
+public class BiomeDecoratorVanillaBase extends BiomeDecoratorRTG
+{
+    
+    public BiomeDecoratorVanillaBase()
+    {
+        super();
+    }
+}

3c3f54c374cf067c76b68b78626d0f48ed33004a
WhichOnesPink <whichonespink@hokecraft.co.uk>
2015-11-26 20:26:30 +0000

    Removed emerald removal from onPopulateChunkEvent()

--- src/main/java/rtg/event/EventManagerRTG.java
+++ src/main/java/rtg/event/EventManagerRTG.java
@@ -11,13 +11,9 @@ import cpw.mods.fml.common.FMLLog;
 import cpw.mods.fml.common.eventhandler.EventPriority;
 import cpw.mods.fml.common.eventhandler.SubscribeEvent;
 
-import net.minecraft.block.Block;
-import net.minecraft.init.Blocks;
-import net.minecraft.world.World;
 import net.minecraft.world.gen.structure.MapGenStructureIO;
 
 import net.minecraftforge.event.terraingen.InitMapGenEvent;
-import net.minecraftforge.event.terraingen.PopulateChunkEvent;
 
 public class EventManagerRTG
 {
@@ -56,57 +52,4 @@ public class EventManagerRTG
             FMLLog.log(Level.INFO, "event newGen = %s", event.newGen.toString());
         }
 	}
-	
-    @SubscribeEvent
-    public void onPopulateChunkEvent(PopulateChunkEvent.Post event)
-    {
-        if (!ConfigRTG.generateOreEmerald)
-        {
-            World world = event.world;
-            int endX = (event.chunkX * 16) + 16;
-            int endZ = (event.chunkZ * 16) + 16;
-            Block checkBlock;
-            Block needleBlock = Blocks.emerald_ore;
-            Block replaceBlock = Blocks.stone;
-            int replaceBlockMeta = 0;
-
-            // Get the highest possible existing block location.
-            int maxY = event.chunkProvider.provideChunk(event.chunkX, event.chunkZ).getTopFilledSegment() + 15;
-            
-            /*if (Loader.isModLoaded("enhancedbiomes") && EnhancedBiomesMod.useNewStone == 1) {
-                
-                needleBlock = (EnhancedBiomesMod.useNewOres == 1) ? EnhancedBiomesBlocks.oreEmeraldEB : Blocks.emerald_ore;
-                replaceBlock = (EnhancedBiomesMod.useNewOres == 1) ? EnhancedBiomesBlocks.stoneEB : Blocks.stone;
-                replaceBlockMeta = -1;
-            }
-            else {
-                
-                needleBlock = Blocks.emerald_ore;
-                replaceBlock = Blocks.stone;
-                replaceBlockMeta = 0;
-            }*/
-            
-            for (int x = event.chunkX * 16; x < endX; ++x)
-            {
-                for (int z = event.chunkZ * 16; z < endZ; ++z)
-                {
-                    for (int y = 0; y < maxY; ++y)
-                    {   
-                        if (world.getBlock(x, y, z).isReplaceableOreGen(world, x, y, z, needleBlock)) {
-                            
-                            /*if (replaceBlockMeta == -1) {
-                                replaceBlockMeta = (EnhancedBiomesMod.useNewOres == 1) ? (byte) world.getBlockMetadata(x, y, z) : (byte) 0;
-                            }*/
-                            
-                            world.setBlock(x, y, z, replaceBlock, replaceBlockMeta, 2);
-                            
-                            if (ConfigRTG.enableDebugging) {
-                                FMLLog.log(Level.INFO, "Emerald replaced at %d, %d, %d", x, y, z);
-                            }
-                        }
-                    }
-                }
-            }
-        }
-    }
 }

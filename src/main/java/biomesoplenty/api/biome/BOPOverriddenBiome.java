dc852ac09a04b30b2a53d86725cd9718e1a8a32f
WhichOnesPink <whichonespink@hokecraft.co.uk>
2015-11-27 04:16:23 +0000

    Removed non-standalone files from BOP API.

--- src/main/java/biomesoplenty/api/biome/BOPOverriddenBiome.java
+++
@@ -1,9 +0,0 @@
-package biomesoplenty.api.biome;
-
-public class BOPOverriddenBiome<T extends BOPBiomeDecorator> extends BOPBiome<T> 
-{
-	public BOPOverriddenBiome(int biomeID, Class<T> clazz) 
-	{
-		super(biomeID, clazz, false);
-	}
-}

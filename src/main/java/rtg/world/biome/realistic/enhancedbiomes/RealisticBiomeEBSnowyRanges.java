3101fd4ce9946eee6d1bb4257216582eaae59a62
WhichOnesPink <whichonespink@hokecraft.co.uk>
2015-11-27 22:02:12 +0000

    Re-decorated EB Snowy Ranges biome.
    Also fixed emerald gen/removal.

--- src/main/java/rtg/world/biome/realistic/enhancedbiomes/RealisticBiomeEBSnowyRanges.java
+++ src/main/java/rtg/world/biome/realistic/enhancedbiomes/RealisticBiomeEBSnowyRanges.java
@@ -1,22 +1,60 @@
 package rtg.world.biome.realistic.enhancedbiomes;
 
-import net.minecraft.init.Blocks;
-import net.minecraft.world.biome.BiomeGenBase;
+import java.util.Random;
 
 import rtg.config.enhancedbiomes.ConfigEB;
+import rtg.util.CellNoise;
+import rtg.util.OpenSimplexNoise;
 import rtg.world.biome.BiomeBase;
-import rtg.world.biome.BiomeBase.BiomeSize;
+import rtg.world.biome.realistic.RealisticBiomeBase;
 import rtg.world.gen.surface.enhancedbiomes.SurfaceEBSnowyRanges;
 import rtg.world.gen.terrain.enhancedbiomes.TerrainEBSnowyRanges;
+import enhancedbiomes.EnhancedBiomesMod;
+import enhancedbiomes.blocks.EnhancedBiomesBlocks;
+
+import net.minecraft.block.Block;
+import net.minecraft.init.Blocks;
+import net.minecraft.world.World;
+import net.minecraft.world.biome.BiomeGenBase;
 
 public class RealisticBiomeEBSnowyRanges extends RealisticBiomeEBBase
-{	
+{
+    private static Block ebTopBlock = EnhancedBiomesMod.useNewGrass ? EnhancedBiomesBlocks.grassEB : Blocks.grass;
+    private static byte ebTopByte = EnhancedBiomesMod.useNewGrass ? (byte)5 : (byte)0;
+    private static Block ebFillBlock = EnhancedBiomesMod.useNewGrass ? EnhancedBiomesBlocks.dirtEB : Blocks.dirt;
+    private static byte ebFillByte = EnhancedBiomesMod.useNewGrass ? (byte)5 : (byte)0;
+    private static Block ebMixTopBlock = Blocks.snow;
+    private static byte ebMixTopByte = (byte)0;
+    private static Block ebMixFillBlock = Blocks.snow;
+    private static byte ebMixFillByte = (byte)0;
+    private static Block ebCliff1Block = Blocks.snow;
+    private static byte ebCliff1Byte = (byte)0;
+    private static Block ebCliff2Block = Blocks.snow;
+    private static byte ebCliff2Byte = (byte)0;
+    
 	public RealisticBiomeEBSnowyRanges(BiomeGenBase ebBiome)
 	{
 		super(
 			ebBiome, BiomeBase.climatizedBiome(BiomeGenBase.river, Climate.COLD),
-			new TerrainEBSnowyRanges(),
-			new SurfaceEBSnowyRanges(ebBiome.topBlock, ebBiome.fillerBlock, Blocks.stone, Blocks.cobblestone)
+			new TerrainEBSnowyRanges(200f, 100f, 0f),
+			new SurfaceEBSnowyRanges(
+                ebTopBlock, //Block top 
+                ebTopByte, //byte topByte
+                ebFillBlock, //Block filler, 
+                ebFillByte, //byte fillerByte
+                ebMixTopBlock, //Block mixTop, 
+                ebMixTopByte, //byte mixTopByte, 
+                ebMixFillBlock, //Block mixFill, 
+                ebMixFillByte, //byte mixFillByte, 
+                ebCliff1Block, //Block cliff1, 
+                ebCliff1Byte, //byte cliff1Byte, 
+                ebCliff2Block, //Block cliff2, 
+                ebCliff2Byte, //byte cliff2Byte, 
+                60f, //float mixWidth, 
+                -0.15f, //float mixHeight, 
+                2f, //float smallWidth, 
+                0.5f //float smallStrength
+            )
 		);
 		
 		this.setRealisticBiomeName("EB Snowy Ranges");
@@ -24,4 +62,16 @@ public class RealisticBiomeEBSnowyRanges extends RealisticBiomeEBBase
 		this.biomeWeight = ConfigEB.weightEBSnowyRanges;
 		this.generateVillages = ConfigEB.villageEBSnowyRanges;
 	}
+	
+    @Override
+    public void rDecorate(World world, Random rand, int chunkX, int chunkY, OpenSimplexNoise simplex, CellNoise cell, float strength,
+        float river)
+    {
+        
+        RealisticBiomeBase.rDecorateSeedBiome(world, rand, chunkX, chunkY, simplex, cell, strength, river, baseBiome);
+        
+        //Emeralds.
+        //rRemoveEmeralds(world, rand, chunkX, chunkY, false);
+        rGenerateEmeralds(world, rand, chunkX, chunkY, false);
+    }
 }

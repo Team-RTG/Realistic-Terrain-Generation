cdf68012c3ffee9541783265988d8dffe96dd694
WhichOnesPink <whichonespink@hokecraft.co.uk>
2015-11-27 20:13:13 +0000

    Re-decorated EB Mountainous Archipelago.
    Also fixed emerald gen/removal.

--- src/main/java/rtg/world/biome/realistic/enhancedbiomes/RealisticBiomeEBMountainousArchipelago.java
+++ src/main/java/rtg/world/biome/realistic/enhancedbiomes/RealisticBiomeEBMountainousArchipelago.java
@@ -1,21 +1,60 @@
 package rtg.world.biome.realistic.enhancedbiomes;
 
-import net.minecraft.world.biome.BiomeGenBase;
+import java.util.Random;
 
+import enhancedbiomes.EnhancedBiomesMod;
+import enhancedbiomes.blocks.EnhancedBiomesBlocks;
 import rtg.config.enhancedbiomes.ConfigEB;
+import rtg.util.CellNoise;
+import rtg.util.OpenSimplexNoise;
 import rtg.world.biome.BiomeBase;
-import rtg.world.biome.BiomeBase.BiomeSize;
+import rtg.world.biome.realistic.RealisticBiomeBase;
 import rtg.world.gen.surface.enhancedbiomes.SurfaceEBMountainousArchipelago;
 import rtg.world.gen.terrain.enhancedbiomes.TerrainEBMountainousArchipelago;
 
+import net.minecraft.block.Block;
+import net.minecraft.init.Blocks;
+import net.minecraft.world.World;
+import net.minecraft.world.biome.BiomeGenBase;
+
 public class RealisticBiomeEBMountainousArchipelago extends RealisticBiomeEBBase
-{	
+{
+    private static Block ebTopBlock = EnhancedBiomesMod.useNewGrass ? EnhancedBiomesBlocks.grassEB : Blocks.grass;
+    private static byte ebTopByte = EnhancedBiomesMod.useNewGrass ? (byte)1 : (byte)0;
+    private static Block ebFillBlock = EnhancedBiomesMod.useNewGrass ? EnhancedBiomesBlocks.dirtEB : Blocks.dirt;
+    private static byte ebFillByte = EnhancedBiomesMod.useNewGrass ? (byte)1 : (byte)0;
+    private static Block ebMixTopBlock = EnhancedBiomesMod.useNewGrass ? EnhancedBiomesBlocks.grassEB : Blocks.grass;
+    private static byte ebMixTopByte = EnhancedBiomesMod.useNewGrass ? (byte)1 : (byte)0;
+    private static Block ebMixFillBlock = EnhancedBiomesMod.useNewGrass ? EnhancedBiomesBlocks.dirtEB : Blocks.dirt;
+    private static byte ebMixFillByte = EnhancedBiomesMod.useNewGrass ? (byte)1 : (byte)0;
+    private static Block ebCliff1Block = (EnhancedBiomesMod.useNewStone == 1) ? EnhancedBiomesBlocks.stoneEB : Blocks.stone;
+    private static byte ebCliff1Byte = (EnhancedBiomesMod.useNewStone == 1) ? (byte)11 : (byte)0;
+    private static Block ebCliff2Block = (EnhancedBiomesMod.useNewStone == 1) ? EnhancedBiomesBlocks.stoneCobbleEB : Blocks.cobblestone;
+    private static byte ebCliff2Byte = (EnhancedBiomesMod.useNewStone == 1) ? (byte)11 : (byte)0;
+    
 	public RealisticBiomeEBMountainousArchipelago(BiomeGenBase ebBiome)
 	{
 		super(
 			ebBiome, BiomeBase.climatizedBiome(BiomeGenBase.river, Climate.COLD),
 			new TerrainEBMountainousArchipelago(200f, 100f, 0f),
-			new SurfaceEBMountainousArchipelago(ebBiome.topBlock, ebBiome.fillerBlock, false, null, 0.95f)
+			new SurfaceEBMountainousArchipelago(
+                ebTopBlock, //Block top 
+                ebTopByte, //byte topByte
+                ebFillBlock, //Block filler, 
+                ebFillByte, //byte fillerByte
+                ebMixTopBlock, //Block mixTop, 
+                ebMixTopByte, //byte mixTopByte, 
+                ebMixFillBlock, //Block mixFill, 
+                ebMixFillByte, //byte mixFillByte, 
+                ebCliff1Block, //Block cliff1, 
+                ebCliff1Byte, //byte cliff1Byte, 
+                ebCliff2Block, //Block cliff2, 
+                ebCliff2Byte, //byte cliff2Byte, 
+                1f, //float mixWidth, 
+                -0.15f, //float mixHeight, 
+                2f, //float smallWidth, 
+                0.5f //float smallStrength
+            )
 		);
 		
 		this.setRealisticBiomeName("EB Mountainous Archipelago");
@@ -23,4 +62,16 @@ public class RealisticBiomeEBMountainousArchipelago extends RealisticBiomeEBBase
 		this.biomeWeight = ConfigEB.weightEBMountainousArchipelago;
 		this.generateVillages = ConfigEB.villageEBMountainousArchipelago;
 	}
+	
+    @Override
+    public void rDecorate(World world, Random rand, int chunkX, int chunkY, OpenSimplexNoise simplex, CellNoise cell, float strength,
+        float river)
+    {
+        
+        RealisticBiomeBase.rDecorateSeedBiome(world, rand, chunkX, chunkY, simplex, cell, strength, river, baseBiome);
+        
+        //Emeralds.
+        //rRemoveEmeralds(world, rand, chunkX, chunkY, false);
+        rGenerateEmeralds(world, rand, chunkX, chunkY, false);
+    }
 }

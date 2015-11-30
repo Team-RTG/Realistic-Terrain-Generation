bc8be1fa07e09c605170fef233c223360f0474c9
WhichOnesPink <whichonespink@hokecraft.co.uk>
2015-11-30 20:43:09 +0000

    Added custom EB API.

---
+++ src/main/java/enhancedbiomes/api/EBGrassMeta.java
@@ -0,0 +1,13 @@
+package enhancedbiomes.api;
+
+
+public class EBGrassMeta
+{
+    public static final byte ALFISOL = (byte)0;
+    public static final byte ANDISOL = (byte)1;
+    public static final byte GELISOL = (byte)3;
+    public static final byte HISTOSOL = (byte)4;
+    public static final byte INCEPTISOL = (byte)5;
+    public static final byte MOLLISOL = (byte)6;
+    public static final byte OXISOL = (byte)7;
+}
---
+++ src/main/java/enhancedbiomes/api/EBStoneMeta.java
@@ -0,0 +1,19 @@
+package enhancedbiomes.api;
+
+
+public class EBStoneMeta
+{
+    public static final byte BASALT = (byte)0;
+    public static final byte SHALE = (byte)1;
+    public static final byte HARDENED_SANDSTONE = (byte)2;
+    public static final byte LIMESTONE = (byte)3;
+    public static final byte SLATE = (byte)4;
+    public static final byte RHYOLITE = (byte)5;
+    public static final byte CHALK = (byte)6;
+    public static final byte MARBLE = (byte)7;
+    public static final byte DOLOMITE = (byte)8;
+    public static final byte SCHIST = (byte)9;
+    public static final byte CHERT = (byte)10;
+    public static final byte GABBRO = (byte)11;
+    public static final byte DACITE = (byte)12;
+}

6b22330e0ffc83a2d7c65c44f4f143a9eeca1a99
WhichOnesPink <whichonespink@hokecraft.co.uk>
2015-11-30 08:35:11 +0000

    Re-decorated EB Glacier biome.

--- src/main/java/rtg/world/biome/realistic/enhancedbiomes/RealisticBiomeEBGlacier.java
+++ src/main/java/rtg/world/biome/realistic/enhancedbiomes/RealisticBiomeEBGlacier.java
@@ -1,5 +1,9 @@
 package rtg.world.biome.realistic.enhancedbiomes;
 
+import enhancedbiomes.EnhancedBiomesMod;
+
+import net.minecraft.block.Block;
+import net.minecraft.init.Blocks;
 import net.minecraft.world.biome.BiomeGenBase;
 
 import rtg.config.enhancedbiomes.ConfigEB;
@@ -9,18 +13,29 @@ import rtg.world.gen.surface.enhancedbiomes.SurfaceEBGlacier;
 import rtg.world.gen.terrain.enhancedbiomes.TerrainEBGlacier;
 
 public class RealisticBiomeEBGlacier extends RealisticBiomeEBBase
-{	
+{
+    public static Block ebDominantStoneBlock;
+    public static byte ebDominantStoneMeta;
+    public static Block ebDominantCobblestoneBlock;
+    public static byte ebDominantCobblestoneMeta;
+    
 	public RealisticBiomeEBGlacier(BiomeGenBase ebBiome)
 	{
 		super(
 			ebBiome, BiomeBase.climatizedBiome(BiomeGenBase.frozenRiver, Climate.ICE),
-			new TerrainEBGlacier(),
-			new SurfaceEBGlacier(ebBiome.topBlock, ebBiome.fillerBlock, false, null, 0.45f)
+			new TerrainEBGlacier(240f, 140f, 0f),
+			new SurfaceEBGlacier(Blocks.packed_ice, Blocks.packed_ice, Blocks.snow, Blocks.snow, Blocks.packed_ice, Blocks.packed_ice, 60f,
+                -0.14f, 14f, 0.25f)
 		);
 		
 		this.setRealisticBiomeName("EB Glacier");
 		this.biomeSize = BiomeSize.NORMAL;
 		this.biomeWeight = ConfigEB.weightEBGlacier;
 		this.generateVillages = ConfigEB.villageEBGlacier;
-	}
+        
+        this.ebDominantStoneBlock = EnhancedBiomesMod.getDominantStone(ebBiome.biomeID);
+        this.ebDominantStoneMeta = EnhancedBiomesMod.getDominantStoneMeta(ebBiome.biomeID);
+        this.ebDominantCobblestoneBlock = EnhancedBiomesMod.getCobbleFromStone(ebDominantStoneBlock);
+        this.ebDominantCobblestoneMeta = ebDominantStoneMeta;
+    }
 }
--- src/main/java/rtg/world/gen/surface/enhancedbiomes/SurfaceEBGlacier.java
+++ src/main/java/rtg/world/gen/surface/enhancedbiomes/SurfaceEBGlacier.java
@@ -5,7 +5,9 @@ import java.util.Random;
 import rtg.util.CellNoise;
 import rtg.util.CliffCalculator;
 import rtg.util.OpenSimplexNoise;
+import rtg.world.biome.realistic.enhancedbiomes.RealisticBiomeEBGlacier;
 import rtg.world.gen.surface.SurfaceBase;
+
 import net.minecraft.block.Block;
 import net.minecraft.init.Blocks;
 import net.minecraft.world.World;
@@ -13,21 +15,91 @@ import net.minecraft.world.biome.BiomeGenBase;
 
 public class SurfaceEBGlacier extends SurfaceEBBase
 {
-	private boolean beach;
-	private Block beachBlock;
-	private float min;
+	private Block mixBlockTop;
+	private Block mixBlockFill;
+	private Block cliffBlock1;
+	private Block cliffBlock2;
+	private float width;
+	private float height;
+	private float smallW;
+	private float smallS;
 	
-	public SurfaceEBGlacier(Block top, Block fill, boolean genBeach, Block genBeachBlock, float minCliff) 
+	public SurfaceEBGlacier(Block top, Block filler, Block mixTop, Block mixFill, Block cliff1, Block cliff2, float mixWidth, float mixHeight, float smallWidth, float smallStrength)
 	{
-		super(top, fill);
-		beach = genBeach;
-		beachBlock = genBeachBlock;
-		min = minCliff;
+		super(top, filler);
+		
+		mixBlockTop = mixTop;
+		mixBlockFill = mixFill;
+		cliffBlock1 = cliff1;
+		cliffBlock2 = cliff2;
+		
+		width = mixWidth;
+		height = mixHeight;
+		smallW = smallWidth;
+		smallS = smallStrength;
 	}
-
+	
 	@Override
 	public void paintTerrain(Block[] blocks, byte[] metadata, int i, int j, int x, int y, int depth, World world, Random rand, OpenSimplexNoise simplex, CellNoise cell, float[] noise, float river, BiomeGenBase[] base)
 	{
+		float c = CliffCalculator.calc(x, y, noise);
+		boolean cliff = c > 1.4f ? true : false;
+		boolean mix = false;
 		
+		for(int k = 255; k > -1; k--)
+		{
+			Block b = blocks[(y * 16 + x) * 256 + k];
+            if(b == Blocks.air)
+            {
+            	depth = -1;
+            }
+            else if(b == Blocks.stone)
+            {
+                depth++;
+
+                if (shouldReplaceStone()) {
+                    blocks[(y * 16 + x) * 256 + k] = RealisticBiomeEBGlacier.ebDominantStoneBlock;
+                    metadata[(y * 16 + x) * 256 + k] = RealisticBiomeEBGlacier.ebDominantStoneMeta;
+                }
+
+            	if(cliff)
+            	{
+            		if(depth > -1 && depth < 2)
+            		{
+            			blocks[(y * 16 + x) * 256 + k] = rand.nextInt(3) == 0 ? cliffBlock2 : cliffBlock1; 
+            		}
+            		else if (depth < 10)
+            		{
+            			blocks[(y * 16 + x) * 256 + k] = cliffBlock1;
+            		}
+            	}
+            	else
+            	{
+	        		if(depth == 0 && k > 61)
+	        		{
+	        			if(simplex.noise2(i / width, j / width) + simplex.noise2(i / smallW, j / smallW) * smallS > height)
+	        			{
+	        				blocks[(y * 16 + x) * 256 + k] = mixBlockTop;
+	        				mix = true;
+	        			}
+	        			else
+	        			{
+	        				blocks[(y * 16 + x) * 256 + k] = topBlock;
+	        			}
+	        		}
+	        		else if(depth < 4)
+	        		{
+	        			if(mix)
+	        			{
+		        			blocks[(y * 16 + x) * 256 + k] = mixBlockFill;
+	        			}
+	        			else
+	        			{
+		        			blocks[(y * 16 + x) * 256 + k] = fillerBlock;
+	        			}
+	        		}
+            	}
+            }
+		}
 	}
 }

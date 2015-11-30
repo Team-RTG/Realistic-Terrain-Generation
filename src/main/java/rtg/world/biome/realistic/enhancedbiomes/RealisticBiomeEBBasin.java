286095e02afd778ef29cbf659ad53650dbd8adc3
WhichOnesPink <whichonespink@hokecraft.co.uk>
2015-11-30 20:45:58 +0000

    Re-decorated EB Basin.

--- src/main/java/rtg/world/biome/realistic/enhancedbiomes/RealisticBiomeEBBasin.java
+++ src/main/java/rtg/world/biome/realistic/enhancedbiomes/RealisticBiomeEBBasin.java
@@ -1,15 +1,13 @@
 package rtg.world.biome.realistic.enhancedbiomes;
 
+import rtg.config.enhancedbiomes.ConfigEB;
+import rtg.world.biome.BiomeBase;
+import rtg.world.gen.surface.enhancedbiomes.SurfaceEBBasin;
+import rtg.world.gen.terrain.enhancedbiomes.TerrainEBBasin;
 import enhancedbiomes.EnhancedBiomesMod;
 
 import net.minecraft.block.Block;
 import net.minecraft.world.biome.BiomeGenBase;
-
-import rtg.config.enhancedbiomes.ConfigEB;
-import rtg.world.biome.BiomeBase;
-import rtg.world.biome.BiomeBase.BiomeSize;
-import rtg.world.gen.surface.enhancedbiomes.SurfaceEBBasin;
-import rtg.world.gen.terrain.enhancedbiomes.TerrainEBBasin;
 
 public class RealisticBiomeEBBasin extends RealisticBiomeEBBase
 {
@@ -22,8 +20,25 @@ public class RealisticBiomeEBBasin extends RealisticBiomeEBBase
 	{
 		super(
 			ebBiome, BiomeBase.climatizedBiome(BiomeGenBase.river, Climate.TEMPERATE),
-			new TerrainEBBasin(),
-			new SurfaceEBBasin(ebBiome.topBlock, ebBiome.fillerBlock)
+			new TerrainEBBasin(false, -15f, 0f, 0f, 0f, 63f),
+			new SurfaceEBBasin(
+                getDominantEBGrass(ebBiome), //Block top 
+                getDominantEBSoilMeta(ebBiome), //byte topByte
+                getDominantEBDirt(ebBiome), //Block filler, 
+                getDominantEBSoilMeta(ebBiome), //byte fillerByte
+                getDominantEBGrass(ebBiome), //Block mixTop, 
+                getDominantEBSoilMeta(ebBiome), //byte mixTopByte, 
+                getDominantEBDirt(ebBiome), //Block mixFill, 
+                getDominantEBSoilMeta(ebBiome), //byte mixFillByte, 
+                EnhancedBiomesMod.getDominantStone(ebBiome.biomeID), //Block cliff1, 
+                EnhancedBiomesMod.getDominantStoneMeta(ebBiome.biomeID), //byte cliff1Byte, 
+                EnhancedBiomesMod.getCobbleFromStone(EnhancedBiomesMod.getDominantStone(ebBiome.biomeID)), //Block cliff2, 
+                EnhancedBiomesMod.getDominantStoneMeta(ebBiome.biomeID), //byte cliff2Byte, 
+                80f, //float mixWidth, 
+                -0.15f, //float mixHeight, 
+                10f, //float smallWidth, 
+                0.5f //float smallStrength
+            )
 		);
 		
 		this.setRealisticBiomeName("EB Basin");

534c0b8c5d62a694903fbce74eb86e82605eb764
WhichOnesPink <whichonespink@hokecraft.co.uk>
2015-12-10 21:09:15 +0000

    Re-decorated BOP Arctic.

--- src/main/java/rtg/world/biome/realistic/biomesoplenty/RealisticBiomeBOPArctic.java
+++ src/main/java/rtg/world/biome/realistic/biomesoplenty/RealisticBiomeBOPArctic.java
@@ -1,14 +1,14 @@
 package rtg.world.biome.realistic.biomesoplenty;
 
-import net.minecraft.block.Block;
-import net.minecraft.world.biome.BiomeGenBase;
-
 import rtg.config.biomesoplenty.ConfigBOP;
 import rtg.world.biome.BiomeBase;
-import rtg.world.biome.BiomeBase.BiomeSize;
 import rtg.world.gen.surface.biomesoplenty.SurfaceBOPArctic;
 import rtg.world.gen.terrain.biomesoplenty.TerrainBOPArctic;
 import biomesoplenty.api.content.BOPCBiomes;
+
+import net.minecraft.block.Block;
+import net.minecraft.init.Blocks;
+import net.minecraft.world.biome.BiomeGenBase;
 
 public class RealisticBiomeBOPArctic extends RealisticBiomeBOPBase
 {	
@@ -22,7 +22,24 @@ public class RealisticBiomeBOPArctic extends RealisticBiomeBOPBase
 		super(
 			bopBiome, BiomeBase.climatizedBiome(BiomeGenBase.frozenRiver, Climate.ICE),
 			new TerrainBOPArctic(),
-			new SurfaceBOPArctic(topBlock, fillerBlock)
+			new SurfaceBOPArctic(
+			    Blocks.snow, //Block top 
+			    (byte)0, //byte topByte
+			    fillerBlock, //Block filler, 
+                (byte)0, //byte fillerByte
+                Blocks.snow, //Block mixTop, 
+                (byte)0, //byte mixTopByte, 
+                fillerBlock, //Block mixFill, 
+                (byte)0, //byte mixFillByte, 
+                Blocks.stone, //Block cliff1, 
+                (byte)0, //byte cliff1Byte, 
+                Blocks.cobblestone, //Block cliff2, 
+                (byte)0, //byte cliff2Byte, 
+                80f, //float mixWidth, 
+                -0.15f, //float mixHeight, 
+                10f, //float smallWidth, 
+                0.5f //float smallStrength
+            )
 		);
 		
 		this.setRealisticBiomeName("BOP Arctic");

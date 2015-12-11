fb1b2e2abf005891b81551e57d0aee63a3211645
WhichOnesPink <whichonespink@hokecraft.co.uk>
2015-12-11 22:24:41 +0000

    Improved BOP Frost Forest.

--- src/main/java/rtg/world/biome/realistic/biomesoplenty/RealisticBiomeBOPFrostForest.java
+++ src/main/java/rtg/world/biome/realistic/biomesoplenty/RealisticBiomeBOPFrostForest.java
@@ -1,14 +1,22 @@
 package rtg.world.biome.realistic.biomesoplenty;
 
-import net.minecraft.block.Block;
-import net.minecraft.init.Blocks;
-import net.minecraft.world.biome.BiomeGenBase;
+import java.util.Random;
 
 import rtg.config.biomesoplenty.ConfigBOP;
+import rtg.util.CellNoise;
+import rtg.util.OpenSimplexNoise;
 import rtg.world.biome.BiomeBase;
+import rtg.world.biome.realistic.RealisticBiomeBase;
+import rtg.world.gen.feature.WorldGenLog;
 import rtg.world.gen.surface.biomesoplenty.SurfaceBOPFrostForest;
 import rtg.world.gen.terrain.biomesoplenty.TerrainBOPFrostForest;
 import biomesoplenty.api.content.BOPCBiomes;
+
+import net.minecraft.block.Block;
+import net.minecraft.init.Blocks;
+import net.minecraft.world.World;
+import net.minecraft.world.biome.BiomeGenBase;
+import net.minecraft.world.gen.feature.WorldGenBlockBlob;
 
 public class RealisticBiomeBOPFrostForest extends RealisticBiomeBOPBase
 {	
@@ -21,8 +29,25 @@ public class RealisticBiomeBOPFrostForest extends RealisticBiomeBOPBase
 	{
 		super(
 			bopBiome, BiomeBase.climatizedBiome(BiomeGenBase.frozenRiver, Climate.ICE),
-			new TerrainBOPFrostForest(0f, 140f, 68f, 200f),
-			new SurfaceBOPFrostForest(topBlock, fillerBlock, Blocks.stone, Blocks.cobblestone)
+			new TerrainBOPFrostForest(63f, 69f, 28f),
+			new SurfaceBOPFrostForest(
+                topBlock, //Block top 
+                (byte)0, //byte topByte
+                fillerBlock, //Block filler, 
+                (byte)0, //byte fillerByte
+                topBlock, //Block mixTop, 
+                (byte)0, //byte mixTopByte, 
+                fillerBlock, //Block mixFill, 
+                (byte)0, //byte mixFillByte, 
+                Blocks.stone, //Block cliff1, 
+                (byte)0, //byte cliff1Byte, 
+                Blocks.cobblestone, //Block cliff2, 
+                (byte)0, //byte cliff2Byte, 
+                80f, //float mixWidth, 
+                -0.15f, //float mixHeight, 
+                10f, //float smallWidth, 
+                0.5f //float smallStrength
+            )
 		);
 		
 		this.setRealisticBiomeName("BOP Frost Forest");
@@ -30,4 +55,46 @@ public class RealisticBiomeBOPFrostForest extends RealisticBiomeBOPBase
 		this.biomeWeight = ConfigBOP.weightBOPFrostForest;
 		this.generateVillages = ConfigBOP.villageBOPFrostForest;
 	}
+	
+    @Override
+    public void rDecorate(World world, Random rand, int chunkX, int chunkY, OpenSimplexNoise simplex, CellNoise cell, float strength,
+        float river)
+    {
+
+        float l = simplex.noise2(chunkX / 100f, chunkY / 100f) * 6f + 0.8f;
+        
+        for (int i23 = 0; i23 < 1; i23++)
+        {
+            int i1 = chunkX + rand.nextInt(16) + 8;
+            int j1 = chunkY + rand.nextInt(16) + 8;
+            int k1 = world.getHeightValue(i1, j1);
+            
+            if (rand.nextInt(14) == 0) {
+                
+                if (rand.nextBoolean()) {
+                    (new WorldGenBlockBlob(Blocks.cobblestone, 0)).generate(world, rand, i1, k1, j1);
+                }
+                else {
+                    (new WorldGenBlockBlob(Blocks.mossy_cobblestone, 0)).generate(world, rand, i1, k1, j1);
+                }
+            }
+        }
+
+        if (l > 0f && rand.nextInt(12) == 0)
+        {
+            int x22 = chunkX + rand.nextInt(16) + 8;
+            int z22 = chunkY + rand.nextInt(16) + 8;
+            int y22 = world.getHeightValue(x22, z22);
+            
+            Block log;
+            byte logMeta;
+            
+            log = Blocks.log;
+            logMeta = (byte)0;
+            
+            (new WorldGenLog(log, logMeta, Blocks.leaves, -1, 3 + rand.nextInt(3))).generate(world, rand, x22, y22, z22);            
+        }
+        
+        RealisticBiomeBase.rDecorateSeedBiome(world, rand, chunkX, chunkY, simplex, cell, strength, river, baseBiome);
+    }
 }
--- src/main/java/rtg/world/gen/terrain/biomesoplenty/TerrainBOPFrostForest.java
+++ src/main/java/rtg/world/gen/terrain/biomesoplenty/TerrainBOPFrostForest.java
@@ -6,35 +6,43 @@ import rtg.world.gen.terrain.TerrainBase;
 
 public class TerrainBOPFrostForest extends TerrainBase
 {
-	private float start;
-	private float height;
-	private float base;
-	private float width;
-	
-	public TerrainBOPFrostForest(float hillStart, float landHeight, float baseHeight, float hillWidth)
-	{
-		start = hillStart;
-		height = landHeight;
-		base = baseHeight;
-		width = hillWidth;
-	}
-	
-	@Override
-	public float generateNoise(OpenSimplexNoise simplex, CellNoise cell, int x, int y, float border, float river)
-	{
-		float h = simplex.noise2(x / width, y / width) * height * river;
-		h = h < start ? start + ((h - start) / 4.5f) : h;
-		
-		if(h > 0f)
-		{
-			float st = h * 1.5f > 15f ? 15f : h * 1.5f;
-			h += cell.noise(x / 70D, y / 70D, 1D) * st;
-		}
-		
-		h += simplex.noise2(x / 20f, y / 20f) * 5f;
-		h += simplex.noise2(x / 12f, y / 12f) * 3f;
-		h += simplex.noise2(x / 5f, y / 5f) * 1.5f;
-		
-    	return base + h;
-	}
+    private float minHeight = 63f;
+    private float maxHeight = 80f;
+    private float hillStrength = 30f;
+    
+    public TerrainBOPFrostForest()
+    {
+    
+    }
+    
+    public TerrainBOPFrostForest(float minHeight, float maxHeight, float hillStrength)
+    {
+        this.minHeight = minHeight;
+        this.maxHeight = maxHeight;
+        this.hillStrength = hillStrength;
+    }
+    
+    @Override
+    public float generateNoise(OpenSimplexNoise simplex, CellNoise cell, int x, int y, float border, float river)
+    {
+    
+        float h = simplex.noise2(x / 200f, y / 200f) * 4;
+        h += simplex.noise2(x / 100f, y / 100f) * 2;
+        
+        float m = simplex.noise2(x / 200f, y / 200f) * hillStrength * river;
+        m *= m / ((hillStrength * 0.1f) + hillStrength);
+        
+        float sm = simplex.noise2(x / hillStrength, y / hillStrength) * 8f;
+        sm *= m / 20f > 3.75f ? 3.75f : m / 20f;
+        m += sm;
+        
+        float l = simplex.noise2(x / 260f, y / 260f) * 38f;
+        l *= l / 25f;
+        l = l < -8f ? -8f : l;
+        
+        float floNoise = maxHeight + h + m - l;
+        floNoise = (floNoise < minHeight) ? minHeight : floNoise;
+        
+        return floNoise;
+    }
 }

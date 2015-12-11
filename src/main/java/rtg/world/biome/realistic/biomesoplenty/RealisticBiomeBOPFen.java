201991261c403543214ca0a1e30a718f3466d4f8
WhichOnesPink <whichonespink@hokecraft.co.uk>
2015-12-11 20:43:13 +0000

    Improved BOP Fen.

--- src/main/java/rtg/world/biome/realistic/biomesoplenty/RealisticBiomeBOPFen.java
+++ src/main/java/rtg/world/biome/realistic/biomesoplenty/RealisticBiomeBOPFen.java
@@ -1,14 +1,23 @@
 package rtg.world.biome.realistic.biomesoplenty;
 
-import net.minecraft.block.Block;
-import net.minecraft.init.Blocks;
-import net.minecraft.world.biome.BiomeGenBase;
+import java.util.Random;
 
 import rtg.config.biomesoplenty.ConfigBOP;
+import rtg.util.CellNoise;
+import rtg.util.OpenSimplexNoise;
 import rtg.world.biome.BiomeBase;
+import rtg.world.biome.realistic.RealisticBiomeBase;
+import rtg.world.gen.feature.WorldGenLog;
 import rtg.world.gen.surface.biomesoplenty.SurfaceBOPFen;
 import rtg.world.gen.terrain.biomesoplenty.TerrainBOPFen;
 import biomesoplenty.api.content.BOPCBiomes;
+import biomesoplenty.api.content.BOPCBlocks;
+
+import net.minecraft.block.Block;
+import net.minecraft.init.Blocks;
+import net.minecraft.world.World;
+import net.minecraft.world.biome.BiomeGenBase;
+import net.minecraft.world.gen.feature.WorldGenBlockBlob;
 
 public class RealisticBiomeBOPFen extends RealisticBiomeBOPBase
 {	
@@ -20,9 +29,26 @@ public class RealisticBiomeBOPFen extends RealisticBiomeBOPBase
 	public RealisticBiomeBOPFen()
 	{
 		super(
-			bopBiome, BiomeBase.climatizedBiome(BiomeGenBase.river, Climate.HOT),
-			new TerrainBOPFen(0f, 140f, 68f, 200f),
-			new SurfaceBOPFen(topBlock, fillerBlock, Blocks.stone, Blocks.cobblestone)
+			bopBiome, BiomeBase.climatizedBiome(BiomeGenBase.river, Climate.TEMPERATE),
+			new TerrainBOPFen(),
+			new SurfaceBOPFen(
+                topBlock, //Block top 
+                (byte)0, //byte topByte
+                fillerBlock, //Block filler, 
+                (byte)0, //byte fillerByte
+                topBlock, //Block mixTop, 
+                (byte)0, //byte mixTopByte, 
+                fillerBlock, //Block mixFill, 
+                (byte)0, //byte mixFillByte, 
+                Blocks.stone, //Block cliff1, 
+                (byte)0, //byte cliff1Byte, 
+                Blocks.cobblestone, //Block cliff2, 
+                (byte)0, //byte cliff2Byte, 
+                80f, //float mixWidth, 
+                -0.15f, //float mixHeight, 
+                10f, //float smallWidth, 
+                0.5f //float smallStrength
+            )
 		);
 		
 		this.setRealisticBiomeName("BOP Fen");
@@ -30,4 +56,52 @@ public class RealisticBiomeBOPFen extends RealisticBiomeBOPBase
 		this.biomeWeight = ConfigBOP.weightBOPFen;
 		this.generateVillages = ConfigBOP.villageBOPFen;
 	}
+	
+    @Override
+    public void rDecorate(World world, Random rand, int chunkX, int chunkY, OpenSimplexNoise simplex, CellNoise cell, float strength,
+        float river)
+    {
+
+        float l = simplex.noise2(chunkX / 100f, chunkY / 100f) * 6f + 0.8f;
+        
+        for (int i23 = 0; i23 < 1; i23++)
+        {
+            int i1 = chunkX + rand.nextInt(16) + 8;
+            int j1 = chunkY + rand.nextInt(16) + 8;
+            int k1 = world.getHeightValue(i1, j1);
+            
+            if (rand.nextInt(12) == 0) {
+                
+                if (rand.nextBoolean()) {
+                    (new WorldGenBlockBlob(Blocks.cobblestone, 0)).generate(world, rand, i1, k1, j1);
+                }
+                else {
+                    (new WorldGenBlockBlob(Blocks.mossy_cobblestone, 0)).generate(world, rand, i1, k1, j1);
+                }
+            }
+        }
+
+        if (l > 0f && rand.nextInt(16) == 0)
+        {
+            int x22 = chunkX + rand.nextInt(16) + 8;
+            int z22 = chunkY + rand.nextInt(16) + 8;
+            int y22 = world.getHeightValue(x22, z22);
+            
+            Block log;
+            byte logMeta;
+
+            if (rand.nextInt(4) == 0) {
+                log = BOPCBlocks.logs3;
+                logMeta = (byte)2;
+            }
+            else {
+                log = Blocks.log2;
+                logMeta = (byte)1;
+            }
+            
+            (new WorldGenLog(log, logMeta, Blocks.leaves, -1, 3 + rand.nextInt(3))).generate(world, rand, x22, y22, z22);            
+        }
+        
+        RealisticBiomeBase.rDecorateSeedBiome(world, rand, chunkX, chunkY, simplex, cell, strength, river, baseBiome);
+    }
 }

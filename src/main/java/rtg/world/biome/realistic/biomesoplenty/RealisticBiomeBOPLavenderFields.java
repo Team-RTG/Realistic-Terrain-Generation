bf74df1a32dadb8324535a2ca8d7d75934595226
WhichOnesPink <whichonespink@hokecraft.co.uk>
2015-12-12 10:23:23 +0000

    Improved BOP Lavender Fields.

--- src/main/java/rtg/world/biome/realistic/biomesoplenty/RealisticBiomeBOPLavenderFields.java
+++ src/main/java/rtg/world/biome/realistic/biomesoplenty/RealisticBiomeBOPLavenderFields.java
@@ -1,13 +1,24 @@
 package rtg.world.biome.realistic.biomesoplenty;
 
-import net.minecraft.block.Block;
-import net.minecraft.world.biome.BiomeGenBase;
+import java.util.Random;
 
 import rtg.config.biomesoplenty.ConfigBOP;
+import rtg.util.CellNoise;
+import rtg.util.OpenSimplexNoise;
 import rtg.world.biome.BiomeBase;
+import rtg.world.biome.realistic.RealisticBiomeBase;
+import rtg.world.gen.feature.WorldGenGrass;
+import rtg.world.gen.feature.tree.WorldGenTreeShrub;
 import rtg.world.gen.surface.biomesoplenty.SurfaceBOPLavenderFields;
 import rtg.world.gen.terrain.biomesoplenty.TerrainBOPLavenderFields;
 import biomesoplenty.api.content.BOPCBiomes;
+import biomesoplenty.api.content.BOPCBlocks;
+import biomesoplenty.common.world.features.trees.WorldGenBOPShrub;
+
+import net.minecraft.block.Block;
+import net.minecraft.init.Blocks;
+import net.minecraft.world.World;
+import net.minecraft.world.biome.BiomeGenBase;
 
 public class RealisticBiomeBOPLavenderFields extends RealisticBiomeBOPBase
 {	
@@ -21,7 +32,7 @@ public class RealisticBiomeBOPLavenderFields extends RealisticBiomeBOPBase
 		super(
 			bopBiome, BiomeBase.climatizedBiome(BiomeGenBase.river, Climate.TEMPERATE),
 			new TerrainBOPLavenderFields(),
-			new SurfaceBOPLavenderFields(topBlock, fillerBlock, false, null, 1.2f)
+			new SurfaceBOPLavenderFields(topBlock, fillerBlock, false, null, 0f, 1.5f, 60f, 65f, 1.5f, topBlock, 0.05f)
 		);
 		
 		this.setRealisticBiomeName("BOP Lavender Fields");
@@ -29,4 +40,54 @@ public class RealisticBiomeBOPLavenderFields extends RealisticBiomeBOPBase
 		this.biomeWeight = ConfigBOP.weightBOPLavenderFields;
 		this.generateVillages = ConfigBOP.villageBOPLavenderFields;
 	}
+	
+    @Override
+    public void rDecorate(World world, Random rand, int chunkX, int chunkY, OpenSimplexNoise simplex, CellNoise cell, float strength,
+        float river)
+    {
+    
+//        for (int f23 = 0; f23 < 16f * strength; f23++)
+//        {
+//            int j15 = chunkX + rand.nextInt(16) + 8;
+//            int j20 = chunkY + rand.nextInt(16) + 8;
+//            int j17 = world.getHeightValue(j15, j20);
+//            
+//            (new WorldGenFlowers(new int[] {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 14, 15})).generate(world, rand, j15, j17, j20);
+//        }
+
+        float l = simplex.noise2(chunkX / 80f, chunkY / 80f) * 60f - 15f;
+        
+        for (int f24 = 0; f24 < 4f * strength; f24++)
+        {
+            if (rand.nextInt(10) == 0) {
+                
+                int i1 = chunkX + rand.nextInt(16) + 8;
+                int j1 = chunkY + rand.nextInt(16) + 8;
+                int k1 = world.getHeightValue(i1, j1);
+                
+                if (k1 < 110)
+                {
+                    if (rand.nextBoolean()) {
+                        
+                        (new WorldGenTreeShrub(rand.nextInt(4) + 1, 0, rand.nextInt(3))).generate(world, rand, i1, k1, j1);
+                    }
+                    else {
+                        
+                        (new WorldGenBOPShrub(BOPCBlocks.logs4, BOPCBlocks.leaves4, 2, 1, new Block[] { Blocks.grass })).generate(world, rand, i1, k1, j1);
+                        
+                    }
+                }
+            }
+        }
+        
+        for (int l14 = 0; l14 < 6f * strength; l14++)
+        {
+            int l19 = chunkX + rand.nextInt(16) + 8;
+            int k22 = rand.nextInt(128);
+            int j24 = chunkY + rand.nextInt(16) + 8;
+            (new WorldGenGrass(Blocks.tallgrass, 1)).generate(world, rand, l19, k22, j24);
+        }
+        
+        RealisticBiomeBase.rDecorateSeedBiome(world, rand, chunkX, chunkY, simplex, cell, strength, river, baseBiome);
+    }
 }

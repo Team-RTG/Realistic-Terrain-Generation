6bde727170ba522060dc08a168d6a24d18c196e1
WhichOnesPink <whichonespink@hokecraft.co.uk>
2015-12-12 00:28:00 +0000

    Improved BOP Glacier.

--- src/main/java/rtg/world/gen/terrain/biomesoplenty/TerrainBOPGlacier.java
+++ src/main/java/rtg/world/gen/terrain/biomesoplenty/TerrainBOPGlacier.java
@@ -6,34 +6,60 @@ import rtg.world.gen.terrain.TerrainBase;
 
 public class TerrainBOPGlacier extends TerrainBase
 {
-	public TerrainBOPGlacier()
-	{
-	}
-	
-	@Override
-	public float generateNoise(OpenSimplexNoise simplex, CellNoise cell, int x, int y, float border, float river)
-	{
-		float b = (12f + (simplex.noise2(x / 300f, y / 300f) * 6f));
-		float h = cell.noise(x / 200D, y / 200D, 1D) * b * river;
-		h *= h * 1.5f;
-		h = h > 155f ? 155f : h;
-		
-		if(h > 2f)
-		{
-			float d = (h - 2f) / 2f > 8f ? 8f : (h - 2f) / 2f;
-			h += simplex.noise2(x / 30f, y / 30f) * d;
-			h += simplex.noise2(x / 50f, y / 50f) * d * 0.5f;
-
-			if(h > 35f)
-			{
-				float d2 = (h - 35f) / 1.5f > 30f ? 30f : (h - 35f) / 1.5f;
-				h += cell.noise(x / 25D, y / 25D, 1D) * d2;
-			}
-		}
-		
-		h += simplex.noise2(x / 18f, y / 18f) * 3;
-		h += simplex.noise2(x / 8f, y / 8f) * 2;
-				
-		return 45f + h + (b * 2);
-	}
+    
+    private float width;
+    private float strength;
+    private float lakeDepth;
+    private float lakeWidth;
+    private float terrainHeight;
+    
+    /*
+     * width = 230f strength = 120f lake = 50f;
+     * 
+     * 230f, 120f, 50f
+     */
+    
+    public TerrainBOPGlacier(float mountainWidth, float mountainStrength, float depthLake)
+    {
+    
+        this(mountainWidth, mountainStrength, depthLake, 260f, 68f);
+    }
+    
+    public TerrainBOPGlacier(float mountainWidth, float mountainStrength, float depthLake, float widthLake, float height)
+    {
+    
+        width = mountainWidth;
+        strength = mountainStrength;
+        lakeDepth = depthLake;
+        lakeWidth = widthLake;
+        terrainHeight = height;
+    }
+    
+    @Override
+    public float generateNoise(OpenSimplexNoise simplex, CellNoise cell, int x, int y, float border, float river)
+    {
+    
+        float h = simplex.noise2(x / 20f, y / 20f) * 2;
+        h += simplex.noise2(x / 7f, y / 7f) * 0.8f;
+        
+        float m = simplex.noise2(x / width, y / width) * strength * river;
+        m *= m / 35f;
+        m = m > 70f ? 70f + (m - 70f) / 2.5f : m;
+        
+        float st = m * 0.7f;
+        st = st > 20f ? 20f : st;
+        float c = cell.noise(x / 30f, y / 30f, 1D) * (5f + st);
+        
+        float sm = simplex.noise2(x / 30f, y / 30f) * 8f + simplex.noise2(x / 8f, y / 8f);
+        sm *= (m + 10f) / 20f > 2.5f ? 2.5f : (m + 10f) / 20f;
+        m += sm;
+        
+        m += c;
+        
+        float l = simplex.noise2(x / lakeWidth, y / lakeWidth) * lakeDepth;
+        l *= l / 25f;
+        l = l < -8f ? -8f : l;
+        
+        return terrainHeight + h + m - l;
+    }
 }

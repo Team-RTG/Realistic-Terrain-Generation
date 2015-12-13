2280f0b7ce0067ec1fea0e28894e92f316edf10d
WhichOnesPink <whichonespink@hokecraft.co.uk>
2015-12-13 13:01:47 +0000

    Improved BOP Tundra.

--- src/main/java/rtg/world/biome/realistic/biomesoplenty/RealisticBiomeBOPTundra.java
+++ src/main/java/rtg/world/biome/realistic/biomesoplenty/RealisticBiomeBOPTundra.java
@@ -20,8 +20,8 @@ public class RealisticBiomeBOPTundra extends RealisticBiomeBOPBase
 	public RealisticBiomeBOPTundra()
 	{
 		super(
-			bopBiome, BiomeBase.climatizedBiome(BiomeGenBase.frozenRiver, Climate.ICE),
-			new TerrainBOPTundra(90f, 180f, 13f, 100f, 38f, 260f, 71f),
+			bopBiome, BiomeBase.climatizedBiome(BiomeGenBase.river, Climate.COLD),
+			new TerrainBOPTundra(),
 			new SurfaceBOPTundra(topBlock, fillerBlock, Blocks.stone, Blocks.cobblestone)
 		);
 		

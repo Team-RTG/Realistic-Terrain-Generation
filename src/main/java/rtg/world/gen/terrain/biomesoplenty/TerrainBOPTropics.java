4c3c71daaf488a6f28221bff50f0f6ef05e0db60
WhichOnesPink <whichonespink@hokecraft.co.uk>
2015-12-13 13:01:25 +0000

    Improved BOP Tropics.

--- src/main/java/rtg/world/gen/terrain/biomesoplenty/TerrainBOPTropics.java
+++ src/main/java/rtg/world/gen/terrain/biomesoplenty/TerrainBOPTropics.java
@@ -6,35 +6,26 @@ import rtg.world.gen.terrain.TerrainBase;
 
 public class TerrainBOPTropics extends TerrainBase
 {
-	private float start;
-	private float height;
-	private float base;
-	private float width;
-	
-	public TerrainBOPTropics(float hillStart, float landHeight, float baseHeight, float hillWidth)
+	public TerrainBOPTropics()
 	{
-		start = hillStart;
-		height = landHeight;
-		base = baseHeight;
-		width = hillWidth;
 	}
 	
 	@Override
 	public float generateNoise(OpenSimplexNoise simplex, CellNoise cell, int x, int y, float border, float river)
 	{
-		float h = simplex.noise2(x / width, y / width) * height * river;
-		h = h < start ? start + ((h - start) / 4.5f) : h;
+		float h = simplex.noise2(x / 160f, y / 160f) * 40f * river;
+		h *= h / 30f;
 		
-		if(h > 0f)
+		if(h < 1f)
 		{
-			float st = h * 1.5f > 15f ? 15f : h * 1.5f;
-			h += cell.noise(x / 70D, y / 70D, 1D) * st;
+			h = 1f;
 		}
 		
-		h += simplex.noise2(x / 20f, y / 20f) * 5f;
-		h += simplex.noise2(x / 12f, y / 12f) * 3f;
-		h += simplex.noise2(x / 5f, y / 5f) * 1.5f;
+		if(h < 4f)
+		{
+			h += (simplex.noise2(x / 50f, y / 50f) + simplex.noise2(x / 15f, y / 15f)) * (4f - h);
+		}
 		
-    	return base + h;
+		return 60f + h;
 	}
 }

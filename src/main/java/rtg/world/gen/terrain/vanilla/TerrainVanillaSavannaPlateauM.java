bfcfbb4f517873945996cf2fd292d1a95fd967ec
WhichOnesPink <whichonespink@hokecraft.co.uk>
2015-12-13 20:00:51 +0000

    Improved vanilla Savanna Plateau M.

--- src/main/java/rtg/world/gen/terrain/vanilla/TerrainVanillaSavannaPlateauM.java
+++ src/main/java/rtg/world/gen/terrain/vanilla/TerrainVanillaSavannaPlateauM.java
@@ -6,35 +6,104 @@ import rtg.world.gen.terrain.TerrainBase;
 
 public class TerrainVanillaSavannaPlateauM extends TerrainBase
 {
-	private float start;
-	private float height;
+	private boolean booRiver;
+	private float[] height;
+	private int heightLength;
+	private float strength;
+	private float cWidth;
+	private float cHeigth;
+	private float cStrength;
 	private float base;
-	private float width;
 	
-	public TerrainVanillaSavannaPlateauM(float hillStart, float landHeight, float baseHeight, float hillWidth)
+	/*
+	 * Example parameters:
+	 * 
+	 * allowed to generate rivers?
+	 * riverGen = true
+	 * 
+	 * canyon jump heights
+	 * heightArray = new float[]{2.0f, 0.5f, 6.5f, 0.5f, 14.0f, 0.5f, 19.0f, 0.5f}
+	 * 
+	 * strength of canyon jump heights
+	 * heightStrength = 35f
+	 * 
+	 * canyon width (cliff to cliff)
+	 * canyonWidth = 160f
+	 * 
+	 * canyon heigth (total heigth)
+	 * canyonHeight = 60f
+	 * 
+	 * canyon strength
+	 * canyonStrength = 40f
+	 * 
+	 */
+	public TerrainVanillaSavannaPlateauM(boolean riverGen, float heightStrength, float canyonWidth, float canyonHeight, float canyonStrength, float baseHeight)
 	{
-		start = hillStart;
-		height = landHeight;
+		booRiver = riverGen;
+		height = new float[]{12.5f, 0.5f, 18.5f, 0.5f};
+		strength = heightStrength;
+		heightLength = height.length;
+		cWidth = canyonWidth;
+		cHeigth = canyonHeight;
+		cStrength = canyonStrength;
 		base = baseHeight;
-		width = hillWidth;
 	}
-	
+
 	@Override
 	public float generateNoise(OpenSimplexNoise simplex, CellNoise cell, int x, int y, float border, float river)
 	{
-		float h = simplex.noise2(x / width, y / width) * height * river;
-		h = h < start ? start + ((h - start) / 4.5f) : h;
+		//float b = simplex.noise2(x / cWidth, y / cWidth) * cHeigth * river;
+		//b *= b / cStrength;
+		river *= 1.3f;
+		river = river > 1f ? 1f : river;
+		float r = simplex.noise2(x / 100f, y / 100f) * 50f;
+		r = r < -7.4f ? -7.4f : r > 7.4f ? 7.4f : r;
+		float b = (17f + r) * river;
 		
-		if(h > 0f)
+		float hn = simplex.noise2(x / 12f, y / 12f) * 0.5f;
+		float sb = 0f;
+		if(b > 0f)
 		{
-			float st = h * 1.5f > 15f ? 15f : h * 1.5f;
-			h += cell.noise(x / 70D, y / 70D, 1D) * st;
+			sb = b;
+			sb = sb < 0f ? 0f : sb > 7f ? 7f : sb;
+			sb = hn * sb;
+		}
+		b += sb;
+
+		float cTotal = 0f;
+		float cTemp = 0f;
+		
+		for(int i = 0; i < heightLength; i += 2)
+		{
+			cTemp = 0;
+			if(b > height[i] && border > 0.6f + (height[i] * 0.015f) + hn * 0.2f)
+			{
+				cTemp = b > height[i] + height[i + 1] ? height[i + 1] : b - height[i];
+				cTemp *= strength;
+			}
+			cTotal += cTemp;
 		}
 		
-		h += simplex.noise2(x / 20f, y / 20f) * 5f;
-		h += simplex.noise2(x / 12f, y / 12f) * 3f;
-		h += simplex.noise2(x / 5f, y / 5f) * 1.5f;
 		
-    	return base + h;
+		float bn = 0f;
+		if(booRiver)
+		{
+			if(b < 5f)
+			{
+				bn = 5f - b;
+				for(int i = 0; i < 3; i++)
+				{
+					bn *= bn / 4.5f;
+				}
+			}
+		}
+		else if(b < 5f)
+		{
+			bn = (simplex.noise2(x / 7f, y / 7f) * 1.3f + simplex.noise2(x / 15f, y / 15f) * 2f) * (5f - b) * 0.2f;
+		}
+		
+		b += cTotal - bn;
+		
+		return base + b;
 	}
 }

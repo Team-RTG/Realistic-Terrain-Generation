fa68a058ef0047d62ea0199d4c12590133de1f7b
WhichOnesPink <whichonespink@hokecraft.co.uk>
2015-12-13 12:59:06 +0000

    Improved BOP Shield.

--- src/main/java/rtg/world/biome/realistic/biomesoplenty/RealisticBiomeBOPShield.java
+++ src/main/java/rtg/world/biome/realistic/biomesoplenty/RealisticBiomeBOPShield.java
@@ -1,14 +1,23 @@
 package rtg.world.biome.realistic.biomesoplenty;
 
-import net.minecraft.block.Block;
-import net.minecraft.init.Blocks;
-import net.minecraft.world.biome.BiomeGenBase;
+import java.util.Random;
 
 import rtg.config.biomesoplenty.ConfigBOP;
+import rtg.util.CellNoise;
+import rtg.util.OpenSimplexNoise;
 import rtg.world.biome.BiomeBase;
+import rtg.world.biome.realistic.RealisticBiomeBase;
+import rtg.world.gen.feature.WorldGenLog;
 import rtg.world.gen.surface.biomesoplenty.SurfaceBOPShield;
 import rtg.world.gen.terrain.biomesoplenty.TerrainBOPShield;
 import biomesoplenty.api.content.BOPCBiomes;
+import biomesoplenty.api.content.BOPCBlocks;
+
+import net.minecraft.block.Block;
+import net.minecraft.init.Blocks;
+import net.minecraft.world.World;
+import net.minecraft.world.biome.BiomeGenBase;
+import net.minecraft.world.gen.feature.WorldGenBlockBlob;
 
 public class RealisticBiomeBOPShield extends RealisticBiomeBOPBase
 {	
@@ -21,7 +30,7 @@ public class RealisticBiomeBOPShield extends RealisticBiomeBOPBase
 	{
 		super(
 			bopBiome, BiomeBase.climatizedBiome(BiomeGenBase.river, Climate.COLD),
-			new TerrainBOPShield(90f, 180f, 13f, 100f, 38f, 260f, 71f),
+			new TerrainBOPShield(0f, 100f, 68f, 170f),
 			new SurfaceBOPShield(topBlock, fillerBlock, Blocks.stone, Blocks.cobblestone)
 		);
 		
@@ -30,4 +39,51 @@ public class RealisticBiomeBOPShield extends RealisticBiomeBOPBase
 		this.biomeWeight = ConfigBOP.weightBOPShield;
 		this.generateVillages = ConfigBOP.villageBOPShield;
 	}
+	
+    @Override
+    public void rDecorate(World world, Random rand, int chunkX, int chunkY, OpenSimplexNoise simplex, CellNoise cell, float strength,
+        float river)
+    {
+    
+        RealisticBiomeBase.rDecorateSeedBiome(world, rand, chunkX, chunkY, simplex, cell, strength, river, baseBiome);
+        
+        float l = simplex.noise2(chunkX / 80f, chunkY / 80f) * 60f - 15f;
+        
+        for (int i23 = 0; i23 < 1; i23++)
+        {
+            int i1 = chunkX + rand.nextInt(16) + 8;
+            int j1 = chunkY + rand.nextInt(16) + 8;
+            int k1 = world.getHeightValue(i1, j1);
+            
+            if (rand.nextInt(16) == 0) {
+                (new WorldGenBlockBlob(Blocks.cobblestone, 0)).generate(world, rand, i1, k1, j1);
+            }
+        }
+
+        if (rand.nextInt(6) == 0)
+        {
+            int x22 = chunkX + rand.nextInt(16) + 8;
+            int z22 = chunkY + rand.nextInt(16) + 8;
+            int y22 = world.getHeightValue(x22, z22);
+            
+            Block log;
+            byte logMeta;
+            int intLogLength;
+            
+            if (rand.nextBoolean()) {
+                
+                log = BOPCBlocks.logs4;
+                logMeta = (byte)0;
+                intLogLength = 3 + rand.nextInt(2);
+            }
+            else {
+                
+                log = Blocks.log;
+                logMeta = (byte)1;
+                intLogLength = 3 + rand.nextInt(2);
+            }
+
+            (new WorldGenLog(log, logMeta, Blocks.leaves, -1, intLogLength)).generate(world, rand, x22, y22, z22);            
+        }
+    }
 }

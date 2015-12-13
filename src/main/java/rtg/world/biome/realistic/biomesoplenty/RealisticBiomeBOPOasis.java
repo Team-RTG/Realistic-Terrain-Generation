150b1c80fecfa470596f158b246ffc0fea6b2311
WhichOnesPink <whichonespink@hokecraft.co.uk>
2015-12-13 12:53:55 +0000

    Improved BOP Oasis.

--- src/main/java/rtg/world/biome/realistic/biomesoplenty/RealisticBiomeBOPOasis.java
+++ src/main/java/rtg/world/biome/realistic/biomesoplenty/RealisticBiomeBOPOasis.java
@@ -1,14 +1,22 @@
 package rtg.world.biome.realistic.biomesoplenty;
 
-import net.minecraft.block.Block;
-import net.minecraft.init.Blocks;
-import net.minecraft.world.biome.BiomeGenBase;
+import java.util.Random;
 
 import rtg.config.biomesoplenty.ConfigBOP;
+import rtg.util.CellNoise;
+import rtg.util.OpenSimplexNoise;
 import rtg.world.biome.BiomeBase;
+import rtg.world.biome.realistic.RealisticBiomeBase;
+import rtg.world.gen.feature.WorldGenLog;
 import rtg.world.gen.surface.biomesoplenty.SurfaceBOPOasis;
 import rtg.world.gen.terrain.biomesoplenty.TerrainBOPOasis;
 import biomesoplenty.api.content.BOPCBiomes;
+import biomesoplenty.api.content.BOPCBlocks;
+
+import net.minecraft.block.Block;
+import net.minecraft.init.Blocks;
+import net.minecraft.world.World;
+import net.minecraft.world.biome.BiomeGenBase;
 
 public class RealisticBiomeBOPOasis extends RealisticBiomeBOPBase
 {	
@@ -22,7 +30,24 @@ public class RealisticBiomeBOPOasis extends RealisticBiomeBOPBase
 		super(
 			bopBiome, BiomeBase.climatizedBiome(BiomeGenBase.river, Climate.OASIS),
 			new TerrainBOPOasis(),
-			new SurfaceBOPOasis(topBlock, fillerBlock, Blocks.stone, Blocks.cobblestone)
+			new SurfaceBOPOasis(
+                topBlock, //Block top 
+                (byte)0, //byte topByte
+                fillerBlock, //Block filler, 
+                (byte)0, //byte fillerByte
+                Blocks.sand, //Block mixTop, 
+                (byte)0, //byte mixTopByte, 
+                Blocks.sandstone, //Block mixFill, 
+                (byte)0, //byte mixFillByte, 
+                Blocks.stone, //Block cliff1, 
+                (byte)0, //byte cliff1Byte, 
+                Blocks.cobblestone, //Block cliff2, 
+                (byte)0, //byte cliff2Byte, 
+                40f, //float mixWidth, 
+                -0.15f, //float mixHeight, 
+                10f, //float smallWidth, 
+                0.5f //float smallStrength
+            )
 		);
 		
 		this.setRealisticBiomeName("BOP Oasis");
@@ -30,4 +55,31 @@ public class RealisticBiomeBOPOasis extends RealisticBiomeBOPBase
 		this.biomeWeight = ConfigBOP.weightBOPOasis;
 		this.generateVillages = ConfigBOP.villageBOPOasis;
 	}
+	
+    @Override
+    public void rDecorate(World world, Random rand, int chunkX, int chunkY, OpenSimplexNoise simplex, CellNoise cell, float strength,
+        float river)
+    {
+    
+        RealisticBiomeBase.rDecorateSeedBiome(world, rand, chunkX, chunkY, simplex, cell, strength, river, baseBiome);
+        
+        float l = simplex.noise2(chunkX / 80f, chunkY / 80f) * 60f - 15f;
+
+        if (rand.nextInt(16) == 0)
+        {
+            int x22 = chunkX + rand.nextInt(16) + 8;
+            int z22 = chunkY + rand.nextInt(16) + 8;
+            int y22 = world.getHeightValue(x22, z22);
+            
+            Block log;
+            byte logMeta;
+            int intLogLength;
+
+            log = BOPCBlocks.logs2;
+            logMeta = (byte)3;
+            intLogLength = 3 + rand.nextInt(3);
+
+            (new WorldGenLog(log, logMeta, Blocks.leaves, -1, intLogLength)).generate(world, rand, x22, y22, z22);            
+        }
+    }
 }

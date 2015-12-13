959726f912992a772c0d679facb134b08d6271bc
WhichOnesPink <whichonespink@hokecraft.co.uk>
2015-12-13 18:40:22 +0000

    Reverted to old vanilla Desert Hills.
    Due to a weird issue with the RollingHills template and sand-based
    biomes, I had to revert to the old Desert Hills for now. Here's the
    problem seed for reference:
    
    -4077228124799393795

--- src/main/java/rtg/world/biome/realistic/vanilla/RealisticBiomeVanillaDesertHills.java
+++ src/main/java/rtg/world/biome/realistic/vanilla/RealisticBiomeVanillaDesertHills.java
@@ -39,7 +39,7 @@ public class RealisticBiomeVanillaDesertHills extends RealisticBiomeVanillaBase
 		super(
 			BiomeGenBase.desertHills,
 			BiomeBase.climatizedBiome(BiomeGenBase.river, Climate.OASIS),
-			new TerrainVanillaDesertHills(65f, 80f, 34f),
+			new TerrainVanillaDesertHills(600f, 50f, 0f),
 			surface
 		);
 		
--- src/main/java/rtg/world/gen/terrain/vanilla/TerrainVanillaDesertHills.java
+++ src/main/java/rtg/world/gen/terrain/vanilla/TerrainVanillaDesertHills.java
@@ -6,45 +6,58 @@ import rtg.world.gen.terrain.TerrainBase;
 
 public class TerrainVanillaDesertHills extends TerrainBase
 {
-    private float minHeight = 63f;
-    private float maxHeight = 80f;
-    private float hillStrength = 30f;
-    
-    // 63f, 80f, 30f
-    
-    public TerrainVanillaDesertHills()
-    {
-    
-    }
-    
-    public TerrainVanillaDesertHills(float minHeight, float maxHeight, float hillStrength)
-    {
-        this.minHeight = minHeight;
-        this.maxHeight = (maxHeight > 80f) ? 80f : ((maxHeight < this.minHeight) ? 80f : maxHeight);
-        this.hillStrength = hillStrength;
-    }
-    
-    @Override
-    public float generateNoise(OpenSimplexNoise simplex, CellNoise cell, int x, int y, float border, float river)
-    {
-    
-        float h = simplex.noise2(x / 200f, y / 200f) * 4;
-        h += simplex.noise2(x / 100f, y / 100f) * 2;
-        
-        float m = simplex.noise2(x / 200f, y / 200f) * hillStrength * river;
-        m *= m / ((hillStrength * 0.1f) + hillStrength);
-        
-        float sm = simplex.noise2(x / hillStrength, y / hillStrength) * 8f;
-        sm *= m / 20f > 3.75f ? 3.75f : m / 20f;
-        m += sm;
-        
-        float l = simplex.noise2(x / 260f, y / 260f) * 38f;
-        l *= l / 25f;
-        l = l < -8f ? -8f : l;
-        
-        float floNoise = maxHeight + h + m - l;
-        floNoise = (floNoise < minHeight) ? minHeight : floNoise;
-        
-        return floNoise;
-    }
+	private float width;
+	private float strength;
+	private float lakeDepth;
+	private float lakeWidth;
+	private float terrainHeight;
+	
+	/*
+	 * width = 230f
+	 * strength = 120f
+	 * lake = 50f;
+	 * 
+	 * 230f, 120f, 50f
+	 */
+
+	public TerrainVanillaDesertHills(float mountainWidth, float mountainStrength, float depthLake)
+	{
+		this(mountainWidth, mountainStrength, depthLake, 160f, 68f);
+	}
+	
+	public TerrainVanillaDesertHills(float mountainWidth, float mountainStrength, float depthLake, float widthLake, float height)
+	{
+		width = mountainWidth;
+		strength = mountainStrength;
+		lakeDepth = depthLake;
+		lakeWidth = widthLake;
+		terrainHeight = height;
+	}
+	
+	@Override
+	public float generateNoise(OpenSimplexNoise simplex, CellNoise cell, int x, int y, float border, float river)
+	{
+		float h = simplex.noise2(x / 20f, y / 20f) * 2;
+		h += simplex.noise2(x / 7f, y / 7f) * 0.8f;
+		
+		float m = simplex.noise2(x / width, y / width) * strength * river;
+		m *= m / 35f;
+		m = m > 70f ? 70f + (m - 70f) / 2.5f : m;
+		
+		float st = m * 0.7f;
+		st = st > 20f ? 20f : st;
+		float c = cell.noise(x / 30f, y / 30f, 1D) * (5f + st);
+		
+		float sm = simplex.noise2(x / 30f, y / 30f) * 8f + simplex.noise2(x / 8f, y / 8f);
+		sm *= (m + 10f) / 20f > 2.5f ? 2.5f : (m + 10f) / 20f;
+		m += sm;
+		
+		m += c;
+		
+		float l = simplex.noise2(x / lakeWidth, y / lakeWidth) * lakeDepth;
+		l *= l / 25f;
+		l = l < -8f ? -8f : l;
+		
+		return terrainHeight + h + m - l;
+	}
 }

8c0a51b9c0dd0a5e7bccab5afc52910d9f51ffbe
WhichOnesPink <whichonespink@hokecraft.co.uk>
2015-12-17 20:31:14 +0000

    Added 'dynamic hardcodes' to SurfaceBase.

--- src/main/java/rtg/world/gen/surface/SurfaceBase.java
+++ src/main/java/rtg/world/gen/surface/SurfaceBase.java
@@ -2,12 +2,17 @@ package rtg.world.gen.surface;
 
 import java.util.Random;
 
-import cpw.mods.fml.common.registry.GameData;
 import rtg.config.rtg.ConfigRTG;
 import rtg.util.CellNoise;
 import rtg.util.OpenSimplexNoise;
+import cpw.mods.fml.common.Loader;
+import cpw.mods.fml.common.registry.GameData;
+import exterminatorJeff.undergroundBiomes.api.BlockCodes;
+import exterminatorJeff.undergroundBiomes.api.UBAPIHook;
+import exterminatorJeff.undergroundBiomes.api.UBStrataColumnProvider;
 
 import net.minecraft.block.Block;
+import net.minecraft.init.Blocks;
 import net.minecraft.world.World;
 import net.minecraft.world.biome.BiomeGenBase;
 
@@ -15,6 +20,7 @@ public class SurfaceBase
 {
 	protected Block topBlock;
 	protected Block fillerBlock;
+	
     protected Block shadowStoneBlock = GameData.getBlockRegistry().getObject(ConfigRTG.shadowStoneBlockId);
     protected byte shadowStoneByte = (byte)ConfigRTG.shadowStoneBlockByte;
     protected Block shadowDesertBlock = GameData.getBlockRegistry().getObject(ConfigRTG.shadowDesertBlockId);
@@ -29,4 +35,60 @@ public class SurfaceBase
 	public void paintTerrain(Block[] blocks, byte[] metadata, int i, int j, int x, int y, int depth, World world, Random rand, OpenSimplexNoise simplex, CellNoise cell, float[] noise, float river, BiomeGenBase[] base)
 	{
 	}
+	
+    protected Block hcStone(World world, int i, int j, int x, int y, int k)
+    {
+        int worldX = i * 16;
+        int worldY = k;
+        int worldZ = j * 16;
+        
+        return Blocks.stone;
+    }
+    
+    protected byte hcStoneMeta(World world, int i, int j, int x, int y, int k)
+    {
+        int worldX = i * 16;
+        int worldY = k;
+        int worldZ = j * 16;
+        
+        return (byte)0;
+    }
+    
+    protected Block hcCobble(World world, int i, int j, int x, int y, int k)
+    {
+        if (Loader.isModLoaded("UndergroundBiomes")) {
+            
+            int worldX = i * 16;
+            int worldY = k;
+            int worldZ = j * 16;
+            
+            UBStrataColumnProvider columnProvider = UBAPIHook.ubAPIHook.dimensionalStrataColumnProvider.ubStrataColumnProvider(0);
+            BlockCodes cobble = columnProvider.strataColumn(worldX, worldZ).cobblestone(worldY);
+            
+            return cobble.block;
+        }
+        else {
+            
+            return Blocks.cobblestone;
+        }
+    }
+    
+    protected byte hcCobbleMeta(World world, int i, int j, int x, int y, int k)
+    {
+        if (Loader.isModLoaded("UndergroundBiomes")) {
+            
+            int worldX = i * 16;
+            int worldY = k;
+            int worldZ = j * 16;
+
+            UBStrataColumnProvider columnProvider = UBAPIHook.ubAPIHook.dimensionalStrataColumnProvider.ubStrataColumnProvider(0);
+            BlockCodes cobble = columnProvider.strataColumn(worldX, worldZ).cobblestone(worldY);
+            
+            return (byte) cobble.metadata;
+        }
+        else {
+            
+            return (byte)0;
+        }
+    }
 }

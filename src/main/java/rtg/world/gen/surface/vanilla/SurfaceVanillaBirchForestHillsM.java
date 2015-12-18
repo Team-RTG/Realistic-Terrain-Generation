186a4892b37224b667525e5fac8e4be4f29019ae
WhichOnesPink <whichonespink@hokecraft.co.uk>
2015-12-18 01:50:54 +0000

    De-hardcoded vanilla surface templates.

--- src/main/java/rtg/world/gen/surface/vanilla/SurfaceVanillaBirchForestHillsM.java
+++ src/main/java/rtg/world/gen/surface/vanilla/SurfaceVanillaBirchForestHillsM.java
@@ -6,6 +6,7 @@ import rtg.util.CellNoise;
 import rtg.util.CliffCalculator;
 import rtg.util.OpenSimplexNoise;
 import rtg.world.gen.surface.SurfaceBase;
+
 import net.minecraft.block.Block;
 import net.minecraft.init.Blocks;
 import net.minecraft.world.World;
@@ -13,15 +14,10 @@ import net.minecraft.world.biome.BiomeGenBase;
 
 public class SurfaceVanillaBirchForestHillsM extends SurfaceBase
 {
-    private Block cliffBlock1;
-    private Block cliffBlock2;
     
-    public SurfaceVanillaBirchForestHillsM(Block top, Block filler, Block cliff1, Block cliff2)
+    public SurfaceVanillaBirchForestHillsM(Block top, Block filler)
     {
         super(top, filler);
-        
-        cliffBlock1 = cliff1;
-        cliffBlock2 = cliff2;
     }
     
     @Override
@@ -45,11 +41,21 @@ public class SurfaceVanillaBirchForestHillsM extends SurfaceBase
                 {
                     if(depth > -1 && depth < 2)
                     {
-                        blocks[(y * 16 + x) * 256 + k] = rand.nextInt(3) == 0 ? cliffBlock2 : cliffBlock1; 
+                        if (rand.nextInt(3) == 0) {
+                            
+                            blocks[(y * 16 + x) * 256 + k] = hcCobble(world, i, j, x, y, k);
+                            metadata[(y * 16 + x) * 256 + k] = hcCobbleMeta(world, i, j, x, y, k);
+                        }
+                        else {
+                            
+                            blocks[(y * 16 + x) * 256 + k] = hcStone(world, i, j, x, y, k);
+                            metadata[(y * 16 + x) * 256 + k] = hcStoneMeta(world, i, j, x, y, k);
+                        }
                     }
                     else if (depth < 10)
                     {
-                        blocks[(y * 16 + x) * 256 + k] = cliffBlock1;
+                        blocks[(y * 16 + x) * 256 + k] = hcStone(world, i, j, x, y, k);
+                        metadata[(y * 16 + x) * 256 + k] = hcStoneMeta(world, i, j, x, y, k);
                     }
                 }
                 else

6d2924cbbecf8266a92c356006a51ae8e3606f79
WhichOnesPink <whichonespink@hokecraft.co.uk>
2016-01-20 12:26:55 +0000

    Added vanilla Jungle Hills config (Cactus)

--- src/main/java/rtg/api/biome/vanilla/config/BiomeConfigVanillaJungleHills.java
+++ src/main/java/rtg/api/biome/vanilla/config/BiomeConfigVanillaJungleHills.java
@@ -1,12 +1,20 @@
 package rtg.api.biome.vanilla.config;
 
+import rtg.api.biome.BiomeConfigProperty;
+import rtg.api.biome.BiomeConfigProperty.Type;
+
 
 public class BiomeConfigVanillaJungleHills extends BiomeConfigVanillaBase
 {
+    public static final String decorationCactusId = "decorationCactus";
+    public static final String decorationCactusName = "RTG Decoration: Cactus";
+    
     public BiomeConfigVanillaJungleHills()
     {
         super();
         
         this.biomeSlug = "junglehills";
+        
+        this.addProperty(new BiomeConfigProperty(decorationCactusId, Type.BOOLEAN, decorationCactusName, "", true));
     }
 }

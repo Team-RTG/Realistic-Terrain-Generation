02f7f5352c695055ea820215b492b170e2bbcd28
WhichOnesPink <whichonespink@hokecraft.co.uk>
2016-01-20 12:26:42 +0000

    Added vanilla Jungle config (Cactus)

--- src/main/java/rtg/api/biome/vanilla/config/BiomeConfigVanillaJungle.java
+++ src/main/java/rtg/api/biome/vanilla/config/BiomeConfigVanillaJungle.java
@@ -1,12 +1,20 @@
 package rtg.api.biome.vanilla.config;
 
+import rtg.api.biome.BiomeConfigProperty;
+import rtg.api.biome.BiomeConfigProperty.Type;
+
 
 public class BiomeConfigVanillaJungle extends BiomeConfigVanillaBase
 {
+    public static final String decorationCactusId = "decorationCactus";
+    public static final String decorationCactusName = "RTG Decoration: Cactus";
+    
     public BiomeConfigVanillaJungle()
     {
         super();
         
         this.biomeSlug = "jungle";
+        
+        this.addProperty(new BiomeConfigProperty(decorationCactusId, Type.BOOLEAN, decorationCactusName, "", true));
     }
 }

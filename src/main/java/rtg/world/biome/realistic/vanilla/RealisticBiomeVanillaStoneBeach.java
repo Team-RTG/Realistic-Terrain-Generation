abbd2e740c1cf05c7bd6e9a60a77b105f694428d
Zeno410 <zeno410@aol.com>
2016-01-21 10:43:01 -0800

    Standardized beach terrain
    Changes stone and cold beaches to use the same terrain as plain beach -
    they all have the same role in RTG. In vanilla stone has elevated
    terrain to match up with XHills but RTG doesn't need that and it's
    frequently counterproductive

--- src/main/java/rtg/world/biome/realistic/vanilla/RealisticBiomeVanillaStoneBeach.java
+++ src/main/java/rtg/world/biome/realistic/vanilla/RealisticBiomeVanillaStoneBeach.java
@@ -9,6 +9,7 @@ import rtg.world.gen.terrain.vanilla.TerrainVanillaStoneBeach;
 import net.minecraft.block.Block;
 import net.minecraft.init.Blocks;
 import net.minecraft.world.biome.BiomeGenBase;
+import rtg.world.gen.terrain.vanilla.TerrainVanillaBeach;
 
 public class RealisticBiomeVanillaStoneBeach extends RealisticBiomeVanillaBase
 {	
@@ -20,7 +21,7 @@ public class RealisticBiomeVanillaStoneBeach extends RealisticBiomeVanillaBase
 		super(
 			BiomeGenBase.stoneBeach,
 			BiomeBase.climatizedBiome(BiomeGenBase.river, Climate.WET),
-			new TerrainVanillaStoneBeach(0f, 100f, 63f, 80f),
+			new TerrainVanillaBeach(),//new TerrainVanillaStoneBeach(0f, 100f, 63f, 80f),
 			new SurfaceVanillaStoneBeach(topBlock, fillerBlock, true, Blocks.gravel, 1f, 1.5f, 85f, 20f, 4f)
 		);
 		

120f01e974f34ce447e7be053eaa472e8da0a522
Zeno410 <zeno410@aol.com>
2016-01-26 11:08:37 -0800

    Flying Mountains revision
    New version of Flying Mountains with tall flat-topped hills and
    mountains on a low plain.

--- src/main/java/rtg/world/gen/terrain/highlands/TerrainHLFlyingMountains.java
+++ src/main/java/rtg/world/gen/terrain/highlands/TerrainHLFlyingMountains.java
@@ -11,6 +11,7 @@ public class TerrainHLFlyingMountains extends TerrainBase
 	private float lakeDepth;
 	private float lakeWidth;
 	private float terrainHeight;
+    private OpenSimplexNoise ruggedness = new OpenSimplexNoise(2L);
 	
 	/*
 	 * width = 230f
@@ -36,7 +37,7 @@ public class TerrainHLFlyingMountains extends TerrainBase
 	}
 
     //reworked for the soar/plunge effect
-    public float generateNoise(OpenSimplexNoise simplex, CellNoise cell, int x, int y, float border, float river)
+    public float generateNoise1(OpenSimplexNoise simplex, CellNoise cell, int x, int y, float border, float river)
 	{
 		float h = simplex.noise2(x / 20f, y / 20f) * 2;
 		h += simplex.noise2(x / 7f, y / 7f) * 0.8f;
@@ -84,31 +85,45 @@ public class TerrainHLFlyingMountains extends TerrainBase
 		l = l < -8f ? -8f : l;*/
 		return terrainHeight + h + m2 - l;
 	}
-	
-	/*@Override
-	public float generateNoise(OpenSimplexNoise simplex, CellNoise cell, int x, int y, float border, float river)
+    @Override
+    public float generateNoise(OpenSimplexNoise simplex, CellNoise cell, int x, int y, float border, float river)
 	{
-		float h = simplex.noise2(x / 20f, y / 20f) * 2;
-		h += simplex.noise2(x / 7f, y / 7f) * 0.8f;
-		
-		float m = simplex.noise2(x / width, y / width) * strength * river;
-		m *= m / 35f;
-		m = m > 70f ? 70f + (m - 70f) / 2.5f : m;
-		
-		float st = m * 0.7f;
-		st = st > 20f ? 20f : st;
-		float c = cell.noise(x / 30f, y / 30f, 1D) * (5f + st);
-		
-		float sm = simplex.noise2(x / 30f, y / 30f) * 8f + simplex.noise2(x / 8f, y / 8f);
-		sm *= (m + 10f) / 20f > 2.5f ? 2.5f : (m + 10f) / 20f;
-		m += sm;
-		
-		m += c;
-		
-		float l = simplex.noise2(x / lakeWidth, y / lakeWidth) * lakeDepth;
-		l *= l / 25f;
-		l = l < -8f ? -8f : l;
-		
-		return terrainHeight + h + m - l;
-	}*/
+        float firstJump = 0.2f;
+        float secondJump = 0.4f;
+        float firstScatter = 0.4f;
+        float secondScatter = 0.6f;
+        float adjust =0;
+        float firstLevel = simplex.noise2(x / width, y / width)*5+10+simplex.noise2(x / 20f, y / 20f);
+        firstLevel *= river;
+        float secondLevel = simplex.noise2(x / width+0.5f, y / width+0.5f)*15+30
+                +simplex.noise2(x / 20f, y / 20f)*3f;
+        float thirdLevel = simplex.noise2(x / width+0.3f, y / width+0.3f)*25+50
+                +simplex.noise2(x / 20f, y / 20f)*5f;
+        float scatter = Math.abs(ruggedness.noise2(x/30f, y/30f));
+        if (river < firstJump||scatter<firstScatter) {
+            adjust = firstLevel;
+        } else {
+            if (river<secondJump||scatter<secondScatter) {
+                if (scatter >0.5f) {
+                    adjust = secondLevel;
+                } else {
+                    float lift = (scatter-0.4f)*10f;// 0 to 1
+                    lift = (0.5f-lift)*2f;// -.5 to +.5
+                    lift = unsignedPower(lift, 0.5f);
+                    lift = -lift/2f+0.5f;
+                    adjust = secondLevel*lift + firstLevel*(1.0f-lift);
+                }
+            } else {
+                if (scatter >0.7f) {
+                    adjust = thirdLevel;
+                } else {
+                    float lift = (scatter-0.6f)*10f;
+                    lift = lift*lift;
+                    adjust = thirdLevel*lift + secondLevel*(1.0f-lift);
+                }
+            }
+        }
+        return terrainHeight + adjust;
+    }
+
 }

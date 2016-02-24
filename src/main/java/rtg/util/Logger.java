5b3e87d3617096e44f48ec626d7d043ae235fe5a
Tobias Pisani <tobiaspisani@gmail.com>
2016-02-24 14:45:16 +0100

    added debug method to rtg.util.Logger

--- src/main/java/rtg/util/Logger.java
+++ src/main/java/rtg/util/Logger.java
@@ -2,45 +2,11 @@ package rtg.util;
 
 import net.minecraftforge.fml.common.FMLLog;
 import org.apache.logging.log4j.Level;
-import rtg.reference.ModInfo;
+import rtg.config.rtg.ConfigRTG;
 
 public class Logger {
-	public static void log(Level level, String format, Object... data) {
-		FMLLog.log(ModInfo.MOD_ID, level, format, data);
-	}
-
-	public static void log(Level level, Throwable throwable, String format, Object... data) {
-		FMLLog.log(ModInfo.MOD_ID, level, throwable, format, data);
-	}
-
-
-	public static void fatal(String format, Object... data) {
-		log(Level.FATAL, format, data);
-	}
-
-	public static void fatal(Throwable throwable, String format,
-							 Object... data) {
-		log(Level.FATAL, throwable, format, data);
-	}
-
-	public static void error(String format, Object... data) {
-		log(Level.ERROR, format, data);
-	}
-
-	public static void error(Throwable throwable, String format,
-							 Object... data) {
-		log(Level.ERROR, throwable, format, data);
-	}
-
-	public static void warn(String format, Object... data) {
-		log(Level.WARN, format, data);
-	}
-
-	public static void info(String format, Object... data) {
-		log(Level.INFO, format, data);
-	}
 
 	public static void debug(String format, Object... data) {
-		log(Level.DEBUG, format, data);
+		if (ConfigRTG.enableDebugging) FMLLog.log(Level.DEBUG, "[RTG-DEBUG] " + format, data);
 	}
 }

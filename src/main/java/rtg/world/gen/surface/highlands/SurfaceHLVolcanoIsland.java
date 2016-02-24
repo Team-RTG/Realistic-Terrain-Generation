00936108e685b30df819057eb1363dc6e106c7e8
WhichOnesPink <whichonespink@hokecraft.co.uk>
2016-02-24 18:08:32 +0000

    Refactored HL Volcano Island to use correct surface class.

--- src/main/java/rtg/world/gen/surface/highlands/SurfaceHLVolcanoIsland.java
+++ src/main/java/rtg/world/gen/surface/highlands/SurfaceHLVolcanoIsland.java
@@ -1,37 +1,33 @@
 package rtg.world.gen.surface.highlands;
 
-import net.minecraft.block.Block;
-import net.minecraft.init.Blocks;
-import net.minecraft.world.World;
-import net.minecraft.world.biome.BiomeGenBase;
-import net.minecraft.world.chunk.ChunkPrimer;
+import java.util.Random;
+
 import rtg.api.biome.BiomeConfig;
 import rtg.util.CellNoise;
 import rtg.util.CliffCalculator;
 import rtg.util.OpenSimplexNoise;
 import rtg.world.gen.surface.SurfaceBase;
 
-import java.util.Random;
+import net.minecraft.block.Block;
+import net.minecraft.init.Blocks;
+import net.minecraft.world.World;
+import net.minecraft.world.biome.BiomeGenBase;
+import net.minecraft.world.chunk.ChunkPrimer;
 
 public class SurfaceHLVolcanoIsland extends SurfaceBase
 {
-
+    
     private Block blockMixTop;
     private byte byteMixTop;
     private Block blockMixFiller;
     private byte byteMixFiller;
-    private Block blockCliff1;
-    private byte byteCliff1;
-    private Block blockCliff2;
-    private byte byteCliff2;
     private float floMixWidth;
     private float floMixHeight;
     private float floSmallWidth;
     private float floSmallStrength;
     
     public SurfaceHLVolcanoIsland(BiomeConfig config, Block top, byte topByte, Block filler, byte fillerByte, Block mixTop, byte mixTopByte, Block mixFiller,
-        byte mixFillerByte, Block cliff1, byte cliff1Byte, Block cliff2, byte cliff2Byte, float mixWidth, float mixHeight,
-        float smallWidth, float smallStrength)
+        byte mixFillerByte, float mixWidth, float mixHeight, float smallWidth, float smallStrength)
     {
     
         super(config, top, topByte, filler, fillerByte);
@@ -40,12 +36,6 @@ public class SurfaceHLVolcanoIsland extends SurfaceBase
         byteMixTop = mixTopByte;
         blockMixFiller = mixFiller;
         byteMixFiller = mixFillerByte;
-        
-        blockCliff1 = cliff1;
-        byteCliff1 = cliff1Byte;
-        
-        blockCliff2 = cliff2;
-        byteCliff2 = cliff2Byte;
         
         floMixWidth = mixWidth;
         floMixHeight = mixHeight;
@@ -78,16 +68,17 @@ public class SurfaceHLVolcanoIsland extends SurfaceBase
                     if (depth > -1 && depth < 2)
                     {
                         if (rand.nextInt(3) == 0) {
-                            primer.setBlockState((y * 16 + x) * 256 + k, blockCliff2.getStateFromMeta(byteCliff2));
+                            
+                            primer.setBlockState((y * 16 + x) * 256 + k, hcCobble(world, i, j, x, y, k));
                         }
                         else {
-                            primer.setBlockState((y * 16 + x) * 256 + k, blockCliff1.getStateFromMeta(byteCliff1));
+                            
+                            primer.setBlockState((y * 16 + x) * 256 + k, hcStone(world, i, j, x, y, k));
                         }
-                        
                     }
                     else if (depth < 10)
                     {
-                        primer.setBlockState((y * 16 + x) * 256 + k, blockCliff1.getStateFromMeta(byteCliff1));
+                        primer.setBlockState((y * 16 + x) * 256 + k, hcStone(world, i, j, x, y, k));
                     }
                 }
                 else

a7c75c7c7f12ee03278e53f8f3e0d032d29097cc
WhichOnesPink <whichonespink@hokecraft.co.uk>
2016-02-28 18:16:27 +0000

    Realistic biomes now check for null biome names.

--- src/main/java/rtg/world/biome/realistic/arsmagica/RealisticBiomeAMBase.java
+++ src/main/java/rtg/world/biome/realistic/arsmagica/RealisticBiomeAMBase.java
@@ -2,6 +2,7 @@ package rtg.world.biome.realistic.arsmagica;
 
 import rtg.api.biome.BiomeConfig;
 import rtg.api.biome.arsmagica.config.BiomeConfigAM;
+import rtg.util.Logger;
 import rtg.world.biome.realistic.RealisticBiomeBase;
 import rtg.world.gen.surface.SurfaceBase;
 import rtg.world.gen.terrain.TerrainBase;
@@ -33,6 +34,11 @@ public class RealisticBiomeAMBase extends RealisticBiomeBase
             {
                 if (b[i] != null)
                 {
+                    if (b[i].biomeName == null) {
+                        Logger.warn("Biome ID %d has no name.", b[i].biomeID);
+                        continue;
+                    }
+                    
                     BiomeGenBase amBiome = b[i];
                     String biomeName = b[i].biomeName;
                     String biomeClass = b[i].getBiomeClass().getName();
--- src/main/java/rtg/world/biome/realistic/growthcraft/RealisticBiomeGCBase.java
+++ src/main/java/rtg/world/biome/realistic/growthcraft/RealisticBiomeGCBase.java
@@ -2,6 +2,7 @@ package rtg.world.biome.realistic.growthcraft;
 
 import rtg.api.biome.BiomeConfig;
 import rtg.api.biome.growthcraft.config.BiomeConfigGC;
+import rtg.util.Logger;
 import rtg.world.biome.realistic.RealisticBiomeBase;
 import rtg.world.gen.surface.SurfaceBase;
 import rtg.world.gen.terrain.TerrainBase;
@@ -33,6 +34,11 @@ public class RealisticBiomeGCBase extends RealisticBiomeBase
             {
                 if (b[i] != null)
                 {
+                    if (b[i].biomeName == null) {
+                        Logger.warn("Biome ID %d has no name.", b[i].biomeID);
+                        continue;
+                    }
+                    
                     BiomeGenBase gcBiome = b[i];
                     String biomeName = b[i].biomeName;
                     String biomeClass = b[i].getBiomeClass().getName();

93351195b25a173b32944a402964e0c35c45e9d9
WhichOnesPink <whichonespink@hokecraft.co.uk>
2016-03-03 22:02:12 +0000

    Wrapped TerrainHighland.

--- src/main/java/rtg/world/gen/terrain/TerrainBase.java
+++ src/main/java/rtg/world/gen/terrain/TerrainBase.java
@@ -250,6 +250,24 @@ public class TerrainBase
         return 68f + h + m - l;
     }
     
+    public static float terrainHighland(int x, int y, OpenSimplexNoise simplex, CellNoise cell, float river, float start, float width, float height)
+    {
+        float h = simplex.noise2(x / width, y / width) * height * river;
+        h = h < start ? start + ((h - start) / 4.5f) : h;
+        
+        if(h > 0f)
+        {
+            float st = h * 1.5f > 15f ? 15f : h * 1.5f;
+            h += cell.noise(x / 70D, y / 70D, 1D) * st;
+        }
+        
+        h += simplex.noise2(x / 20f, y / 20f) * 5f;
+        h += simplex.noise2(x / 12f, y / 12f) * 3f;
+        h += simplex.noise2(x / 5f, y / 5f) * 1.5f;
+        
+        return getTerrainBase() + h;
+    }
+    
     public static float terrainPolar(int x, int y, OpenSimplexNoise simplex, float river)
     {
         float st = (simplex.noise2(x / 160f, y / 160f) + 0.38f) * (ConfigRTG.duneHeight + 23f) * river;

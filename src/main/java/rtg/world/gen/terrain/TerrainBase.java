be1a29e8626b71522e97e63c284f2790431a29ea
WhichOnesPink <whichonespink@hokecraft.co.uk>
2016-03-04 07:17:39 +0000

    Wrapped TerrainMountain.

--- src/main/java/rtg/world/gen/terrain/TerrainBase.java
+++ src/main/java/rtg/world/gen/terrain/TerrainBase.java
@@ -359,6 +359,32 @@ public class TerrainBase
         return 74f + b;
     }
     
+    public static float terrainMountain(int x, int y, OpenSimplexNoise simplex, CellNoise cell, float river)
+    {
+        float h = simplex.noise2(x / 300f, y / 300f) * 135f * river;
+        h *= h / 32f;
+        h = h > 150f ? 150f : h;
+
+        if(h > 10f)
+        {
+            float d = (h - 10f) / 2f > 8f ? 8f : (h - 10f) / 2f;
+            h += simplex.noise2(x / 35f, y / 35f) * d;
+            h += simplex.noise2(x / 60f, y / 60f) * d * 0.5f;
+
+            if(h > 35f)
+            {
+                float d2 = (h - 35f) / 1.5f > 30f ? 30f : (h - 35f) / 1.5f;
+                h += cell.noise(x / 25D, y / 25D, 1D) * d2;
+            }
+        }
+        
+        h += simplex.noise2(x / 28f, y / 28f) * 4;
+        h += simplex.noise2(x / 18f, y / 18f) * 2;
+        h += simplex.noise2(x / 8f, y / 8f) * 2;
+        
+        return h + 67f;
+    }
+    
     public static float terrainPolar(int x, int y, OpenSimplexNoise simplex, float river)
     {
         float st = (simplex.noise2(x / 160f, y / 160f) + 0.38f) * (ConfigRTG.duneHeight + 23f) * river;

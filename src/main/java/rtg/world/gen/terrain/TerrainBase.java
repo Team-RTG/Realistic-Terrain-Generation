a1db8e49fd0501b5364148d7448844ee2f2b86a2
WhichOnesPink <whichonespink@hokecraft.co.uk>
2016-03-04 09:30:46 +0000

    Wrapped TerrainSwampRiver.

--- src/main/java/rtg/world/gen/terrain/TerrainBase.java
+++ src/main/java/rtg/world/gen/terrain/TerrainBase.java
@@ -525,4 +525,22 @@ public class TerrainBase
         
         return h + 56f;
     }
+    
+    public static float terrainSwampRiver(int x, int y, OpenSimplexNoise simplex, float river)
+    {
+        float h = simplex.noise2(x / 180f, y / 180f) * 40f * river;
+        h *= h / 35f;
+        
+        if(h < 1f)
+        {
+            h = 1f;
+        }
+        
+        if(h < 4f)
+        {
+            h += (simplex.noise2(x / 50f, y / 50f) + simplex.noise2(x / 15f, y / 15f)) * (4f - h);
+        }
+        
+        return 60f + h;
+    }
 }

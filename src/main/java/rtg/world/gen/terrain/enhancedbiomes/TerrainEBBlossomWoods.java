8ea3b5041475bb3cde30682f052728e1dd91eceb
WhichOnesPink <whichonespink@hokecraft.co.uk>
2016-03-05 09:10:34 +0000

    Wrapped TerrainPlains.

--- src/main/java/rtg/world/gen/terrain/enhancedbiomes/TerrainEBBlossomWoods.java
+++ src/main/java/rtg/world/gen/terrain/enhancedbiomes/TerrainEBBlossomWoods.java
@@ -15,19 +15,6 @@ public class TerrainEBBlossomWoods extends TerrainBase
     @Override
     public float generateNoise(OpenSimplexNoise simplex, CellNoise cell, int x, int y, float border, float river)
     {
-
-        float floNoise;
-        float st = (simplex.noise2(x / 160f, y / 160f) + 0.38f) * 10f * river;
-        st = st < 0.2f ? 0.2f : st;
-
-        float h = simplex.noise2(x / 60f, y / 60f) * st * 2f;
-        h = h > 0f ? -h : h;
-        h += st;
-        h *= h / 80f;
-        h += st;
-
-        floNoise = 65f + h;
-
-        return floNoise;
+        return terrainPlains(x, y, simplex, river, 80f, 65f);
     }
 }

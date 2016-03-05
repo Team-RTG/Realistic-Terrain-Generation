56d1c96a3119abdaa732aa6ccb2e008a7d54a438
WhichOnesPink <whichonespink@hokecraft.co.uk>
2016-03-05 17:37:19 +0000

    Fully wrapped TerrainLonelyMountain.

--- src/main/java/rtg/world/gen/terrain/biomesoplenty/TerrainBOPGlacier.java
+++ src/main/java/rtg/world/gen/terrain/biomesoplenty/TerrainBOPGlacier.java
@@ -38,28 +38,6 @@ public class TerrainBOPGlacier extends TerrainBase
     @Override
     public float generateNoise(OpenSimplexNoise simplex, CellNoise cell, int x, int y, float border, float river)
     {
-
-        float h = simplex.noise2(x / 20f, y / 20f) * 2;
-        h += simplex.noise2(x / 7f, y / 7f) * 0.8f;
-
-        float m = simplex.noise2(x / width, y / width) * strength * river;
-        m *= m / 35f;
-        m = m > 70f ? 70f + (m - 70f) / 2.5f : m;
-
-        float st = m * 0.7f;
-        st = st > 20f ? 20f : st;
-        float c = cell.noise(x / 30f, y / 30f, 1D) * (5f + st);
-
-        float sm = simplex.noise2(x / 30f, y / 30f) * 8f + simplex.noise2(x / 8f, y / 8f);
-        sm *= (m + 10f) / 20f > 2.5f ? 2.5f : (m + 10f) / 20f;
-        m += sm;
-
-        m += c;
-
-        float l = simplex.noise2(x / lakeWidth, y / lakeWidth) * lakeDepth;
-        l *= l / 25f;
-        l = l < -8f ? -8f : l;
-
-        return terrainHeight + h + m - l;
+        return terrainLonelyMountain(x, y, simplex, cell, river, strength, width, lakeWidth, lakeDepth, terrainHeight);
     }
 }

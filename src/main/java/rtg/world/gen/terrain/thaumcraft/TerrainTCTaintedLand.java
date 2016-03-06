97af9c407485c993c2047bb4521ba74f89d3b347
WhichOnesPink <whichonespink@hokecraft.co.uk>
2016-03-06 06:22:01 +0000

    Wrapped TerrainForest.

--- src/main/java/rtg/world/gen/terrain/thaumcraft/TerrainTCTaintedLand.java
+++ src/main/java/rtg/world/gen/terrain/thaumcraft/TerrainTCTaintedLand.java
@@ -13,11 +13,6 @@ public class TerrainTCTaintedLand extends TerrainBase
 	@Override
 	public float generateNoise(OpenSimplexNoise simplex, CellNoise cell, int x, int y, float border, float river)
 	{
-		float h = simplex.noise2(x / 100f, y / 100f) * 8;
-		h += simplex.noise2(x / 30f, y / 30f) * 4;
-		h += simplex.noise2(x / 15f, y / 15f) * 2;
-		h += simplex.noise2(x / 7f, y / 7f);
-
-    	return 70f + (20f * river) + h;
+        return terrainForest(x, y, simplex, river, 70f);
 	}
 }

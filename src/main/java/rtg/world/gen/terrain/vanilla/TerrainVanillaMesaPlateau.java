bfc04be6a2fde0255a0f4776199cc5bba387a8eb
Tobias Pisani <tobiaspisani@gmail.com>
2016-03-11 09:31:28 +0100

    Fix for canyoncolour crash

--- src/main/java/rtg/world/gen/terrain/vanilla/TerrainVanillaMesaPlateau.java
+++ src/main/java/rtg/world/gen/terrain/vanilla/TerrainVanillaMesaPlateau.java
@@ -38,8 +38,11 @@ public class TerrainVanillaMesaPlateau extends TerrainBase
 		/**    Values come in pairs per layer. First is how high to step up.
 		 * 	Second is a value between 0 and 1, signifying when to step up.
 		 */
-		height = new float[]{24.0f, 0.4f, 24f, 0.5f};
-		smooth = 0.1f;
+		height = new float[]{32.0f, 0.4f, 20f, 0.7f};
+		/**
+		 * lower values = smoother.
+		 */
+		smooth = 50f;
 		heightLength = height.length;
 		base = 69f;
 	}
@@ -52,14 +55,15 @@ public class TerrainVanillaMesaPlateau extends TerrainBase
 		float b = simplex.noise2(x / 40f, y / 40f) * 1.5f;
 		b *= river;
 
-		float sn = simplex.noise2(x / 50f, y / 50f) * 0.5f + 0.5f;
-		sn += simplex.noise2(x / 8f, y / 8f) * 0.02 + 0.02;
+		float sn = simplex.noise2(x / 100f, y / 100f) * 0.5f + 0.5f;
+		sn += simplex.noise2(x / 8f, y / 8f) * 0.02 + 0.02f;
 		float n;
 		for (int i = 0; i < heightLength; i += 2) {
-			n = (sn - height[i + 1]) / smooth;
+			n = (sn - height[i + 1]) / (1 - height[i + 1]);
+			n = n * smooth;
 			n = (n < 0) ? 0 : (n > 1) ? 1 : n;
-			if (n > height[i + 1]) {
-				b += (height[i] * (n + 0.5f) / 0.5f);
+			if (sn > height[i + 1]) {
+				b += (height[i] * n);
 			}
 		}
 

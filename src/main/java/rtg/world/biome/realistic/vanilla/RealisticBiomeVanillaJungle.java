9bf45334d847bd5b1d36625966caf7b5e5a13f25
WhichOnesPink <whichonespink@hokecraft.co.uk>
2016-03-12 21:52:35 +0000

    Removed rDecorate from vanilla Jungle.

--- src/main/java/rtg/world/biome/realistic/vanilla/RealisticBiomeVanillaJungle.java
+++ src/main/java/rtg/world/biome/realistic/vanilla/RealisticBiomeVanillaJungle.java
@@ -124,15 +124,4 @@ public class RealisticBiomeVanillaJungle extends RealisticBiomeVanillaBase
 		decoBoulder.strengthFactor = 2f;
 		this.decos.add(decoBoulder);
 	}
-	
-    
-    //@Override
-    //public void rDecorate(World world, Random rand, int chunkX, int chunkY, OpenSimplexNoise simplex, CellNoise cell, float strength, float river)
-    //{
-        
-        /**
-         * Using rDecorateSeedBiome() to partially decorate the biome? If so, then comment out this method.
-         */
-        //rOreGenSeedBiome(world, rand, chunkX, chunkY, simplex, cell, strength, river, baseBiome);
-    //}
 }

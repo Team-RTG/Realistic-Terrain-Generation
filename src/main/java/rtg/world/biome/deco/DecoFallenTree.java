bb9e3cb5c65cec8ce69f3fa61ad08c282da74f1f
WhichOnesPink <whichonespink@hokecraft.co.uk>
2016-03-12 20:39:56 +0000

    Improved DecoFallenTree.

--- src/main/java/rtg/world/biome/deco/DecoFallenTree.java
+++ src/main/java/rtg/world/biome/deco/DecoFallenTree.java
@@ -1,10 +1,13 @@
 package rtg.world.biome.deco;
 
+import static net.minecraftforge.event.terraingen.DecorateBiomeEvent.Decorate.EventType.TREE;
+
 import java.util.Random;
 
 import net.minecraft.block.Block;
 import net.minecraft.init.Blocks;
 import net.minecraft.world.World;
+import net.minecraftforge.event.terraingen.TerrainGen;
 import rtg.util.CellNoise;
 import rtg.util.OpenSimplexNoise;
 import rtg.world.biome.realistic.RealisticBiomeBase;
@@ -49,27 +52,30 @@ public class DecoFallenTree extends DecoBase
 	{
 		if (this.allowed) {
 			
-			float noise = simplex.noise2(chunkX / this.logDistribution.noiseDivisor(), chunkY / this.logDistribution.noiseDivisor()) * this.logDistribution.noiseFactor() + this.logDistribution.noiseAddend();
-			
-            for (int i = 0; i < this.loops; i++)
-            {
-                if (isValidLogCondition(noise, rand))
-                {
-                    int x22 = chunkX + rand.nextInt(16) + 8;
-                    int z22 = chunkY + rand.nextInt(16) + 8;
-                    int y22 = world.getHeightValue(x22, z22);
-                    
-                    if (y22 <= this.maxY) {
-                    	
-                    	if (this.maxSize > this.minSize) {
-                    		(new WorldGenLog(this.logBlock, this.logMeta, this.leavesBlock, this.leavesMeta, this.minSize + rand.nextInt(this.maxSize - this.minSize))).generate(world, rand, x22, y22, z22);
-                    	}
-                    	else if (this.maxSize == this.minSize) {
-                    		(new WorldGenLog(this.logBlock, this.logMeta, this.leavesBlock, this.leavesMeta, this.minSize)).generate(world, rand, x22, y22, z22);
-                    	}
-                    }
-                }
-            }
+			if (TerrainGen.decorate(world, rand, chunkX, chunkY, TREE)) {
+				
+				float noise = simplex.noise2(chunkX / this.logDistribution.noiseDivisor(), chunkY / this.logDistribution.noiseDivisor()) * this.logDistribution.noiseFactor() + this.logDistribution.noiseAddend();
+				
+	            for (int i = 0; i < this.loops; i++)
+	            {
+	                if (isValidLogCondition(noise, rand))
+	                {
+	                    int x22 = chunkX + rand.nextInt(16) + 8;
+	                    int z22 = chunkY + rand.nextInt(16) + 8;
+	                    int y22 = world.getHeightValue(x22, z22);
+	                    
+	                    if (y22 <= this.maxY) {
+	                    	
+	                    	if (this.maxSize > this.minSize) {
+	                    		(new WorldGenLog(this.logBlock, this.logMeta, this.leavesBlock, this.leavesMeta, this.minSize + rand.nextInt(this.maxSize - this.minSize))).generate(world, rand, x22, y22, z22);
+	                    	}
+	                    	else if (this.maxSize == this.minSize) {
+	                    		(new WorldGenLog(this.logBlock, this.logMeta, this.leavesBlock, this.leavesMeta, this.minSize)).generate(world, rand, x22, y22, z22);
+	                    	}
+	                    }
+	                }
+	            }
+			}
 		}
 	}
 	

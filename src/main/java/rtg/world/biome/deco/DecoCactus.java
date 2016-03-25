54094b3d7d6d2e1a268fd26bfa3f5680c74446f2
Tobias Pisani <tobiaspisani@gmail.com>
2016-03-25 15:22:04 +0100

    Add soil option to DecoCactus

--- src/main/java/rtg/world/biome/deco/DecoCactus.java
+++ src/main/java/rtg/world/biome/deco/DecoCactus.java
@@ -1,5 +1,7 @@
 package rtg.world.biome.deco;
 
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.init.Blocks;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import net.minecraftforge.event.terraingen.TerrainGen;
@@ -23,6 +25,10 @@ public class DecoCactus extends DecoBase
 	public float strengthFactor;
 	public int maxY;
 	public boolean sandOnly;
+    /**
+     * Block to place under cactus. Sand, Red sand and coarse dirt are possibilities.
+     */
+    public IBlockState soil;
 	
 	public DecoCactus()
 	{
@@ -35,6 +41,7 @@ public class DecoCactus extends DecoBase
 		this.maxY = 255; // No height limit by default.
 		this.strengthFactor = 2f; // The higher the value, the more there will be.
 		this.sandOnly = false;
+        this.soil = Blocks.sand.getDefaultState();
 		
 		this.addDecoTypes(DecoType.CACTUS);
 	}

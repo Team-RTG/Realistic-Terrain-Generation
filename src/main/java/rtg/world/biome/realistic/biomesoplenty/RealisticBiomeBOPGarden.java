830fcdf7eacbfc5193a5b6cb279fc96d97587662
WhichOnesPink <whichonespink@hokecraft.co.uk>
2016-04-05 00:31:40 +0100

    Re-decorated BOP Garden.

--- src/main/java/rtg/world/biome/realistic/biomesoplenty/RealisticBiomeBOPGarden.java
+++ src/main/java/rtg/world/biome/realistic/biomesoplenty/RealisticBiomeBOPGarden.java
@@ -1,10 +1,12 @@
 package rtg.world.biome.realistic.biomesoplenty;
 
 import rtg.api.biome.BiomeConfig;
+import rtg.world.biome.deco.DecoBaseBiomeDecorations;
+import rtg.world.biome.deco.DecoFlowersRTG;
+import rtg.world.biome.deco.DecoFlowersRTG.HeightType;
 import rtg.world.gen.surface.biomesoplenty.SurfaceBOPGarden;
 import rtg.world.gen.terrain.biomesoplenty.TerrainBOPGarden;
 import biomesoplenty.api.content.BOPCBiomes;
-
 import net.minecraft.block.Block;
 import net.minecraft.world.biome.BiomeGenBase;
 
@@ -35,5 +37,14 @@ public class RealisticBiomeBOPGarden extends RealisticBiomeBOPBase
                 0.5f //float smallStrength
             )
 		);
+		
+		DecoFlowersRTG decoFlowers1 = new DecoFlowersRTG();
+		decoFlowers1.flowers = new int[] {0, 1, 2, 3, 4, 5, 6, 7, 8, 9}; //Only 1-block-tall flowers.
+		decoFlowers1.strengthFactor = 2f;
+		decoFlowers1.heightType = HeightType.GET_HEIGHT_VALUE;
+        this.addDeco(decoFlowers1);
+        
+		DecoBaseBiomeDecorations decoBaseBiomeDecorations = new DecoBaseBiomeDecorations();
+		this.addDeco(decoBaseBiomeDecorations);
 	}
 }

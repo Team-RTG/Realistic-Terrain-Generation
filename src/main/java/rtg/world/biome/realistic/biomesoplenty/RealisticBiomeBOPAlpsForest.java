503317586d2b597ab087cd7fe2968c279bbd528c
WhichOnesPink <whichonespink@hokecraft.co.uk>
2016-04-05 11:06:11 +0100

    Re-decorated BOP Alps Forest.

--- src/main/java/rtg/world/biome/realistic/biomesoplenty/RealisticBiomeBOPAlpsForest.java
+++ src/main/java/rtg/world/biome/realistic/biomesoplenty/RealisticBiomeBOPAlpsForest.java
@@ -1,21 +1,16 @@
 package rtg.world.biome.realistic.biomesoplenty;
 
-import java.util.Random;
-
+import net.minecraft.block.Block;
+import net.minecraft.init.Blocks;
+import net.minecraft.world.biome.BiomeGenBase;
 import rtg.api.biome.BiomeConfig;
 import rtg.api.biome.biomesoplenty.config.BiomeConfigBOPAlpsForest;
-import rtg.util.CellNoise;
-import rtg.util.OpenSimplexNoise;
-import rtg.world.gen.feature.WorldGenLog;
-import rtg.world.gen.feature.tree.WorldGenTreeRTGShrub;
+import rtg.world.biome.deco.DecoFallenTree;
+import rtg.world.biome.deco.DecoFallenTree.LogCondition;
+import rtg.world.biome.deco.DecoShrub;
 import rtg.world.gen.surface.biomesoplenty.SurfaceBOPAlpsForest;
 import rtg.world.gen.terrain.biomesoplenty.TerrainBOPAlpsForest;
 import biomesoplenty.api.content.BOPCBiomes;
-
-import net.minecraft.block.Block;
-import net.minecraft.init.Blocks;
-import net.minecraft.world.World;
-import net.minecraft.world.biome.BiomeGenBase;
 
 public class RealisticBiomeBOPAlpsForest extends RealisticBiomeBOPBase
 {	
@@ -32,49 +27,27 @@ public class RealisticBiomeBOPAlpsForest extends RealisticBiomeBOPBase
 			new SurfaceBOPAlpsForest(config, topBlock, fillerBlock, false, null, 0f, 1.5f, 60f, 65f, 1.5f, Blocks.stone, 0.15f)
 		);
 		this.generatesEmeralds = true;
+		
+		DecoFallenTree decoFallenTree = new DecoFallenTree();
+		decoFallenTree.distribution.noiseDivisor = 100f;
+		decoFallenTree.distribution.noiseFactor = 6f;
+		decoFallenTree.distribution.noiseAddend = 0.8f;
+		decoFallenTree.logCondition = LogCondition.NOISE_GREATER_AND_RANDOM_CHANCE;
+		decoFallenTree.logConditionNoise = 0f;
+		decoFallenTree.logConditionChance = 12;
+		decoFallenTree.maxY = 100;
+		decoFallenTree.logBlock = Blocks.log;
+		decoFallenTree.logMeta = (byte)1;
+		decoFallenTree.leavesBlock = Blocks.leaves;
+		decoFallenTree.leavesMeta = (byte)-1;
+		decoFallenTree.minSize = 3;
+		decoFallenTree.maxSize = 6;
+		this.addDeco(decoFallenTree, this.config._boolean(BiomeConfigBOPAlpsForest.decorationLogsId));        
+        
+        DecoShrub decoShrub = new DecoShrub();
+        decoShrub.maxY = 100;
+        decoShrub.strengthFactor = 2f;
+        decoShrub.chance = 8;
+		this.addDeco(decoShrub);		
 	}
-	
-    @Override
-    public void rDecorate(World world, Random rand, int chunkX, int chunkY, OpenSimplexNoise simplex, CellNoise cell, float strength, float river)
-    {
-        
-        /**
-         * Using rDecorateSeedBiome() to partially decorate the biome? If so, then comment out this method.
-         */
-        //rOreGenSeedBiome(world, rand, chunkX, chunkY, simplex, cell, strength, river, baseBiome);
-        
-        rDecorateSeedBiome(world, rand, chunkX, chunkY, simplex, cell, strength, river, baseBiome);
-        
-        float l = simplex.noise2(chunkX / 100f, chunkY / 100f) * 6f + 0.8f;
-        
-        if (this.config.getPropertyById(BiomeConfigBOPAlpsForest.decorationLogsId).valueBoolean) {
-        
-            if (l > 0f && rand.nextInt(12) == 0)
-            {
-                int x22 = chunkX + rand.nextInt(16) + 8;
-                int z22 = chunkY + rand.nextInt(16) + 8;
-                int y22 = world.getHeightValue(x22, z22);
-                (new WorldGenLog(1, 3 + rand.nextInt(4), false)).generate(world, rand, x22, y22, z22);
-            }
-        }
-        
-        for (int b = 0; b < 2f * strength; b++)
-        {
-            int i1 = chunkX + rand.nextInt(16) + 8;
-            int j1 = chunkY + rand.nextInt(16) + 8;
-            int k1 = world.getHeightValue(i1, j1);
-            
-            if (rand.nextInt(8) == 0) {
-                
-                if (rand.nextInt(10) == 0)
-                {
-                    (new WorldGenTreeRTGShrub(rand.nextInt(5) + 4, rand.nextInt(2), rand.nextInt(2))).generate(world, rand, i1, k1, j1);
-                }
-                else
-                {
-                    (new WorldGenTreeRTGShrub(rand.nextInt(4) + 1, rand.nextInt(2), rand.nextInt(2))).generate(world, rand, i1, k1, j1);
-                }
-            }
-        }
-    }
 }

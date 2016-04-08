384c0167daa9183d6fe5a582c0c393b5b86257a6
WhichOnesPink <whichonespink@hokecraft.co.uk>
2016-04-08 20:49:41 +0100

    Re-decorated AbyssalCraft biomes.

--- src/main/java/rtg/world/biome/realistic/abyssalcraft/RealisticBiomeACDarklandsForest.java
+++ src/main/java/rtg/world/biome/realistic/abyssalcraft/RealisticBiomeACDarklandsForest.java
@@ -1,24 +1,20 @@
 package rtg.world.biome.realistic.abyssalcraft;
 
-import java.util.Random;
-
+import net.minecraft.world.biome.BiomeGenBase;
 import rtg.api.biome.BiomeConfig;
 import rtg.api.biome.abyssalcraft.config.BiomeConfigACDarklandsForest;
-import rtg.util.CellNoise;
-import rtg.util.OpenSimplexNoise;
-import rtg.world.gen.feature.WorldGenGrass;
-import rtg.world.gen.feature.WorldGenLog;
-import rtg.world.gen.feature.tree.WorldGenTreeRTGShrubCustom;
-import rtg.world.gen.feature.tree.abyssalcraft.WorldGenTreeACDarkwood;
+import rtg.world.biome.deco.DecoAbyssalCraftTree;
+import rtg.world.biome.deco.DecoAbyssalCraftTree.TreeType;
+import rtg.world.biome.deco.DecoBaseBiomeDecorations;
+import rtg.world.biome.deco.DecoFallenTree;
+import rtg.world.biome.deco.DecoFallenTree.LogCondition;
+import rtg.world.biome.deco.DecoGrass;
+import rtg.world.biome.deco.DecoShrubCustom;
+import rtg.world.biome.deco.DecoTree.TreeCondition;
 import rtg.world.gen.surface.abyssalcraft.SurfaceACDarklandsForest;
 import rtg.world.gen.terrain.abyssalcraft.TerrainACDarklandsForest;
 
 import com.shinoow.abyssalcraft.api.block.ACBlocks;
-
-import net.minecraft.init.Blocks;
-import net.minecraft.world.World;
-import net.minecraft.world.biome.BiomeGenBase;
-import net.minecraft.world.gen.feature.WorldGenerator;
 
 public class RealisticBiomeACDarklandsForest extends RealisticBiomeACBase
 {
@@ -30,70 +26,49 @@ public class RealisticBiomeACDarklandsForest extends RealisticBiomeACBase
             acBiome,
             BiomeGenBase.river,
             new TerrainACDarklandsForest(),
-            new SurfaceACDarklandsForest(config, acBiome.topBlock, acBiome.fillerBlock, false, null, 0f, 1.5f, 60f, 65f, 1.5f, acBiome.topBlock, (byte)0, 0.10f));
-    }
-    
-    @Override
-    public void rDecorate(World world, Random rand, int chunkX, int chunkY, OpenSimplexNoise simplex, CellNoise cell, float strength, float river)
-    {
+            new SurfaceACDarklandsForest(config, acBiome.topBlock, acBiome.fillerBlock, false, null, 0f, 1.5f, 60f, 65f, 1.5f, acBiome.topBlock, (byte)0, 0.10f)
+        );
         
-        /**
-         * Using rDecorateSeedBiome() to partially decorate the biome? If so, then comment out this method.
-         */
-        //rOreGenSeedBiome(world, rand, chunkX, chunkY, simplex, cell, strength, river, baseBiome);
-    
-        float l = simplex.noise2(chunkX / 80f, chunkY / 80f) * 60f - 15f;
+		DecoAbyssalCraftTree decoTrees = new DecoAbyssalCraftTree();
+		decoTrees.strengthNoiseFactorXForLoops = true;
+		decoTrees.distribution.noiseDivisor = 80f;
+		decoTrees.distribution.noiseFactor = 60f;
+		decoTrees.distribution.noiseAddend = -15f;
+		decoTrees.treeType = TreeType.DARKWOOD;
+		decoTrees.treeCondition = TreeCondition.RANDOM_CHANCE;
+		decoTrees.treeConditionChance = 3;
+		decoTrees.maxY = 110;
+		this.addDeco(decoTrees);
         
-        for (int b1 = 0; b1 < l * strength; b1++)
-        {
-
-            if (rand.nextInt(3) == 0) {
-                
-                int j6 = chunkX + rand.nextInt(16) + 8;
-                int k10 = chunkY + rand.nextInt(16) + 8;
-                int z52 = world.getHeightValue(j6, k10);
-                
-                WorldGenerator worldgenerator = new WorldGenTreeACDarkwood(6 + rand.nextInt(6), 10 + rand.nextInt(10), 0, 0);
-                worldgenerator.setScale(1.0D, 1.0D, 1.0D);
-                worldgenerator.generate(world, rand, j6, z52, k10);
-            }
-        }
+        DecoFallenTree decoFallenTree = new DecoFallenTree();
+        decoFallenTree.logCondition = LogCondition.NOISE_GREATER_AND_RANDOM_CHANCE;
+        decoFallenTree.logConditionNoise = 0f;
+        decoFallenTree.logConditionChance = 12;
+        decoFallenTree.maxY = 100;
+        decoFallenTree.logBlock = ACBlocks.darklands_oak_wood;
+        decoFallenTree.logMeta = (byte)0;
+        decoFallenTree.leavesBlock = ACBlocks.darklands_oak_leaves;
+        decoFallenTree.leavesMeta = (byte)-1;
+        decoFallenTree.minSize = 2;
+        decoFallenTree.maxSize = 3;
+		this.addDeco(decoFallenTree, this.config._boolean(BiomeConfigACDarklandsForest.decorationLogsId));
         
-        if (this.config.getPropertyById(BiomeConfigACDarklandsForest.decorationLogsId).valueBoolean) {
+        DecoShrubCustom decoShrubCustom = new DecoShrubCustom();
+        decoShrubCustom.logBlock = ACBlocks.darklands_oak_wood;
+        decoShrubCustom.logMeta = (byte)0;
+        decoShrubCustom.leavesBlock = ACBlocks.darklands_oak_leaves;
+        decoShrubCustom.leavesMeta = (byte)0;
+        decoShrubCustom.maxY = 110;
+        decoShrubCustom.notEqualsZerochance = 3;
+        decoShrubCustom.strengthFactor = 3f;
+		this.addDeco(decoShrubCustom);        
         
-            if (rand.nextInt((int) (10f / strength)) == 0)
-            {
-                int x22 = chunkX + rand.nextInt(16) + 8;
-                int z22 = chunkY + rand.nextInt(16) + 8;
-                int y22 = world.getHeightValue(x22, z22);
-                
-                if (y22 < 100)
-                {
-                    (new WorldGenLog(ACBlocks.darklands_oak_wood, 0, ACBlocks.darklands_oak_leaves, -1, 2 + rand.nextInt(2))).generate(world, rand, x22, y22, z22);
-                }
-            }
-        }
+		DecoGrass decoGrass = new DecoGrass();
+		decoGrass.maxY = 128;
+		decoGrass.strengthFactor = 8f;
+        this.addDeco(decoGrass);
         
-        for (int f24 = 0; f24 < 3f * strength; f24++)
-        {
-            int i1 = chunkX + rand.nextInt(16) + 8;
-            int j1 = chunkY + rand.nextInt(16) + 8;
-            int k1 = world.getHeightValue(i1, j1);
-            
-            if (k1 < 110 && rand.nextInt(3) != 0)
-            {
-                (new WorldGenTreeRTGShrubCustom(rand.nextInt(4) + 1, ACBlocks.darklands_oak_wood, (byte)0, ACBlocks.darklands_oak_leaves, (byte)0)).generate(world, rand, i1, k1, j1);
-            }
-        }
-        
-        for (int l14 = 0; l14 < 8f * strength; l14++)
-        {
-            int l19 = chunkX + rand.nextInt(16) + 8;
-            int k22 = rand.nextInt(128);
-            int j24 = chunkY + rand.nextInt(16) + 8;
-            (new WorldGenGrass(Blocks.tallgrass, 1)).generate(world, rand, l19, k22, j24);
-        }
-        
-        rDecorateSeedBiome(world, rand, chunkX, chunkY, simplex, cell, strength, river, baseBiome);
+		DecoBaseBiomeDecorations decoBaseBiomeDecorations = new DecoBaseBiomeDecorations();
+		this.addDeco(decoBaseBiomeDecorations);
     }
 }

f3684e7d44df6327b74e4ca5f2b5178636c5cb91
topisani <tobiaspisani@gmail.com>
2016-04-09 23:38:53 +0200

    Add HeightSelectorPart

---
+++ src/main/java/teamrtg/rtg/world/biome/surface/part/HeightSelectorPart.java
@@ -0,0 +1,35 @@
+package teamrtg.rtg.world.biome.surface.part;
+
+import teamrtg.rtg.api.biome.RealisticBiomeBase;
+import teamrtg.rtg.util.noise.RTGNoise;
+
+/**
+ * @author topisani
+ */
+public class HeightSelectorPart extends SurfacePartBase {
+    private final int min;
+    private final int max;
+    private RTGNoise minNoise = RTGNoise.EMPTY;
+    private RTGNoise maxNoise = RTGNoise.EMPTY;
+
+    public HeightSelectorPart(RealisticBiomeBase biome, int min, int max) {
+        super(biome);
+        this.min = min;
+        this.max = max;
+    }
+
+    public HeightSelectorPart setMinNoise(RTGNoise minNoise) {
+        this.minNoise = minNoise;
+        return this;
+    }
+
+    public HeightSelectorPart setMaxNoise(RTGNoise maxNoise) {
+        this.maxNoise = maxNoise;
+        return this;
+    }
+
+    @Override
+    public boolean applies(int x, int y, int z, int depth, float[] noise) {
+        return y >= (min + minNoise.getAt(x, y, z)) && y <= (max + maxNoise.getAt(x, y, z));
+    }
+}

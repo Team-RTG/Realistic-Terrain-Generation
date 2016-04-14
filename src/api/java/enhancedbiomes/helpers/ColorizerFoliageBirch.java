41a148cea9a07b3a98b78df59920ef485415ae30
srs-bsns <forfrdm@gmail.com>
2016-04-14 02:28:44 -0400

    Attempt to suppress Travis error

--- src/api/java/enhancedbiomes/helpers/ColorizerFoliageBirch.java
+++ src/api/java/enhancedbiomes/helpers/ColorizerFoliageBirch.java
@@ -6,11 +6,13 @@ public class ColorizerFoliageBirch extends ColorizerFoliage
 {
     public static int birchColour = 8431445;
 	
+/* FIXME: STFU, Travis!
     public ColorizerFoliageBirch()
     {
     	
     }
-    
+*/
+
 	/**
      * Gets the foliage color for birch type (metadata 2) trees
      */

1ac8c09921f96a53a04e4acd5ccc7c8179c8b09e
WhichOnesPink <whichonespink@hokecraft.co.uk>
2016-04-18 22:12:39 +0100

    Added minDuneHeight to replace hardcoded values.

--- src/main/java/rtg/world/gen/terrain/TerrainDunes.java
+++ src/main/java/rtg/world/gen/terrain/TerrainDunes.java
@@ -12,7 +12,7 @@ public class TerrainDunes extends TerrainBase
 
 	public float generateNoise(OpenSimplexNoise simplex, CellNoise cell, int x, int y, float border, float river)
 	{
-        float st = (simplex.noise2(x / 160f, y / 160f) + 0.38f) * (ConfigRTG.duneHeight + 23f);
+        float st = (simplex.noise2(x / 160f, y / 160f) + 0.38f) * (minDuneHeight + (float)ConfigRTG.duneHeight);
         st = st < 0.2f ? 0.2f : st;
 
         float h = simplex.noise2(x / 60f, y / 60f) * st * 2f;
--- src/main/java/rtg/world/gen/terrain/biomesoplenty/TerrainBOPArctic.java
+++ src/main/java/rtg/world/gen/terrain/biomesoplenty/TerrainBOPArctic.java
@@ -15,7 +15,7 @@ public class TerrainBOPArctic extends TerrainBase
 	@Override
 	public float generateNoise(OpenSimplexNoise simplex, CellNoise cell, int x, int y, float border, float river)
 	{
-		return terrainPolar(x, y, simplex, river, 140f, 23+ConfigRTG.duneHeight, 60, 70, base) +
+		return terrainPolar(x, y, simplex, river, 140f, (minDuneHeight + (float)ConfigRTG.duneHeight), 60, 70, base) +
                 groundNoise(base, base, 2f, simplex)*river;
 	}
 }

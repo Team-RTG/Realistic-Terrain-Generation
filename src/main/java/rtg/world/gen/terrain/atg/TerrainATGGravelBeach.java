1f0242a16629232971aa55a48ddd8313528317ad
WhichOnesPink <whichonespink@hokecraft.co.uk>
2016-04-20 20:12:54 +0100

    Re-terrained ATG biomes.

--- src/main/java/rtg/world/gen/terrain/atg/TerrainATGGravelBeach.java
+++ src/main/java/rtg/world/gen/terrain/atg/TerrainATGGravelBeach.java
@@ -1,18 +1,13 @@
 package rtg.world.gen.terrain.atg;
 
-import rtg.util.CellNoise;
-import rtg.util.OpenSimplexNoise;
-import rtg.world.gen.terrain.TerrainBase;
+import rtg.world.gen.terrain.vanilla.TerrainVanillaBeach;
 
-public class TerrainATGGravelBeach extends TerrainBase
+
+public class TerrainATGGravelBeach extends TerrainVanillaBeach
 {
-	public TerrainATGGravelBeach()
-	{
-	}
 
-	@Override
-	public float generateNoise(OpenSimplexNoise simplex, CellNoise cell, int x, int y, float border, float river)
-	{
-        return terrainBeach(x, y, simplex, river, 180f, 35f, 60f);
-	}
+    public TerrainATGGravelBeach()
+    {
+
+    }
 }
--- src/main/java/rtg/world/gen/terrain/atg/TerrainATGVolcano.java
+++ src/main/java/rtg/world/gen/terrain/atg/TerrainATGVolcano.java
@@ -6,13 +6,13 @@ import rtg.world.gen.terrain.TerrainBase;
 
 public class TerrainATGVolcano extends TerrainBase
 {
-    public TerrainATGVolcano()
-    {
-    }
+	public TerrainATGVolcano()
+	{
+	}
 
-    @Override
-    public float generateNoise(OpenSimplexNoise simplex, CellNoise cell, int x, int y, float border, float river)
-    {
-        return terrainVolcano(x, y, simplex, cell, border, 65f);
-    }
+	@Override
+	public float generateNoise(OpenSimplexNoise simplex, CellNoise cell, int x, int y, float border, float river)
+	{
+        return terrainVolcano(x, y, simplex, cell, border, 70f);
+	}
 }

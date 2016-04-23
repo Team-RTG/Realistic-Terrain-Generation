f621b71d909ce2e06cb34aa6f29539c621661da8
WhichOnesPink <whichonespink@hokecraft.co.uk>
2016-04-23 03:05:40 +0100

    Finishing touches to HL Tropics.
    Added more trees, decreased log size & frequency, put height cap on base
    biome decorations to prevent trees from spawning at high altitudes, and
    removed non-white flowers.

--- src/main/java/rtg/world/biome/realistic/highlands/RealisticBiomeHLTropics.java
+++ src/main/java/rtg/world/biome/realistic/highlands/RealisticBiomeHLTropics.java
@@ -40,6 +40,7 @@ public class RealisticBiomeHLTropics extends RealisticBiomeHLBase
         
         this.waterSurfaceLakeChance = 3;
         
+        // First, a thick layer of shrubs to make sure the canopy trees are spaced out enough.
         DecoHLTree highlandsShrubs = new DecoHLTree();
         highlandsShrubs.logMeta = 3;
         highlandsShrubs.leavesMeta = 0;
@@ -49,14 +50,18 @@ public class RealisticBiomeHLTropics extends RealisticBiomeHLBase
 		highlandsShrubs.maxY = 90;
 		this.addDeco(highlandsShrubs);
 		
+		/**
+		 *  Fill in the gaps with sparse (2f), thin-trunked canopy trees.
+		 *  If you want more trees, try decreasing the density of shrubs above instead of increasing the density of trees here.
+		 */
         DecoHLTree thinCanopyTrees = new DecoHLTree();
         thinCanopyTrees.thickTrunk = false;
         thinCanopyTrees.minSize = 13;
         thinCanopyTrees.maxSize = 24;
 		thinCanopyTrees.strengthFactorForLoops = 2f;
 		thinCanopyTrees.treeType = rtg.world.biome.deco.DecoHLTree.TreeType.CANOPY;
-		thinCanopyTrees.treeCondition = TreeCondition.RANDOM_CHANCE;
-		thinCanopyTrees.treeConditionChance = 3;
+		thinCanopyTrees.treeCondition = TreeCondition.ALWAYS_GENERATE;
+		//thinCanopyTrees.treeConditionChance = 1;
 		thinCanopyTrees.maxY = 120;
 		this.addDeco(thinCanopyTrees);
 		
@@ -71,6 +76,7 @@ public class RealisticBiomeHLTropics extends RealisticBiomeHLBase
 //		thickCanopyTrees.maxY = 120;
 //		this.addDeco(thickCanopyTrees);
 		
+		// Canopy wood logs.
 		DecoFallenTree decoFallenTree = new DecoFallenTree();
 		decoFallenTree.loops = 1;
 		decoFallenTree.distribution.noiseDivisor = 100f;
@@ -78,16 +84,17 @@ public class RealisticBiomeHLTropics extends RealisticBiomeHLBase
 		decoFallenTree.distribution.noiseAddend = 0.8f;
 		decoFallenTree.logCondition = LogCondition.NOISE_GREATER_AND_RANDOM_CHANCE;
 		decoFallenTree.logConditionNoise = 0f;
-		decoFallenTree.logConditionChance = 8;
+		decoFallenTree.logConditionChance = 10;
 		decoFallenTree.maxY = 90;
 		decoFallenTree.logBlock = Highlands.vanillaBlocksFlag ? Blocks.log : HighlandsBlocks.canopyWood;
 		decoFallenTree.logMeta = (byte)0;
 		decoFallenTree.leavesBlock = Highlands.vanillaBlocksFlag ? Blocks.leaves : HighlandsBlocks.canopyLeaves;
 		decoFallenTree.leavesMeta = (byte)-1;
 		decoFallenTree.minSize = 4;
-		decoFallenTree.maxSize = 9;
+		decoFallenTree.maxSize = 7;
 		this.addDeco(decoFallenTree, this.config._boolean(BiomeConfigHLTropics.decorationLogsId));
 
+		// Sprinkle a few boulders around. No mossy cobble needed as vines will provide enough green in various places.
 		DecoBoulder decoBoulder = new DecoBoulder();
 		decoBoulder.boulderBlock = Blocks.cobblestone;
 		decoBoulder.chance = 16;
@@ -95,23 +102,33 @@ public class RealisticBiomeHLTropics extends RealisticBiomeHLBase
 		decoBoulder.strengthFactor = 2f;
 		this.addDeco(decoBoulder);
 		
+		/**
+		 *  Now that we've covered most of the chunk, let the base biome try to spawn in some thick-trunked canopy trees
+		 *  and other native decorations.
+		 */
 		DecoBaseBiomeDecorations decoBaseBiomeDecorations = new DecoBaseBiomeDecorations();
 		decoBaseBiomeDecorations.equalsZeroChance = 4;
+		decoBaseBiomeDecorations.maxY = 210; // No trees at the top of mountains.
 		this.addDeco(decoBaseBiomeDecorations);
 		
+		/**
+		 *  Vines should come after all full blocks have been placed for greatest effect.
+		 *  Throw in some lilypads to add contrast to the ponds.
+		 */
 		DecoJungleLilypadVines decoJungleLilypadVines = new DecoJungleLilypadVines();
 		this.addDeco(decoJungleLilypadVines);
-		
 		DecoJungleGrassVines decoJungleGrassVines = new DecoJungleGrassVines();
 		this.addDeco(decoJungleLilypadVines);
 		
+		// White flowers to brighten up the place.
 		DecoFlowersRTG decoFlowersRTG = new DecoFlowersRTG();
-        decoFlowersRTG.flowers = new int[]{2, 3, 8};
-        decoFlowersRTG.chance = 4;
+        decoFlowersRTG.flowers = new int[]{3, 6, 8};
+        decoFlowersRTG.chance = 3;
         decoFlowersRTG.maxY = 120;
         decoFlowersRTG.strengthFactor = 2f;
         this.addDeco(decoFlowersRTG);
         
+        // Grass & fern filler.
 		DecoGrass decoGrass = new DecoGrass();
 		decoGrass.maxY = 128;
 		decoGrass.loops = 16;

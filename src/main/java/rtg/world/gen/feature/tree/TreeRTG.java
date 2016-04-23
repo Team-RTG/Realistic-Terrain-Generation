9b90e515fb1f9c617d55b777b7de97d7a23e2ed5
WhichOnesPink <whichonespink@hokecraft.co.uk>
2016-04-23 13:27:07 +0100

    Added TreeRTGPinaceaePinusPonderosa.
    The Pinaceae Pinus Ponderosa (formerly known as 'Big Pine') is the first
    tree of the new RTG Tree System.

---
+++ src/main/java/rtg/world/gen/feature/tree/TreeRTG.java
@@ -0,0 +1,81 @@
+package rtg.world.gen.feature.tree;
+
+import java.util.Random;
+
+import net.minecraft.block.Block;
+import net.minecraft.block.material.Material;
+import net.minecraft.world.World;
+import net.minecraft.world.gen.feature.WorldGenerator;
+
+public class TreeRTG extends WorldGenerator
+{
+
+	public Block logBlock;
+	public byte logMeta;
+	public Block leavesBlock;
+	public byte leavesMeta;
+	public int trunkSize;
+	public int crownSize;
+	
+	public TreeRTG()
+	{
+
+	}
+
+	@Override
+	public boolean generate(World world, Random rand, int x, int y, int z)
+	{
+		return false;
+	}
+	
+    public void buildTrunk(World world, Random rand, int x, int y, int z)
+    {
+
+    }
+    
+    public void buildBranch(World world, Random rand, int x, int y, int z, int dX, int dZ, int logLength, int leaveSize)
+    {
+
+    }
+	
+    public void buildLeaves(World world, int x, int y, int z)
+    {
+
+    }
+    
+	public TreeRTG setLogBlock(Block logBlock)
+	{
+		this.logBlock = logBlock;
+		return this;
+	}
+	
+	public TreeRTG setLogMeta(byte logMeta)
+	{
+		this.logMeta = logMeta;
+		return this;
+	}
+	
+	public TreeRTG setLeavesBlock(Block leavesBlock)
+	{
+		this.leavesBlock = leavesBlock;
+		return this;
+	}
+	
+	public TreeRTG setLeavesMeta(byte leavesMeta)
+	{
+		this.leavesMeta = leavesMeta;
+		return this;
+	}
+	
+	public TreeRTG setTrunkSize(int trunkSize)
+	{
+		this.trunkSize = trunkSize;
+		return this;
+	}
+	
+	public TreeRTG setCrownSize(int crownSize)
+	{
+		this.crownSize = crownSize;
+		return this;
+	}
+}
---
+++ src/main/java/rtg/world/gen/feature/tree/rtg/pinaceae/TreeRTGPinaceaePinus.java
@@ -0,0 +1,21 @@
+package rtg.world.gen.feature.tree.rtg.pinaceae;
+
+import java.util.Random;
+
+import net.minecraft.world.World;
+import rtg.world.gen.feature.tree.rtg.TreeRTGPinaceae;
+
+public class TreeRTGPinaceaePinus extends TreeRTGPinaceae
+{
+
+	public TreeRTGPinaceaePinus()
+	{
+		super();
+	}
+
+	@Override
+	public boolean generate(World world, Random rand, int x, int y, int z)
+	{
+		return false;
+	}
+}

931522b9f536d2dc898180ca35e388b7d2efbb45
WhichOnesPink <whichonespink@hokecraft.co.uk>
2016-04-27 19:47:45 +0100

    Gutted EB and Highlands bootstraps.

--- src/api/java/highlands/Highlands.java
+++ src/api/java/highlands/Highlands.java
@@ -1,18 +1,7 @@
 package highlands;
 
-import highlands.api.HighlandsBiomes;
-import highlands.api.HighlandsBlocks;
-
-import java.io.File;
-
-import net.minecraft.block.Block;
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.world.WorldType;
-import net.minecraft.world.biome.BiomeGenBase;
-import net.minecraft.world.biome.WorldChunkManager;
-import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.common.config.Configuration;
-import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.Mod.EventHandler;
 import cpw.mods.fml.common.Mod.Instance;
 import cpw.mods.fml.common.SidedProxy;
@@ -20,8 +9,8 @@ import cpw.mods.fml.common.event.FMLInitializationEvent;
 import cpw.mods.fml.common.event.FMLPostInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 
-@Mod(modid="Highlands", name="Highlands", version="2.2.0",
-		dependencies = "after:Forestry;after:MineFactoryReloaded;after:Thaumcraft;after:BuildCraft|Transport")
+//@Mod(modid="Highlands", name="Highlands", version="2.2.0",
+//		dependencies = "after:Forestry;after:MineFactoryReloaded;after:Thaumcraft;after:BuildCraft|Transport")
 public class Highlands {
 
 	public static String modid = "Highlands";
@@ -60,50 +49,50 @@ public class Highlands {
     
     @EventHandler
 	public void preInit(FMLPreInitializationEvent event) {
-		//new settings set-up
-		Configuration config = new Configuration(new File(event.getModConfigurationDirectory() + File.separator + "Highlands" + File.separator + "General.cfg"));
-		config.load();
-		Config.setUpConfig(config);
-		config.save();
-		
-		Initializer.constructBlocks();
-		Initializer.initRecipes();
-		for (Block block : HighlandsBlocks.logs)
-			Initializer.setFireProperties(block, 5, 5);
-		for (Block block : HighlandsBlocks.leaves)
-			Initializer.setFireProperties(block, 30, 60);
-		for (Block block : HighlandsBlocks.plants)
-			Initializer.setFireProperties(block, 60, 100);
-		for (Block block : HighlandsBlocks.planks)
-			Initializer.setFireProperties(block, 5, 20);
+//		//new settings set-up
+//		Configuration config = new Configuration(new File(event.getModConfigurationDirectory() + File.separator + "Highlands" + File.separator + "General.cfg"));
+//		config.load();
+//		Config.setUpConfig(config);
+//		config.save();
+//		
+//		Initializer.constructBlocks();
+//		Initializer.initRecipes();
+//		for (Block block : HighlandsBlocks.logs)
+//			Initializer.setFireProperties(block, 5, 5);
+//		for (Block block : HighlandsBlocks.leaves)
+//			Initializer.setFireProperties(block, 30, 60);
+//		for (Block block : HighlandsBlocks.plants)
+//			Initializer.setFireProperties(block, 60, 100);
+//		for (Block block : HighlandsBlocks.planks)
+//			Initializer.setFireProperties(block, 5, 20);
 	}
 	
 	@EventHandler
 	public void load(FMLInitializationEvent event) {
-		//construct all variables
-		HighlandsBiomes.initBiomeArrays();
-		Initializer.constructBiomes();
-		Initializer.constructSettings();
-		
-		//set up sub-biomes
-		Initializer.setUpAllSubBiomes();
-		
-		//register event manager
-		MinecraftForge.TERRAIN_GEN_BUS.register(new HighlandsEventManager());
-		MinecraftForge.EVENT_BUS.register(new HighlandsEventManager());
-		
-		// allow player spawning in biomes
-		for(BiomeGenBase i : HighlandsBiomes.biomesForDefault){
-			WorldChunkManager.allowedBiomes.add(i);
-		}
-		if(WorldChunkManager.allowedBiomes.size() == 0)WorldChunkManager.allowedBiomes.add(HighlandsBiomes.ocean2);
-		
-		//initiate all recipes and ore dictionary definitions
-		
-		
-		
-		
-		proxy.registerRenderers();
+//		//construct all variables
+//		HighlandsBiomes.initBiomeArrays();
+//		Initializer.constructBiomes();
+//		Initializer.constructSettings();
+//		
+//		//set up sub-biomes
+//		Initializer.setUpAllSubBiomes();
+//		
+//		//register event manager
+//		MinecraftForge.TERRAIN_GEN_BUS.register(new HighlandsEventManager());
+//		MinecraftForge.EVENT_BUS.register(new HighlandsEventManager());
+//		
+//		// allow player spawning in biomes
+//		for(BiomeGenBase i : HighlandsBiomes.biomesForDefault){
+//			WorldChunkManager.allowedBiomes.add(i);
+//		}
+//		if(WorldChunkManager.allowedBiomes.size() == 0)WorldChunkManager.allowedBiomes.add(HighlandsBiomes.ocean2);
+//		
+//		//initiate all recipes and ore dictionary definitions
+//		
+//		
+//		
+//		
+//		proxy.registerRenderers();
 	}
 	
 	@EventHandler

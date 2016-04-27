d1a30700c021f20a55cf6c3d74327ffa53aa6239
topisani <tobiaspisani@gmail.com>
2016-04-27 10:14:27 +0200

    Port WorldGenPond

--- src/main/java/teamrtg/rtg/world/gen/RealisticBiomeGenerator.java
+++ src/main/java/teamrtg/rtg/world/gen/RealisticBiomeGenerator.java
@@ -24,6 +24,7 @@ import teamrtg.rtg.util.noise.OpenSimplexNoise;
 import teamrtg.rtg.util.noise.SimplexOctave;
 import teamrtg.rtg.world.biome.surface.part.SurfacePart;
 import teamrtg.rtg.world.gen.deco.DecoBase;
+import teamrtg.rtg.world.gen.feature.WorldGenPond;
 
 import java.util.Random;
 
@@ -110,7 +111,7 @@ public class RealisticBiomeGenerator {
 
                 if (rand.nextInt(Mods.RTG.config.UNDERGROUND_LAVA_LAKE_CHANCE.get()) == 0 && (RandomUtil.getRandomInt(rand, 1, Mods.RTG.config.UNDERGROUND_LAVA_LAKE_CHANCE.get()) == 1)) {
 
-                    (new WorldGenLakes(Blocks.LAVA)).generate(worldObj, rand, new BlockPos(i2, l4, i8));
+                    (new WorldGenPond(Blocks.LAVA)).generate(worldObj, rand, i2, l4, i8);
                 }
             }
         }
@@ -129,7 +130,7 @@ public class RealisticBiomeGenerator {
 
                     if (l4 > 63) {
 
-                        (new WorldGenLakes(Blocks.LAVA)).generate(worldObj, rand, new BlockPos(i2, l4, i8));
+                        (new WorldGenPond(Blocks.LAVA)).generate(worldObj, rand, i2, l4, i8);
                     }
                 }
             }
@@ -330,13 +331,13 @@ public class RealisticBiomeGenerator {
                 depth = -1;
             } else if (b == Blocks.STONE) {
                 depth++;
-                if (Mods.RTG.config.ENABLE_RTG_BIOME_DECORATIONS.get() && this.biome.config.USE_RTG_SURFACES.get()) {
-                        this.biome.surface.paintWithSubparts(primer, i, y, j, depth, noise, river);
+                if (Mods.RTG.config.ENABLE_RTG_SURFACES.get() && this.biome.config.USE_RTG_SURFACES.get()) {
+                    this.biome.surface.paintWithSubparts(primer, i, y, j, depth, noise, river);
                 } else {
                     this.genericPart.paintWithSubparts(primer, i, y, j, depth, noise, river);
-                    }
                 }
             }
+        }
     }
 
     public void decorate(World world, Random rand, int chunkX, int chunkY, OpenSimplexNoise simplex, CellNoise cell, float strength, float river) {

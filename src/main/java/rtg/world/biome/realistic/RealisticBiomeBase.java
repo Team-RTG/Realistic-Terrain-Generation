4290627d495e1ab884f6173f0331606d14bb11e4
Zeno410 <zeno410@aol.com>
2016-05-03 20:54:34 -0700

    Uncommenting LakePressure

--- src/main/java/rtg/world/biome/realistic/RealisticBiomeBase.java
+++ src/main/java/rtg/world/biome/realistic/RealisticBiomeBase.java
@@ -31,6 +31,7 @@ import rtg.util.CellNoise;
 import rtg.util.OpenSimplexNoise;
 import rtg.util.PlaneLocation;
 import rtg.util.RandomUtil;
+import rtg.util.SimplexOctave;
 import rtg.world.biome.BiomeBase;
 import rtg.world.biome.RTGBiomeProvider;
 import rtg.world.biome.deco.DecoBase;
@@ -402,26 +403,26 @@ public class RealisticBiomeBase extends BiomeBase {
     }
 
     public float lakePressure(OpenSimplexNoise simplex, CellNoise simplexCell,int x, int y, float border) {
-    	return 1f;
-//        if (noLakes) return 1f;
-//        if (1>0) return 1f;
-//        SimplexOctave.Disk jitter = new SimplexOctave.Disk();
-//        simplex.riverJitter().evaluateNoise((float)x / 240.0, (float)y / 240.0, jitter);
-//        double pX = x + jitter.deltax() * largeBendSize;
-//        double pY = y + jitter.deltay() * largeBendSize;
-//        simplex.mountain().evaluateNoise((float)x / 80.0, (float)y / 80.0, jitter);
-//        pX += jitter.deltax() * mediumBendSize;
-//        pY += jitter.deltay() * mediumBendSize;
-//        simplex.octave(4).evaluateNoise((float)x / 30.0, (float)y / 30.0, jitter);
-//        pX += jitter.deltax() * smallBendSize;
-//        pY += jitter.deltay() * smallBendSize;
-//        //double results =simplexCell.river().noise(pX / lakeInterval, pY / lakeInterval,1.0);
-//        double [] lakeResults = simplexCell.river().eval((float)pX/ lakeInterval, (float)pY/ lakeInterval);
-//        float results = 1f-(float)((lakeResults[1]-lakeResults[0])/lakeResults[1]);
-//        if (results >1.01) throw new RuntimeException("" + lakeResults[0]+ " , "+lakeResults[1]);
-//        if (results<-.01) throw new RuntimeException("" + lakeResults[0]+ " , "+lakeResults[1]);
-//        //return simplexCell.river().noise((float)x/ lakeInterval, (float)y/ lakeInterval,1.0);
-//        return results;
+    	
+        if (noLakes) return 1f;
+        if (1>0) return 1f;
+        SimplexOctave.Disk jitter = new SimplexOctave.Disk();
+        simplex.riverJitter().evaluateNoise((float)x / 240.0, (float)y / 240.0, jitter);
+        double pX = x + jitter.deltax() * largeBendSize;
+        double pY = y + jitter.deltay() * largeBendSize;
+        simplex.mountain().evaluateNoise((float)x / 80.0, (float)y / 80.0, jitter);
+        pX += jitter.deltax() * mediumBendSize;
+        pY += jitter.deltay() * mediumBendSize;
+        simplex.octave(4).evaluateNoise((float)x / 30.0, (float)y / 30.0, jitter);
+        pX += jitter.deltax() * smallBendSize;
+        pY += jitter.deltay() * smallBendSize;
+        //double results =simplexCell.river().noise(pX / lakeInterval, pY / lakeInterval,1.0);
+        double [] lakeResults = simplexCell.river().eval((float)pX/ lakeInterval, (float)pY/ lakeInterval);
+        float results = 1f-(float)((lakeResults[1]-lakeResults[0])/lakeResults[1]);
+        if (results >1.01) throw new RuntimeException("" + lakeResults[0]+ " , "+lakeResults[1]);
+        if (results<-.01) throw new RuntimeException("" + lakeResults[0]+ " , "+lakeResults[1]);
+        //return simplexCell.river().noise((float)x/ lakeInterval, (float)y/ lakeInterval,1.0);
+        return results;
     }
 
     public float lakeFlattening(float pressure, float bottomLevel, float topLevel) {

2758e9a3f4c88a560f9abefa973da3d9ef748153
topisani <tobiaspisani@gmail.com>
2016-05-14 15:53:41 +0200

    fix merge issues

--- src/api/java/com/shinoow/abyssalcraft/api/event/package-info.java
+++
@@ -1,4 +0,0 @@
-@API(apiVersion=AbyssalCraftAPI.API_VERSION,owner="abyssalcraft",provides="AbyssalCraftAPI|Event")
-package com.shinoow.abyssalcraft.api.event;
-import net.minecraftforge.fml.common.API;
-import com.shinoow.abyssalcraft.api.AbyssalCraftAPI;
--- src/api/java/com/shinoow/abyssalcraft/api/package-info.java
+++
@@ -1,3 +0,0 @@
-@API(apiVersion=AbyssalCraftAPI.API_VERSION,owner="abyssalcraft",provides="AbyssalCraftAPI|Core")
-package com.shinoow.abyssalcraft.api;
-import net.minecraftforge.fml.common.API;

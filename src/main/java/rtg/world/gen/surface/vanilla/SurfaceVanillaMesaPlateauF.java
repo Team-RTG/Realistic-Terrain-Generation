082f9011e574302131f3a36249ca7b587f59cf2c
Zeno410 <zeno410@aol.com>
2016-05-28 00:41:33 -0700

    Fix sudden color changes on Mesa tops

--- src/main/java/rtg/world/gen/surface/vanilla/SurfaceVanillaMesaPlateauF.java
+++ src/main/java/rtg/world/gen/surface/vanilla/SurfaceVanillaMesaPlateauF.java
@@ -27,6 +27,7 @@ public class SurfaceVanillaMesaPlateauF extends SurfaceBase {
     {
         float c = CliffCalculator.calc(x, y, noise);
         boolean cliff = c > 1.3f;
+        float noiseHeight = noise[y*16+x];
 
         for (int k = 255; k > -1; k--) {
         	Block b = blocks[(y * 16 + x) * 256 + k];
@@ -34,15 +35,16 @@ public class SurfaceVanillaMesaPlateauF extends SurfaceBase {
                 depth = -1;
             } else if (b == Blocks.stone) {
                 depth++;
+                float effectiveHeight = noiseHeight-(float)depth;
 
                 if (depth > -1 && depth < 12) {
                     if (cliff) {
-	        			blocks[(y * 16 + x) * 256 + k] = CanyonColour.MESA.getBlockForHeight(i, k,j);
-	        		    metadata[(y * 16 + x) * 256 + k] = CanyonColour.MESA.getMetaForHeight(i, k,j);
+	        			blocks[(y * 16 + x) * 256 + k] = CanyonColour.MESA.getBlockForHeight(i, effectiveHeight,j);
+	        		    metadata[(y * 16 + x) * 256 + k] = CanyonColour.MESA.getMetaForHeight(i, effectiveHeight,j);
                     } else {
                         if (depth > 4) {
-    	        			blocks[(y * 16 + x) * 256 + k] = CanyonColour.MESA.getBlockForHeight(i, k,j);
-    	        		    metadata[(y * 16 + x) * 256 + k] = CanyonColour.MESA.getMetaForHeight(i, k,j);
+    	        			blocks[(y * 16 + x) * 256 + k] = CanyonColour.MESA.getBlockForHeight(i, effectiveHeight,j);
+    	        		    metadata[(y * 16 + x) * 256 + k] = CanyonColour.MESA.getMetaForHeight(i, effectiveHeight,j);
                         } else if (k < 62) {
     	        			blocks[(y * 16 + x) * 256 + k] = Blocks.dirt;
     	        		    metadata[(y * 16 + x) * 256 + k] = (byte)0;
@@ -74,8 +76,8 @@ public class SurfaceVanillaMesaPlateauF extends SurfaceBase {
                         }
                     }
                 } else if (k > 63) {
-        			blocks[(y * 16 + x) * 256 + k] = CanyonColour.MESA.getBlockForHeight(i, k,j);
-        		    metadata[(y * 16 + x) * 256 + k] = CanyonColour.MESA.getMetaForHeight(i, k,j);
+        			blocks[(y * 16 + x) * 256 + k] = CanyonColour.MESA.getBlockForHeight(i, effectiveHeight,j);
+        		    metadata[(y * 16 + x) * 256 + k] = CanyonColour.MESA.getMetaForHeight(i, effectiveHeight,j);
                 }
             }
         }

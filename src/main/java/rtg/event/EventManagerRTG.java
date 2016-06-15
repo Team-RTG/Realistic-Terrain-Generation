d6fd01913e6fcd4647ec88847c035ab6b88cb135
WhichOnesPink <whichonespink@hokecraft.co.uk>
2016-06-16 00:51:10 +0100

    Fixed 'disappearing sapling' bug when growing RTG trees from vanilla saplings.

--- src/main/java/rtg/event/EventManagerRTG.java
+++ src/main/java/rtg/event/EventManagerRTG.java
@@ -267,31 +267,31 @@ public class EventManagerRTG
 			return;
 		}
 		
-		World world = event.world;
 		Random rand = event.rand;
-		int x = event.x;
-		int y = event.y;
-		int z = event.z;
-		Block saplingBlock = world.getBlock(x, y, z);
-		byte saplingMeta = (byte) saplingBlock.getDamageValue(world, x, y, z);
-		
-		Logger.debug("Ground Sapling Block = %s", saplingBlock.getLocalizedName());
-		Logger.debug("Ground Sapling Meta = %d", saplingMeta);
 		
 		// Should we generate a vanilla tree instead?
 		if (rand.nextInt(ConfigRTG.rtgTreeChance) != 0) {
 			Logger.debug("Skipping RTG tree generation.");
 			return;
-		}
+		}		
 		
+		World world = event.world;
+		int x = event.x;
+		int y = event.y;
+		int z = event.z;
+		
+		Block saplingBlock = world.getBlock(x, y, z);
+		byte saplingMeta = (byte) saplingBlock.getDamageValue(world, x, y, z);
+
 		WorldChunkManagerRTG cmr = (WorldChunkManagerRTG) world.getWorldChunkManager();
 		BiomeGenBase bgg = cmr.getBiomeGenAt(x, z);
 		RealisticBiomeBase rb = RealisticBiomeBase.getBiome(bgg.biomeID);
-		
-		Logger.debug("Biome = %s", rb.baseBiome.biomeName);
-		
 		ArrayList<TreeRTG> biomeTrees = rb.rtgTrees;
 		
+		Logger.debug("Biome = %s", rb.baseBiome.biomeName);
+		Logger.debug("Ground Sapling Block = %s", saplingBlock.getLocalizedName());
+		Logger.debug("Ground Sapling Meta = %d", saplingMeta);
+
 		if (biomeTrees.size() > 0) {
 			
 			// First, let's get all of the trees in this biome that match the sapling on the ground.
@@ -317,17 +317,16 @@ public class EventManagerRTG
 				
 				Logger.debug("Tree = %s", tree.getClass().getName());
 
-				if (tree.minCrownSize > 0 && tree.maxCrownSize > tree.minCrownSize) {
-					tree.crownSize = RandomUtil.getRandomInt(rand, tree.minCrownSize, tree.maxCrownSize);
-				}
-				
+				// Set the trunk size if min/max values have been set.
 				if (tree.minTrunkSize > 0 && tree.maxTrunkSize > tree.minTrunkSize) {
 					tree.trunkSize = RandomUtil.getRandomInt(rand, tree.minTrunkSize, tree.maxTrunkSize);
 				}
 				
-				// We have to remove the sapling manually because some trees grow around it, leaving the original sapling.
-				world.setBlock(x, y, z, Blocks.air, (byte)0, 2);
-				
+				// Set the crown size if min/max values have been set.
+				if (tree.minCrownSize > 0 && tree.maxCrownSize > tree.minCrownSize) {
+					tree.crownSize = RandomUtil.getRandomInt(rand, tree.minCrownSize, tree.maxCrownSize);
+				}
+
 				/**
 				 * Set the generateFlag to what it needs to be for growing trees from saplings,
 				 * generate the tree, and then set it back to what it was before.
@@ -336,11 +335,19 @@ public class EventManagerRTG
 				 */
 				int oldFlag = tree.generateFlag;
 				tree.generateFlag = 3;
-				tree.generate(world, rand, x, y, z);
+				boolean generated = tree.generate(world, rand, x, y, z);
 				tree.generateFlag = oldFlag;
-				
-				// Prevent the original tree from generating.
-				event.setResult(Result.DENY);
+
+				if (generated) {
+					
+					// Prevent the original tree from generating.
+					event.setResult(Result.DENY);
+					
+					// Sometimes we have to remove the sapling manually because some trees grow around it, leaving the original sapling.
+					if (world.getBlock(x, y, z) == saplingBlock) {
+						world.setBlock(x, y, z, Blocks.air, (byte)0, 2);
+					}
+				}
 			}
 			else {
 				

5cc928d3e79d243866b8b3dc4cd4a2d13076ee4d
WhichOnesPink <whichonespink@hokecraft.co.uk>
2016-06-24 09:10:52 +0100

    DecoBoulder checks radius before generating in villages.

--- src/main/java/rtg/world/biome/deco/DecoBoulder.java
+++ src/main/java/rtg/world/biome/deco/DecoBoulder.java
@@ -8,6 +8,8 @@ import net.minecraft.world.World;
 import net.minecraft.world.gen.feature.WorldGenerator;
 import rtg.util.CellNoise;
 import rtg.util.OpenSimplexNoise;
+import rtg.util.WorldUtil;
+import rtg.util.WorldUtil.SurroundCheckType;
 import rtg.world.biome.realistic.RealisticBiomeBase;
 import rtg.world.gen.feature.WorldGenBlob;
 
@@ -51,6 +53,7 @@ public class DecoBoulder extends DecoBase
 	{
 		if (this.allowed) {
 			
+			WorldUtil worldUtil = new WorldUtil(world);
 			WorldGenerator worldGenerator = new WorldGenBlob(boulderBlock, this.boulderMeta, 0, rand, this.water);
 			
             for (int l1 = 0; l1 < this.strengthFactor * strength; ++l1)
@@ -60,6 +63,14 @@ public class DecoBoulder extends DecoBase
                 int k1 = world.getHeightValue(i1, j1);
                 
                 if (k1 >= this.minY && k1 <= this.maxY && rand.nextInt(this.chance) == 0) {
+                	
+	                // If we're in a village, check to make sure the boulder has extra room to grow to avoid corrupting the village.
+	                if (hasPlacedVillageBlocks) {
+		                if (!worldUtil.isSurroundedByBlock(Blocks.air, 2, SurroundCheckType.CARDINAL, rand, i1, k1, j1)) {
+		                	return;
+		                }
+	                }
+	                
                 	worldGenerator.generate(world, rand, i1, k1, j1);
                 }
             }

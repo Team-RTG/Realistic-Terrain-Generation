2b8d7d4d5b1a8ecc5d1e28402fc189a6a15d4b20
WhichOnesPink <whichonespink@hokecraft.co.uk>
2016-06-26 11:37:55 +0100

    Added biome support for Minestrappolation.
    Redwood Forest & The Frost.

---
+++ src/main/java/rtg/api/biome/minestrappolation/config/BiomeConfigMSBase.java
@@ -0,0 +1,12 @@
+package rtg.api.biome.minestrappolation.config;
+
+import rtg.api.biome.BiomeConfig;
+
+public class BiomeConfigMSBase extends BiomeConfig
+{
+
+    public BiomeConfigMSBase(String biomeSlug)
+    {
+        super("minestrappolation", biomeSlug);
+    }
+}

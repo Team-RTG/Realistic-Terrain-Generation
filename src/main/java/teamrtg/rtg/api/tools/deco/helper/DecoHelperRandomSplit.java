0a25f3dd6a70485aa691b297aed198dedd558cf8
WhichOnesPink <whichonespink@hokecraft.co.uk>
2016-06-30 00:43:56 +0100

    1.7.10ified deco helpers.

--- src/main/java/teamrtg/rtg/api/tools/deco/helper/DecoHelperRandomSplit.java
+++ src/main/java/teamrtg/rtg/api/tools/deco/helper/DecoHelperRandomSplit.java
@@ -8,35 +8,48 @@ import teamrtg.rtg.api.world.gen.RealisticBiomeGenerator;
 
 /**
  * This deco helper takes an array of deco objects and an array of chances and generates one accordingly.
+ * 
  * @author WhichOnesPink
+ *
  */
-public class DecoHelperRandomSplit extends DecoBase {
+public class DecoHelperRandomSplit extends DecoBase
+{
 
-    public DecoBase[] decos;
-    public int[] chances;
+	public DecoBase[] decos;
+	public int[] chances;
+	
+	public DecoHelperRandomSplit()
+	{
+		super();
+		
+		this.decos = new DecoBase[]{};
+		this.chances = new int[]{};
+	}
+	
+	@Override
+	public void generate(RTGWorld rtgWorld, Random rand, int chunkX, int chunkY, float strength, float river, RealisticBiomeGenerator realisticBiomeGenerator, boolean hasPlacedVillageBlocks)
+	{
+		if (this.allowed) {
+			
+			if (this.decos.length < 1 || this.chances.length < 1 || this.decos.length != this.chances.length) {
+				throw new RuntimeException("DecoHelperRandomSplit is confused.");
+			}
 
-    public DecoHelperRandomSplit() {
-        super();
+            int totalChances = 0;
+			for (int i = 0; i < this.decos.length; i++) {
+                totalChances += chances[i];
+			}
+			int chosen = rand.nextInt(totalChances);
 
-        this.decos = new DecoBase[] {};
-        this.chances = new int[] {};
-    }
-
-    @Override
-    public void generate(RTGWorld rtgWorld, Random rand, int chunkX, int chunkY, float strength, float river, RealisticBiomeGenerator realisticBiomeGenerator, boolean hasPlacedVillageBlocks) {
-        if (this.allowed) {
-
-            if (this.decos.length < 1 || this.chances.length < 1 || this.decos.length != this.chances.length) {
-                throw new RuntimeException("DecoHelperRandomSplit is confused.");
-            }
-
-            for (int i = 0; i < this.decos.length; i++) {
-
-                if (rand.nextInt(this.chances[i]) == 0) {
-
-                    this.decos[i].generate(rtgWorld, rand, chunkX, chunkY, strength, river, realisticBiomeGenerator, hasPlacedVillageBlocks);
-                }
-            }
-        }
-    }
+			for (int i = 0; i < this.decos.length; i++) {
+				
+				if (chosen < (this.chances[i])) {
+					
+					this.decos[i].generate(rtgWorld, rand, chunkX, chunkY, strength, river, realisticBiomeGenerator, hasPlacedVillageBlocks);
+				}
+                // decrement chosen for the chances missed and continue;
+                chosen -= chances[i];
+			}
+		}
+	}
 }

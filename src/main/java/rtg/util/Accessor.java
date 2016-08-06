a4c20c60c59046ce40d53461701f442d91ff1e4e
WhichOnesPink <whichonespink@hokecraft.co.uk>
2016-08-06 07:55:24 +0100

    Ported utils from 1.7.10

--- src/main/java/rtg/util/Accessor.java
+++ src/main/java/rtg/util/Accessor.java
@@ -2,63 +2,67 @@ package rtg.util;
 
 
 import java.lang.reflect.Field;
+import java.util.Arrays;
+
 /**
- *
  * @author Zeno410
+ *         Modified by topisani to allow for multiple possible field names.
+ *         This is useful for catching both the obfuscated and the deobfuscated field.
+ *         Updated and ported to 1.7.10 by srs_bsns
  */
-public class Accessor<ObjectType,FieldType>{
-        private Field field;
-        private final String fieldName;
-        public Accessor(String _fieldName) {
-            fieldName = _fieldName;
-        }
+public class Accessor<ObjectType, FieldType> {
+    private final String[] fieldNames;
+    private Field field;
 
-        private Field field(ObjectType example) {
-            Class classObject = example.getClass();
-            if (field == null) {
-                try {setField(classObject);}
-                catch(IllegalAccessException e) {
-                    throw new RuntimeException(e);
-                }
+    public Accessor(String... _fieldName) {
+        fieldNames = _fieldName;
+    }
+
+    @SuppressWarnings("unchecked")
+    public FieldType get(ObjectType object) {
+        try {
+            return (FieldType) (field(object).get(object));
+        } catch (IllegalArgumentException | IllegalAccessException ex) {
+            throw new RuntimeException(ex);
+        }
+    }
+
+    private Field field(ObjectType example) {
+        Class classObject = example.getClass();
+        if (field == null) {
+            try {
+                setField(classObject);
+            } catch (IllegalAccessException e) {
+                throw new RuntimeException(e);
             }
-            return field;
         }
+        return field;
+    }
 
-        private void setField(Class classObject) throws IllegalAccessException{
-            // hunts through the class object and all superclasses looking for the field name
-            Field [] fields;
-            do {
-                fields = classObject.getDeclaredFields();
-                for (int i = 0; i < fields.length;i ++) {
-                    if (fields[i].getName().contains(fieldName)) {
-                        field = fields[i];
+    private void setField(Class classObject) throws IllegalAccessException {
+        // hunts through the class object and all superclasses looking for the field name
+        Field[] fields;
+        do {
+            fields = classObject.getDeclaredFields();
+            for (Field field1 : fields) {
+                for (String fieldName : fieldNames) {
+                    if (field1.getName().contains(fieldName)) {
+                        field = field1;
                         field.setAccessible(true);
                         return;
                     }
                 }
-                classObject = classObject.getSuperclass();
-            } while (classObject != Object.class);
-            throw new RuntimeException(fieldName +" not found in class "+classObject.getName());
-        }
-
-        public FieldType get(ObjectType object) {
-            try {
-                 return (FieldType)(field(object).get(object));
-            } catch (IllegalArgumentException ex) {
-                throw new RuntimeException(ex);
-            } catch (IllegalAccessException ex) {
-                throw new RuntimeException(ex);
             }
-        }
-
-        public void setField(ObjectType object,FieldType fieldValue) {
-            try {
-                field(object).set(object, fieldValue);
-            } catch (IllegalArgumentException ex) {
-                throw new RuntimeException(ex);
-            } catch (IllegalAccessException ex) {
-                throw new RuntimeException(ex);
-            }
-        }
-
+            classObject = classObject.getSuperclass();
+        } while (classObject != Object.class);
+        throw new RuntimeException("None of " + Arrays.toString(fieldNames) + " found in class " + classObject.getName());
     }
+
+    public void setField(ObjectType object, FieldType fieldValue) {
+        try {
+            field(object).set(object, fieldValue);
+        } catch (IllegalArgumentException | IllegalAccessException ex) {
+            throw new RuntimeException(ex);
+        }
+    }
+}
---
+++ src/main/java/rtg/util/LimitedMap.java
@@ -0,0 +1,100 @@
+
+package rtg.util;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Set;
+
+/**
+ *
+ * @author Zeno410
+ */
+public class LimitedMap<Key,Value> implements Map<Key,Value> {
+
+    private int limit;
+    private int nextIndex = 0;
+    private boolean reachedLimit = false;
+    private final ArrayList<Key> keyList;
+    private final HashMap<Key,Value> map;
+
+    public LimitedMap(int maximumSize) {
+        limit = maximumSize;
+        keyList = new ArrayList<Key>(limit);
+        map = new HashMap<Key,Value>(limit);
+    }
+
+    public int size() {
+       if (reachedLimit) return limit;
+       return nextIndex;
+    }
+
+    public boolean isEmpty() {
+        return size() == 0;
+    }
+
+    public boolean containsKey(Object arg0) {
+        return keyList.contains(arg0);
+    }
+
+    public boolean containsValue(Object arg0) {
+        for (Value value: map.values()) {
+            if (value.equals(arg0)) return true;
+        };
+        return false;
+    }
+
+    public Value get(Object arg0) {
+        return map.get(arg0);
+    }
+
+    public synchronized Value put(Key arg0, Value arg1) {
+        if (reachedLimit) {
+            map.remove(keyList.get(nextIndex));
+            keyList.set(nextIndex++, arg0);
+            if (nextIndex >= limit) {
+                nextIndex = 0;
+            }
+        } else {
+            keyList.add(arg0);
+            nextIndex ++;
+            if (nextIndex >= limit) {
+                reachedLimit = true;
+                nextIndex = 0;
+            }
+        }
+        return map.put(arg0, arg1);
+    }
+
+    public Value remove(Object arg0) {
+        return null;// not going to bother
+    }
+
+    public void putAll(Map<? extends Key, ? extends Value> arg0) {
+        for (Key key: arg0.keySet()) {
+            put(key,arg0.get(key));
+        }
+    }
+
+    public void clear() {
+        nextIndex = 0;
+        reachedLimit = false;
+        keyList.clear();
+        map.clear();
+    }
+
+    public Set<Key> keySet() {
+        return map.keySet();
+    }
+
+    public Collection<Value> values() {
+        return map.values();
+    }
+
+    public Set<Entry<Key, Value>> entrySet() {
+        return map.entrySet();
+    }
+
+}

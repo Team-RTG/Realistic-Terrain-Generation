29cc2ff18a05e7f50e0eb1815e50570c8b3d52c6
WhichOnesPink <whichonespink@hokecraft.co.uk>
2016-08-10 13:54:20 +0100

    Gradle stuff.

--- src/main/java/rtg/reference/ModInfo.java
+++ src/main/java/rtg/reference/ModInfo.java
@@ -1,11 +1,16 @@
 package rtg.reference;
 
-public class ModInfo {
+import net.minecraftforge.common.ForgeVersion;
 
+public class ModInfo {
     public static final String MOD_ID = "@MOD_ID@";
     public static final String MOD_NAME = "@MOD_NAME@";
     public static final String MOD_VERSION = "@MOD_VERSION@";
-    public static final String FORGE_DEP = "@MCF_VERSION@";
+    public static final String FORGE_DEP = "" + ForgeVersion.majorVersion + '.'
+        + ForgeVersion.minorVersion + '.'
+        + ForgeVersion.revisionVersion + '.'
+        + ForgeVersion.buildVersion;
+    public static final String MOD_DEPS = "";
     public static final String PROXY_COMMON = "rtg.proxy.CommonProxy";
     public static final String PROXY_CLIENT = "rtg.proxy.ClientProxy";
 }

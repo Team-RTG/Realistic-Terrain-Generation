f4311cac8d7ab60257dda3e650e03a4892ec46cc
WhichOnesPink <whichonespink@hokecraft.co.uk>
2016-08-10 05:56:49 +0100

    Re-surfaced & re-decorated vanilla Mesa Plateau.

--- src/main/java/rtg/world/biome/realistic/vanilla/RealisticBiomeVanillaMesaPlateau.java
+++ src/main/java/rtg/world/biome/realistic/vanilla/RealisticBiomeVanillaMesaPlateau.java
@@ -1,12 +1,21 @@
 package rtg.world.biome.realistic.vanilla;
 
+import java.util.Random;
+
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.init.Blocks;
+import net.minecraft.world.World;
 import net.minecraft.world.biome.BiomeGenBase;
+import net.minecraft.world.chunk.ChunkPrimer;
 import net.minecraft.world.gen.feature.WorldGenTrees;
 
 import rtg.api.biome.BiomeConfig;
+import rtg.util.CellNoise;
+import rtg.util.OpenSimplexNoise;
 import rtg.world.biome.deco.*;
+import rtg.world.biome.deco.collection.DecoCollectionDesertRiver;
+import rtg.world.gen.surface.SurfaceBase;
+import rtg.world.gen.surface.SurfaceRiverOasis;
 import rtg.world.gen.surface.vanilla.SurfaceVanillaMesaPlateau;
 import rtg.world.gen.terrain.vanilla.TerrainVanillaMesaPlateau;
 
@@ -25,6 +34,8 @@ public class RealisticBiomeVanillaMesaPlateau extends RealisticBiomeVanillaBase 
         );
 
         this.noLakes = true;
+
+        this.addDecoCollection(new DecoCollectionDesertRiver());
 
         DecoShrub decoShrub = new DecoShrub();
         decoShrub.chance = 10;
@@ -57,5 +68,15 @@ public class RealisticBiomeVanillaMesaPlateau extends RealisticBiomeVanillaBase 
         decoTree.treeConditionNoise = 0f;
         decoTree.minY = 74;
         addDeco(decoTree);
+    }
+
+    @Override
+    public void rReplace(ChunkPrimer primer, int i, int j, int x, int y, int depth, World world, Random rand,
+                         OpenSimplexNoise simplex, CellNoise cell, float[] noise, float river, BiomeGenBase[] base) {
+
+        this.getSurface().paintTerrain(primer, i, j, x, y, depth, world, rand, simplex, cell, noise, river, base);
+
+        SurfaceBase riverSurface = new SurfaceRiverOasis(this.config);
+        riverSurface.paintTerrain(primer, i, j, x, y, depth, world, rand, simplex, cell, noise, river, base);
     }
 }

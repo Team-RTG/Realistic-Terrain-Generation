19b1815f27b5cf2ae328f52d9bf193eae8952324
WhichOnesPink <whichonespink@hokecraft.co.uk>
2016-08-20 04:59:15 +0100

    Removed unused files.

--- src/main/java/teamrtg/rtg/api/util/noise/CellNoise.java
+++
@@ -1,7 +0,0 @@
-package teamrtg.rtg.api.util.noise;
-
-public interface CellNoise {
-    public float noise(double x, double z, double depth);
-    public CellOctave octave(int index);
-    public CellOctave river();
-}

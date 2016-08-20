012d3f3b9d03620e5ea07d8d3f89ef3492a9010c
WhichOnesPink <whichonespink@hokecraft.co.uk>
2016-08-20 04:57:16 +0100

    Added BOP API to src/api.

---
+++ src/api/java/biomesoplenty/api/item/BOPMaterials.java
@@ -0,0 +1,25 @@
+/*******************************************************************************
+ * Copyright 2014-2016, the Biomes O' Plenty Team
+ * 
+ * This work is licensed under a Creative Commons Attribution-NonCommercial-NoDerivatives 4.0 International Public License.
+ * 
+ * To view a copy of this license, visit http://creativecommons.org/licenses/by-nc-nd/4.0/.
+ ******************************************************************************/
+
+package biomesoplenty.api.item;
+
+import net.minecraft.item.Item.ToolMaterial;
+import net.minecraft.item.ItemArmor.ArmorMaterial;
+
+public class BOPMaterials
+{
+    public static ArmorMaterial wading_boots_material;
+    public static ArmorMaterial flippers_material;
+    public static ArmorMaterial plain_flower_band_material;
+    public static ArmorMaterial lush_flower_band_material;
+    public static ArmorMaterial exotic_flower_band_material;
+    public static ArmorMaterial dull_flower_band_material;
+    
+    public static ArmorMaterial mud_armor_material;
+    public static ToolMaterial mud_tool_material;
+}

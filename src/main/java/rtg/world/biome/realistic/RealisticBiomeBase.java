a2b36e0c256fedf5f68dc11befe1eed854e8e351
WhichOnesPink <whichonespink@hokecraft.co.uk>
2016-09-30 15:50:06 +0100

    CHECK_DECAY = FALSE for all RTG tree leaves.

--- src/main/java/rtg/world/biome/realistic/RealisticBiomeBase.java
+++ src/main/java/rtg/world/biome/realistic/RealisticBiomeBase.java
@@ -3,6 +3,8 @@ package rtg.world.biome.realistic;
 import java.util.ArrayList;
 import java.util.Random;
 
+import net.minecraft.block.BlockLeaves;
+import net.minecraft.block.state.IBlockState;
 import net.minecraft.init.Biomes;
 import net.minecraft.util.math.ChunkPos;
 import net.minecraft.world.World;
@@ -452,6 +454,18 @@ public class RealisticBiomeBase {
 
             // Set the sapling data for this tree before we add it to the list.
             tree.saplingBlock = SaplingUtil.getSaplingFromLeaves(tree.leavesBlock);
+
+            /*
+             * Make sure all leaves delay their decay to prevent insta-despawning of leaves (e.g. Swamp Willow)
+             * The try/catch is a safeguard against trees that use leaves which aren't an instance of BlockLeaves.
+             */
+            try {
+                IBlockState leaves = tree.leavesBlock.withProperty(BlockLeaves.CHECK_DECAY, false);
+                tree.leavesBlock = leaves;
+            }
+            catch (Exception e) {
+                // Do nothing.
+            }
 
             this.rtgTrees.add(tree);
         }

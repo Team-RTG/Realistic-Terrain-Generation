d5bc9f4a529172d5ef5e8f9f42f303b768bffb6b
srs-bsns <forfrdm@gmail.com>
2016-10-04 01:23:47 -0400

    Some clean up
    - update build scripts
        * preparations for: proper dependencies, removing third-party source
        * updated mod_deps.properties
        * added gradle property for verbose warnings
    
    - fixed ModInfo.java
        * proper version ranging for Forge
        * readded mod dependencies
        * fixed version tokens for testing without using gradle runClient task
    
    - cleaned up RTG.java
        * updated container annotation
        * fixed ambiguity
        * moved some things for better clarity

--- src/main/java/rtg/proxy/CommonProxy.java
+++ src/main/java/rtg/proxy/CommonProxy.java
@@ -4,7 +4,9 @@ import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 
-public class CommonProxy {
+public class CommonProxy
+{
+    public static final String LOCATION = "rtg.proxy.CommonProxy";
 
     public void preInit(FMLPreInitializationEvent event) {
 

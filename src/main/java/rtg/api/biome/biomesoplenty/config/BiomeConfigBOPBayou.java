1118dee9894dfa6551d70dc7955f1524dca0c137
WhichOnesPink <whichonespink@hokecraft.co.uk>
2016-11-05 03:09:43 +0000

    Re-decorated BOP Bayou.

--- src/main/java/rtg/api/biome/biomesoplenty/config/BiomeConfigBOPBayou.java
+++ src/main/java/rtg/api/biome/biomesoplenty/config/BiomeConfigBOPBayou.java
@@ -1,10 +1,26 @@
 package rtg.api.biome.biomesoplenty.config;
 
 
+import rtg.api.biome.BiomeConfigProperty;
+
 public class BiomeConfigBOPBayou extends BiomeConfigBOPBase {
+
+    public static final String decorationLogsId = "decorationLogs";
+    public static final String decorationLogsName = "RTG Decoration: Logs";
+
+    public static final String surfaceMixBlockId = "surfaceMixBlock";
+    public static final String surfaceMixBlockName = "RTG Surface: Mix Block";
+
+    public static final String surfaceMixBlockMetaId = "surfaceMixBlockMeta";
+    public static final String surfaceMixBlockMetaName = "RTG Surface: Mix Block Meta";
 
     public BiomeConfigBOPBayou() {
 
         super("bayou");
+
+        this.addProperty(new BiomeConfigProperty(decorationLogsId, BiomeConfigProperty.Type.BOOLEAN, decorationLogsName, "", true));
+
+        this.addProperty(new BiomeConfigProperty(surfaceMixBlockId, BiomeConfigProperty.Type.STRING, surfaceMixBlockName, "", ""));
+        this.addProperty(new BiomeConfigProperty(surfaceMixBlockMetaId, BiomeConfigProperty.Type.STRING, surfaceMixBlockMetaName, "", ""));
     }
 }

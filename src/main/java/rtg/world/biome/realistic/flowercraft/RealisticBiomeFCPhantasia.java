0fc1c826bca8649d38334e449ea011393b46ef66
WhichOnesPink <whichonespink@hokecraft.co.uk>
2016-11-09 17:19:18 +0000

    initTerrain'd FC biomes.

--- src/main/java/rtg/world/biome/realistic/flowercraft/RealisticBiomeFCPhantasia.java
+++ src/main/java/rtg/world/biome/realistic/flowercraft/RealisticBiomeFCPhantasia.java
@@ -4,9 +4,12 @@ import net.minecraft.init.Biomes;
 import net.minecraft.world.biome.Biome;
 
 import rtg.api.biome.BiomeConfig;
+import rtg.util.CellNoise;
+import rtg.util.OpenSimplexNoise;
 import rtg.world.biome.deco.DecoBaseBiomeDecorations;
 import rtg.world.gen.surface.flowercraft.SurfaceFCPhantasia;
-import rtg.world.gen.terrain.flowercraft.TerrainFCPhantasia;
+import rtg.world.gen.terrain.GroundEffect;
+import rtg.world.gen.terrain.TerrainBase;
 
 public class RealisticBiomeFCPhantasia extends RealisticBiomeFCBase {
 
@@ -15,11 +18,32 @@ public class RealisticBiomeFCPhantasia extends RealisticBiomeFCBase {
     public RealisticBiomeFCPhantasia(Biome biome, BiomeConfig config) {
 
         super(config, biome, river,
-            new TerrainFCPhantasia(),
+            new rtg.world.gen.terrain.flowercraft.TerrainFCPhantasia(),
             new SurfaceFCPhantasia(config, biome.topBlock, biome.fillerBlock)
         );
 
         DecoBaseBiomeDecorations decoBaseBiomeDecorations = new DecoBaseBiomeDecorations();
         this.addDeco(decoBaseBiomeDecorations);
+    }
+
+    @Override
+    public TerrainBase initTerrain() {
+
+        return new TerrainFCPhantasia();
+    }
+
+    public class TerrainFCPhantasia extends TerrainBase {
+
+        private GroundEffect groundEffect = new GroundEffect(4f);
+
+        public TerrainFCPhantasia() {
+
+        }
+
+        @Override
+        public float generateNoise(OpenSimplexNoise simplex, CellNoise cell, int x, int y, float border, float river) {
+            //return terrainPlains(x, y, simplex, river, 160f, 10f, 60f, 200f, 66f);
+            return riverized(65f + groundEffect.added(simplex, cell, x, y), river);
+        }
     }
 }

505fcee351d6af2c999aad8da11bfe85fd521b38
WhichOnesPink <whichonespink@hokecraft.co.uk>
2016-11-28 20:01:00 +0000

    MF terrains now use RTGWorld.

--- src/main/java/rtg/world/biome/realistic/mithwoodforest/RealisticBiomeMFMithwoodForest.java
+++ src/main/java/rtg/world/biome/realistic/mithwoodforest/RealisticBiomeMFMithwoodForest.java
@@ -9,7 +9,6 @@ import net.minecraft.init.Blocks;
 import net.minecraft.world.biome.Biome;
 import net.minecraft.world.chunk.ChunkPrimer;
 
-import rtg.api.util.noise.CellNoise;
 import rtg.api.util.noise.OpenSimplexNoise;
 import rtg.api.world.RTGWorld;
 import rtg.config.BiomeConfig;
@@ -58,11 +57,11 @@ public class RealisticBiomeMFMithwoodForest extends RealisticBiomeMFBase {
         }
 
         @Override
-        public float generateNoise(OpenSimplexNoise simplex, CellNoise cell, int x, int y, float border, float river) {
+        public float generateNoise(RTGWorld rtgWorld, int x, int y, float border, float river) {
 
-            groundNoise = groundNoise(x, y, groundNoiseAmplitudeHills, simplex);
+            groundNoise = groundNoise(x, y, groundNoiseAmplitudeHills, rtgWorld.simplex);
 
-            float h = terrainPlains(x, y, simplex, river, 160f, 10f, 60f, 80f, 65f);
+            float h = terrainPlains(x, y, rtgWorld.simplex, river, 160f, 10f, 60f, 80f, 65f);
 
             return riverized(groundNoise + h, river);
         }

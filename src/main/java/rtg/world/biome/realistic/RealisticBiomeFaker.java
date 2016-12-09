2801202e55c413b2c0205aebee0ac8ea910df54b
WhichOnesPink <whichonespink@hokecraft.co.uk>
2016-12-09 02:22:41 +0000

    Added RealisticBiomeFaker.

---
+++ src/main/java/rtg/world/biome/realistic/RealisticBiomeFaker.java
@@ -0,0 +1,112 @@
+package rtg.world.biome.realistic;
+
+import java.lang.reflect.Field;
+
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.init.Blocks;
+import net.minecraft.world.biome.Biome;
+import net.minecraft.world.chunk.ChunkPrimer;
+import net.minecraft.world.gen.ChunkProviderOverworld;
+import net.minecraft.world.gen.NoiseGeneratorPerlin;
+
+import rtg.api.util.BiomeUtils;
+import rtg.api.util.LimitedMap;
+import rtg.api.util.PlaneLocation;
+import rtg.api.world.RTGWorld;
+import rtg.util.Logger;
+import static rtg.api.util.MathUtils.globalToChunk;
+import static rtg.api.util.MathUtils.globalToLocal;
+
+/**
+ * @author topisani
+ */
+public class RealisticBiomeFaker {
+
+    public static boolean[] fakeBiomes = new boolean[256];
+    private final ChunkProviderOverworld fakeProvider;
+    private LimitedMap<PlaneLocation.Invariant, int[]> chunkHeights = new LimitedMap<>(64); //Keep the heights for the last 64 chunks around for a bit. We might need them
+    private RTGWorld rtgWorld;
+    private NoiseGeneratorPerlin surfaceNoise;
+
+    public RealisticBiomeFaker(RTGWorld rtgWorld) {
+        this.rtgWorld = rtgWorld;
+        fakeProvider = new ChunkProviderOverworld(rtgWorld.world, rtgWorld.world.getSeed(), rtgWorld.world.getWorldInfo().isMapFeaturesEnabled(), rtgWorld.world.getWorldInfo().getGeneratorOptions());
+
+        Field field;
+        try {
+            // Obf name
+            field = fakeProvider.getClass().getDeclaredField("field_185994_m");
+            field.setAccessible(true);
+            this.surfaceNoise = (NoiseGeneratorPerlin) field.get(fakeProvider);
+        } catch (Exception e) {
+            // Either we are in a dev environment or something is very wrong
+            try {
+                // Deobf name
+                field = fakeProvider.getClass().getDeclaredField("surfaceNoise");
+                field.setAccessible(true);
+                this.surfaceNoise = (NoiseGeneratorPerlin) field.get(fakeProvider);
+            } catch (Exception e2) {
+                Logger.fatal(
+                    "Failed to access private field 'surfaceNoise' in ChunkProviderOverworld. Are you in a deobfuscated environment with other mappings?",
+                    e2
+                );
+            }
+        }
+    }
+
+    public static void initFakeBiomes() {
+        Biome[] b = BiomeUtils.getRegisteredBiomes();
+        for (Biome biome : b) {
+            if (biome != null) {
+                try {
+                    RealisticBiomeBase.getBiome(Biome.getIdForBiome(biome)).baseBiome.getBiomeName();
+                } catch (Exception e) {
+                    //new RealisticBiomeGenerator(new FakedRTGBiome(Mods.RTG, biome));
+                    fakeBiomes[Biome.getIdForBiome(biome)] = true;
+                }
+            }
+        }
+    }
+
+    public boolean isFakeBiome(int id) {
+        return fakeBiomes[id];
+    }
+
+    public int getHeightAt(int x, int z) {
+        return this.getHeightsAt(globalToChunk(x), globalToChunk(z))[globalToLocal(x) * 16 + globalToLocal(z)];
+    }
+
+    public int[] getHeightsAt(int cx, int cz) {
+        PlaneLocation.Invariant inLoc = new PlaneLocation.Invariant(cx, cz);
+        int[] heights = null;
+        for (PlaneLocation.Invariant location : chunkHeights.keySet()) {
+            if (location.equals(inLoc)) heights = chunkHeights.get(location);
+        }
+        if (heights == null) heights = this.fakeTerrain(cx, cz);
+        return heights;
+    }
+
+    public int[] fakeTerrain(int cx, int cz) {
+        ChunkPrimer primer = new ChunkPrimer();
+        fakeProvider.setBlocksInChunk(cx, cz, primer);
+        int[] heights = new int[256];
+        for (int x = 0; x < 16; x++) {
+            for (int z = 0; z < 16; z++) {
+                for (int y = 255; y >= 0; --y) {
+                    IBlockState iblockstate = primer.getBlockState(x, y, z);
+                    if (iblockstate != null && iblockstate != Blocks.AIR.getDefaultState()) {
+                        heights[x * 16 + z] = y;
+                        break;
+                    }
+                }
+            }
+        }
+        chunkHeights.put(new PlaneLocation.Invariant(cx, cz), heights);
+        return heights;
+    }
+
+    public void fakeSurface(int bx, int bz, ChunkPrimer primer, Biome biome) {
+        //For some really messed up reason these need to be flipped... see BGB#300
+        biome.genTerrainBlocks(rtgWorld.world, rtgWorld.rand, primer, bz, bx, this.surfaceNoise.getValue(bx, bz));
+    }
+}

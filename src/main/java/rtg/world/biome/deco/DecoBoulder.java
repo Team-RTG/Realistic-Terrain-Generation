e4708c04121b71a0cc7a1aa3388f955169249a4c
WhichOnesPink <whichonespink@hokecraft.co.uk>
2017-01-23 16:45:36 +0000

    Added height options & valid ground blocks to DecoBoulder.

--- src/main/java/rtg/world/biome/deco/DecoBoulder.java
+++ src/main/java/rtg/world/biome/deco/DecoBoulder.java
@@ -1,12 +1,16 @@
 package rtg.world.biome.deco;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Random;
 
+import net.minecraft.block.Block;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.init.Blocks;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.gen.feature.WorldGenerator;
 
+import rtg.api.util.RandomUtil;
 import rtg.api.util.WorldUtil;
 import rtg.api.world.RTGWorld;
 import rtg.world.biome.realistic.RealisticBiomeBase;
@@ -21,8 +25,10 @@ public class DecoBoulder extends DecoBase {
     private float strengthFactor; // Higher = more/bigger boulders.
     private int minY; // Lower height restriction.
     private int maxY; // Upper height restriction.
+    private HeightType heightType; // How we determine the Y coord.
     private int chance; // Higher = more rare.
     private boolean water;
+    protected ArrayList<Block> validGroundBlocks;
 
     public DecoBoulder() {
 
@@ -36,8 +42,18 @@ public class DecoBoulder extends DecoBase {
         this.setStrengthFactor(2f);
         this.setMinY(60); // Sensible lower height limit by default.
         this.setMaxY(255); // No upper height limit by default.
+        this.setHeightType(HeightType.GET_HEIGHT_VALUE);
         this.setChance(10);
         this.water = true;
+
+        this.validGroundBlocks = new ArrayList<Block>(Arrays.asList(
+            Blocks.GRASS,
+            Blocks.DIRT,
+            Blocks.STONE,
+            Blocks.GRAVEL,
+            Blocks.CLAY,
+            Blocks.SAND
+        ));
 
         this.addDecoTypes(DecoType.BOULDER);
     }
@@ -48,12 +64,29 @@ public class DecoBoulder extends DecoBase {
         if (this.allowed) {
 
             WorldUtil worldUtil = new WorldUtil(rtgWorld.world);
-            WorldGenerator worldGenerator = new WorldGenBlob(boulderBlock, 0, rand, this.water);
+            WorldGenerator worldGenerator = new WorldGenBlob(boulderBlock, 0, rand, this.water, validGroundBlocks);
 
             for (int l1 = 0; l1 < this.strengthFactor * strength; ++l1) {
+
                 int i1 = worldX + rand.nextInt(16);// + 8;
                 int j1 = worldZ + rand.nextInt(16);// + 8;
-                int k1 = rtgWorld.world.getHeight(new BlockPos(i1, 0, j1)).getY();
+                int k1;
+
+                switch (this.heightType) {
+
+                    case NEXT_INT:
+                        k1 = RandomUtil.getRandomInt(rand, this.minY, this.maxY);
+                        break;
+
+                    case GET_HEIGHT_VALUE:
+                        k1 = rtgWorld.world.getHeight(new BlockPos(i1, 0, j1)).getY();
+                        break;
+
+                    default:
+                        k1 = rtgWorld.world.getHeight(new BlockPos(i1, 0, j1)).getY();
+                        break;
+
+                }
 
                 if (k1 >= this.minY && k1 <= this.maxY && rand.nextInt(this.chance) == 0) {
 
@@ -68,6 +101,11 @@ public class DecoBoulder extends DecoBase {
                 }
             }
         }
+    }
+
+    public enum HeightType {
+        NEXT_INT,
+        GET_HEIGHT_VALUE;
     }
 
     public IBlockState getBoulderBlock() {
@@ -133,6 +171,28 @@ public class DecoBoulder extends DecoBase {
     public DecoBoulder setWater(boolean water) {
 
         this.water = water;
+        return this;
+    }
+
+    public HeightType getHeightType() {
+
+        return heightType;
+    }
+
+    public DecoBoulder setHeightType(HeightType heightType) {
+
+        this.heightType = heightType;
+        return this;
+    }
+
+    public ArrayList<Block> getValidGroundBlocks() {
+
+        return validGroundBlocks;
+    }
+
+    public DecoBoulder setValidGroundBlocks(ArrayList<Block> validGroundBlocks) {
+
+        this.validGroundBlocks = validGroundBlocks;
         return this;
     }
 }

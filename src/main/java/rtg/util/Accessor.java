3fe128d036e70b0c18e803382ce9fb500a432460
WhichOnesPink <whichonespink@hokecraft.co.uk>
2017-01-31 22:57:13 +0000

    Backported Accessor from 1.11.2

--- src/main/java/rtg/util/Accessor.java
+++ src/main/java/rtg/util/Accessor.java
@@ -8,31 +8,38 @@ import java.util.Arrays;
  * @author Zeno410
  *         Modified by topisani to allow for multiple possible field names.
  *         This is useful for catching both the obfuscated and the deobfuscated field.
- *         Updated and ported to 1.7.10 by srs_bsns
  */
 public class Accessor<ObjectType, FieldType> {
+
     private final String[] fieldNames;
     private Field field;
 
     public Accessor(String... _fieldName) {
+
         fieldNames = _fieldName;
     }
 
-    @SuppressWarnings("unchecked")
     public FieldType get(ObjectType object) {
+
         try {
-            return (FieldType) (field(object).get(object));
-        } catch (IllegalArgumentException | IllegalAccessException ex) {
+            return (FieldType)(field(object).get(object));
+        }
+        catch (IllegalArgumentException ex) {
+            throw new RuntimeException(ex);
+        }
+        catch (IllegalAccessException ex) {
             throw new RuntimeException(ex);
         }
     }
 
     private Field field(ObjectType example) {
+
         Class classObject = example.getClass();
         if (field == null) {
             try {
                 setField(classObject);
-            } catch (IllegalAccessException e) {
+            }
+            catch (IllegalAccessException e) {
                 throw new RuntimeException(e);
             }
         }
@@ -44,25 +51,32 @@ public class Accessor<ObjectType, FieldType> {
         Field[] fields;
         do {
             fields = classObject.getDeclaredFields();
-            for (Field field1 : fields) {
+            for (int i = 0; i < fields.length; i++) {
                 for (String fieldName : fieldNames) {
-                    if (field1.getName().contains(fieldName)) {
-                        field = field1;
+                    if (fields[i].getName().contains(fieldName)) {
+                        field = fields[i];
                         field.setAccessible(true);
                         return;
                     }
                 }
             }
             classObject = classObject.getSuperclass();
-        } while (classObject != Object.class);
+        }
+        while (classObject != Object.class);
         throw new RuntimeException("None of " + Arrays.toString(fieldNames) + " found in class " + classObject.getName());
     }
 
     public void setField(ObjectType object, FieldType fieldValue) {
+
         try {
             field(object).set(object, fieldValue);
-        } catch (IllegalArgumentException | IllegalAccessException ex) {
+        }
+        catch (IllegalArgumentException ex) {
+            throw new RuntimeException(ex);
+        }
+        catch (IllegalAccessException ex) {
             throw new RuntimeException(ex);
         }
     }
+
 }

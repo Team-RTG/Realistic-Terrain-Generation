bc81bdb20047eda14114d4041fd827fb298f357b
WhichOnesPink <whichonespink@hokecraft.co.uk>
2017-01-31 21:27:26 +0000

    Backported Logger from 1.10.2

--- src/main/java/rtg/util/Logger.java
+++ src/main/java/rtg/util/Logger.java
@@ -1,17 +1,18 @@
 package rtg.util;
 
-import net.minecraft.crash.CrashReport;
-import net.minecraft.util.ReportedException;
 import net.minecraftforge.fml.common.FMLLog;
+
 import org.apache.logging.log4j.Level;
+
 import rtg.config.rtg.ConfigRTG;
+
 
 public class Logger {
 
     public static void debug(String format, Object... data) {
 
         if (ConfigRTG.enableDebugging) {
-            FMLLog.log(Level.INFO, "[RTG-DEBUG] " + format, data);
+            FMLLog.log(Level.WARN, "[RTG-DEBUG] " + format, data);
         }
     }
 
@@ -30,10 +31,8 @@ public class Logger {
         FMLLog.log(Level.ERROR, "[RTG-ERROR] " + format, data);
     }
 
-    public static void fatal(String message, Throwable throwable, Object... data) {
+    public static void fatal(String format, Object... data) {
 
-        FMLLog.log(Level.FATAL, "[RTG-FATAL] " + message, data);
-        CrashReport crashreport = CrashReport.makeCrashReport(throwable, message);
-        throw new ReportedException(crashreport);
+        FMLLog.log(Level.FATAL, "[RTG-FATAL] " + format, data);
     }
 }

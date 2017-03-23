9515541eaa12b8c8b851438fb126706e521b8c56
Zeno410 <zeno410@aol.com>
2017-03-23 11:38:39 -0700

    Plateau utility

---
+++ src/main/java/rtg/world/gen/terrain/PlateauStep.java
@@ -0,0 +1,19 @@
+
+package rtg.world.gen.terrain;
+
+/**
+ *
+ * @author Zeno410
+ */
+public class PlateauStep {
+    public float height = 32;
+    public float start = 0.3f;
+    public float finish = 0.5f;
+    
+    public float increase(float simplexVal) {
+        if (simplexVal <= start) return 0;
+        if (simplexVal >= finish) return height;
+        return (simplexVal - start)/(finish - start) * height;
+    }
+
+}

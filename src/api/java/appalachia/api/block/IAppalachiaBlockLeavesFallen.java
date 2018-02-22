85f5f8c7d3862d6393c7aa161f9494cf12f461c8
srs-bsns <forfrdm@gmail.com>
2018-02-22 08:03:49 -0500

    Fix dependencies
    Removed unneeded API source from our source tree and add AbyssalCraft
    and Biome O' Plenty APIs properly
    
    Signed-off-by: srs-bsns <forfrdm@gmail.com>

--- src/api/java/appalachia/api/block/IAppalachiaBlockLeavesFallen.java
+++
@@ -1,9 +0,0 @@
-package appalachia.api.block;
-
-
-import net.minecraft.block.properties.PropertyInteger;
-
-public interface IAppalachiaBlockLeavesFallen {
-
-    public static final PropertyInteger LAYERS = PropertyInteger.create("layers", 1, 8);
-}

329ce03a9ad39f94734be4e5438604cd9190f7bc
WhichOnesPink <whichonespink@hokecraft.co.uk>
2018-08-12 21:09:34 +0100

    Re-added biome support for Abyssalcraft.

---
+++ src/main/java/rtg/world/biome/realistic/abyssalcraft/SurfaceACBase.java
@@ -0,0 +1,28 @@
+package rtg.world.biome.realistic.abyssalcraft;
+
+import com.shinoow.abyssalcraft.api.block.ACBlocks;
+import net.minecraft.block.state.IBlockState;
+import rtg.api.config.BiomeConfig;
+import rtg.api.world.RTGWorld;
+import rtg.api.world.surface.SurfaceBase;
+
+
+public abstract class SurfaceACBase extends SurfaceBase {
+
+    public SurfaceACBase(BiomeConfig config, IBlockState top, IBlockState fill) {
+
+        super(config, top, fill);
+    }
+
+    @Override
+    protected IBlockState hcStone(RTGWorld rtgWorld, int i, int j, int x, int y, int k) {
+
+        return ACBlocks.stone.getDefaultState();
+    }
+
+    @Override
+    protected IBlockState hcCobble(RTGWorld rtgWorld, int worldX, int worldZ, int chunkX, int chunkZ, int worldY) {
+
+        return ACBlocks.cobblestone.getDefaultState();
+    }
+}

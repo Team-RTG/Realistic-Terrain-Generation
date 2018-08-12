20b72dd157c1480a712abd9a3850a89b2106731c
srs-bsns <forfrdm@gmail.com>
2018-08-12 08:28:03 -0400

    Fix allowed DimensionTypes
    This should now work in all OVERWORLD `DimensionType`s.
    This should work in all surface based JED `DimensionType`s
    This should work in any DimensionType that has the suffix '_rtg'
    
    Closes #1128
    
    Signed-off-by: srs-bsns <forfrdm@gmail.com>

--- src/main/java/rtg/world/WorldTypeRTG.java
+++ src/main/java/rtg/world/WorldTypeRTG.java
@@ -3,7 +3,9 @@ package rtg.world;
 import net.minecraft.world.World;
 import net.minecraft.world.WorldType;
 import net.minecraft.world.biome.BiomeProvider;
+import net.minecraft.world.gen.ChunkGeneratorOverworld;
 import net.minecraft.world.gen.IChunkGenerator;
+import net.minecraftforge.common.DimensionManager;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 import rtg.api.RTGAPI;
@@ -13,6 +15,7 @@ import rtg.world.biome.BiomeProviderRTG;
 import rtg.world.gen.ChunkGeneratorRTG;
 
 
+@SuppressWarnings("EqualsAndHashcode")
 public final class WorldTypeRTG extends WorldType {
 
     private static WorldTypeRTG INSTANCE;
@@ -35,24 +38,49 @@ public final class WorldTypeRTG extends WorldType {
     @Override
     public BiomeProvider getBiomeProvider(World world) {
 
-        if (RTGAPI.isAllowedDimensionType(world.provider.getDimension())) {
-            Logger.debug("WorldTypeRTG#getBiomeProvider() returning BiomeProviderRTG");
+        // TODO: It may be appropriate to add a config setting for a DimensionType whitelist in the future.
+        if (RTGAPI.isAllowedDimensionType(world.provider.getDimension()) ||
+            DimensionManager.getProviderType(world.provider.getDimension()).getSuffix().equals("_rtg") ||
+            DimensionManager.getProviderType(world.provider.getDimension()).name().startsWith("jed_surface")) {
+
+            Logger.debug("WorldType#getBiomeProvider: Allowed DimensionType detected (ID:{}, Type:{}, Suffix:{}).. returning BiomeProviderRTG",
+                world.provider.getDimension(),
+                world.provider.getDimensionType(),
+                DimensionManager.getProviderType(world.provider.getDimension()).getSuffix()
+            );
             return new BiomeProviderRTG(RTGWorld.getInstance(world));
         }
         else {
-            throw new RuntimeException(String.format("Illegal DimensionType (%s) for RTG WorldType", world.provider.getDimensionType().getName()));
+            Logger.debug("WorldType#getBiomeProvider: DimensionType not in whitelist (ID:{}, Type:{}, Suffix:{}).. returning BiomeProvider",
+                world.provider.getDimension(),
+                world.provider.getDimensionType(),
+                DimensionManager.getProviderType(world.provider.getDimension()).getSuffix()
+            );
+            return new BiomeProvider(world.getWorldInfo());
         }
     }
 
     @Override
     public IChunkGenerator getChunkGenerator(World world, String generatorOptions) {
 
-        if (RTGAPI.isAllowedDimensionType(world.provider.getDimension())) {
-            Logger.debug("WorldTypeRTG#getChunkGenerator() returning ChunkGeneratorRTG for Dim {}", world.provider.getDimension());
+        if (RTGAPI.isAllowedDimensionType(world.provider.getDimension()) ||
+            DimensionManager.getProviderType(world.provider.getDimension()).getSuffix().equals("_rtg") ||
+            DimensionManager.getProviderType(world.provider.getDimension()).name().startsWith("jed_surface")) {
+
+            Logger.debug("WorldType#getChunkGenerator: Allowed DimensionType detected (ID:{}, Type:{}, Suffix:{}).. returning ChunkGeneratorRTG",
+                world.provider.getDimension(),
+                world.provider.getDimensionType(),
+                DimensionManager.getProviderType(world.provider.getDimension()).getSuffix()
+            );
             return new ChunkGeneratorRTG(RTGWorld.getInstance(world));
         }
         else {
-            throw new RuntimeException(String.format("Illegal DimensionType (%s) for RTG WorldType", world.provider.getDimensionType().getName()));
+            Logger.debug("WorldType#getChunkGenerator: DimensionType not in whitelist (ID:{}, Type:{}, Suffix:{}).. returning ChunkGeneratorOverworld",
+                world.provider.getDimension(),
+                world.provider.getDimensionType(),
+                DimensionManager.getProviderType(world.provider.getDimension()).getSuffix()
+            );
+            return new ChunkGeneratorOverworld(world, world.getWorldInfo().getSeed(), world.getWorldInfo().isMapFeaturesEnabled(), world.getWorldInfo().getGeneratorOptions());
         }
     }
 

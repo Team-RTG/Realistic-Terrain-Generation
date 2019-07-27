70f0e09c243dc8a22ba9cfc3344a98b07c5dca98
WhichOnesPink <whichonespink@hokecraft.co.uk>
2019-07-27 15:23:22 +0100

    Shrubs now don't generate on sand by default.

--- src/main/java/rtg/api/world/deco/DecoShrub.java
+++ src/main/java/rtg/api/world/deco/DecoShrub.java
@@ -1,20 +1,18 @@
 package rtg.api.world.deco;
 
-import java.util.Random;
-
 import net.minecraft.block.BlockPlanks.EnumType;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.init.Blocks;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.ChunkPos;
-
 import net.minecraftforge.event.terraingen.DecorateBiomeEvent.Decorate;
 import net.minecraftforge.event.terraingen.TerrainGen;
-
 import rtg.api.util.BlockUtil;
 import rtg.api.world.RTGWorld;
 import rtg.api.world.biome.IRealisticBiome;
 import rtg.api.world.gen.feature.WorldGenShrubRTG;
+
+import java.util.Random;
 
 
 /**
@@ -49,7 +47,7 @@ public class DecoShrub extends DecoBase {
          */
         this.size = -1;
         this.useDefaultRandom = false;
-        this.sand = true; //Whether shrubs generate on sand
+        this.sand = false; //Whether shrubs generate on sand
         this.randomLogBlocks = new IBlockState[]{Blocks.LOG.getDefaultState(), BlockUtil.getStateLog(EnumType.SPRUCE)};
         this.randomLeavesBlocks = new IBlockState[]{Blocks.LEAVES.getDefaultState(), BlockUtil.getStateLeaf(EnumType.SPRUCE)};
         this.setStrengthFactor(3f); // Not sure why it was done like this, but... the higher the value, the more there will be.

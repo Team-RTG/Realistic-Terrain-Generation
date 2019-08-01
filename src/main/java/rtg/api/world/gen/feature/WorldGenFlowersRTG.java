aac21884e529492fa1d97d352b28a85c20702888
WhichOnesPink <whichonespink@hokecraft.co.uk>
2019-08-01 23:43:17 +0100

    Flowers no longer require sky access to generate.

--- src/main/java/rtg/api/world/gen/feature/WorldGenFlowersRTG.java
+++ src/main/java/rtg/api/world/gen/feature/WorldGenFlowersRTG.java
@@ -1,18 +1,18 @@
 package rtg.api.world.gen.feature;
 
-import java.util.Collection;
-import java.util.Collections;
-import java.util.Random;
-
 import com.google.common.collect.Iterables;
+import net.minecraft.block.BlockFlower;
 import net.minecraft.block.BlockFlower.EnumFlowerType;
 import net.minecraft.block.state.IBlockState;
-import net.minecraft.init.Blocks;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import net.minecraft.world.gen.feature.WorldGenerator;
 import rtg.api.util.BlockUtil;
 import rtg.api.util.Logger;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Random;
 
 
 public class WorldGenFlowersRTG extends WorldGenerator {
@@ -32,12 +32,9 @@ public class WorldGenFlowersRTG extends WorldGenerator {
         }
 
         IBlockState flower = BlockUtil.getStateFlower(Iterables.get(this.flowers, new Random().nextInt(this.flowers.size())));
+        BlockFlower block = (BlockFlower) flower.getBlock();
 
-        if (world.isAirBlock(pos) &&
-            (world.provider.hasSkyLight() || pos.getY() < 255) &&
-            Blocks.RED_FLOWER.canPlaceBlockAt(world, pos) &&
-            Blocks.RED_FLOWER.canBlockStay(world, pos, flower)) {
-
+        if (world.isAirBlock(pos) && pos.getY() < 255 && block.canPlaceBlockAt(world, pos) && block.canBlockStay(world, pos, flower)) {
             world.setBlockState(pos, flower, 2);
             return true;
         }

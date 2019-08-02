d20f3846221574eb92cd0ced236bc596c8bb9c17
WhichOnesPink <whichonespink@hokecraft.co.uk>
2019-08-02 12:30:31 +0100

    Fixed bug with Jungle Cactus generating on top of leaves and not generating on red sand.

--- src/main/java/rtg/api/world/gen/feature/WorldGenJungleCacti.java
+++ src/main/java/rtg/api/world/gen/feature/WorldGenJungleCacti.java
@@ -1,12 +1,14 @@
 package rtg.api.world.gen.feature;
 
-import java.util.Random;
-
+import net.minecraft.block.BlockSand;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.init.Blocks;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import net.minecraft.world.gen.feature.WorldGenerator;
+import rtg.api.util.BlockUtil;
+
+import java.util.Random;
 
 
 public class WorldGenJungleCacti extends WorldGenerator {
@@ -21,26 +23,34 @@ public class WorldGenJungleCacti extends WorldGenerator {
     }
 
     @Override
-    public boolean generate(World world, Random rand, BlockPos blockPos) {
+    public boolean generate(World world, Random rand, BlockPos pos) {
 
-        for (int tries = 0; tries < 10; ++tries) {
+        int x = pos.getX();
+        int y = pos.getY();
+        int z = pos.getZ();
+        IBlockState b;
+        for (int l = 0; l < 10; ++l) {
+            int i1 = x + rand.nextInt(8) - rand.nextInt(8);
+            int j1 = y + rand.nextInt(4) - rand.nextInt(4);
+            int k1 = z + rand.nextInt(8) - rand.nextInt(8);
 
-            BlockPos pos = blockPos.add(rand.nextInt(8) - rand.nextInt(8), rand.nextInt(4) - rand.nextInt(4), rand.nextInt(8) - rand.nextInt(8));
-            if (world.isAirBlock(pos)) {
-                IBlockState ground = world.getBlockState(blockPos.down());
-                if (ground == Blocks.SAND.getDefaultState() || (!sandOnly && (ground == Blocks.GRASS.getDefaultState() || ground == Blocks.DIRT.getDefaultState()))) {
-                    if (ground == Blocks.GRASS.getDefaultState() || ground == Blocks.DIRT.getDefaultState()) {
-                        world.setBlockState(pos.down(), Blocks.SAND.getDefaultState(), 2);
+            if (world.isAirBlock(new BlockPos(i1, j1, k1))) {
+                b = world.getBlockState(new BlockPos(i1, j1 - 1, k1));
+                if (b == Blocks.SAND.getDefaultState() || (!sandOnly && (b == Blocks.GRASS.getDefaultState() || b == Blocks.DIRT.getDefaultState()))) {
+                    int l1 = 1 + rand.nextInt(rand.nextInt(3) + 1);
+                    if (b == Blocks.GRASS.getDefaultState() || b == Blocks.DIRT.getDefaultState()) {
+                        world.setBlockState(new BlockPos(i1, j1 - 1, k1), BlockUtil.getStateSand(BlockSand.EnumType.RED_SAND), 2);
                     }
 
-                    for (int i = 0; i < (1 + rand.nextInt(rand.nextInt(3) + 1)) + extraHeight; ++i) {
-                        if (Blocks.CACTUS.canBlockStay(world, pos.up(i))) {
-                            world.setBlockState(pos.up(i), Blocks.CACTUS.getDefaultState(), 2);
+                    for (int i2 = 0; i2 < l1 + extraHeight; ++i2) {
+                        if (Blocks.CACTUS.canBlockStay(world, new BlockPos(i1, j1 + i2, k1))) {
+                            world.setBlockState(new BlockPos(i1, j1 + i2, k1), Blocks.CACTUS.getDefaultState(), 2);
                         }
                     }
                 }
             }
         }
+
         return true;
     }
 }

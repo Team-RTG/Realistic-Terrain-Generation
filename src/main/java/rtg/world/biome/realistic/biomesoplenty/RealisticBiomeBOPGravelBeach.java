7e3466b57799263834778be54fe6f6395dc0c7b7
WhichOnesPink <whichonespink@hokecraft.co.uk>
2019-08-11 00:42:51 +0100

    Re-designed BOP Gravel Beach to more closely resemble vanilla's Stone Beach.

--- src/main/java/rtg/world/biome/realistic/biomesoplenty/RealisticBiomeBOPGravelBeach.java
+++ src/main/java/rtg/world/biome/realistic/biomesoplenty/RealisticBiomeBOPGravelBeach.java
@@ -10,8 +10,6 @@ import net.minecraft.init.Blocks;
 import net.minecraft.world.biome.Biome;
 import net.minecraft.world.chunk.ChunkPrimer;
 import rtg.api.config.BiomeConfig;
-import rtg.api.util.WorldUtil.Terrain;
-import rtg.api.util.noise.SimplexNoise;
 import rtg.api.world.RTGWorld;
 import rtg.api.world.surface.SurfaceBase;
 import rtg.api.world.terrain.TerrainBase;
@@ -34,7 +32,9 @@ public class RealisticBiomeBOPGravelBeach extends RealisticBiomeBOPBase {
 
     @Override
     public void initConfig() {
-
+        this.getConfig().SURFACE_BLEED_IN.set(true);
+        this.getConfig().SURFACE_BLEED_OUT.set(true);
+        this.getConfig().addProperty(this.getConfig().SURFACE_MIX_FILLER_BLOCK).set("");
     }
 
     @Override
@@ -46,7 +46,7 @@ public class RealisticBiomeBOPGravelBeach extends RealisticBiomeBOPBase {
     @Override
     public SurfaceBase initSurface() {
 
-        return new SurfaceBOPGravelBeach(getConfig(), biome.topBlock, biome.fillerBlock, biome.topBlock, biome.fillerBlock, 1);
+        return new SurfaceBOPGravelBeach(getConfig(), biome.topBlock, biome.fillerBlock);
     }
 
     public static class TerrainBOPGravelBeach extends TerrainBase {
@@ -58,78 +58,43 @@ public class RealisticBiomeBOPGravelBeach extends RealisticBiomeBOPBase {
         @Override
         public float generateNoise(RTGWorld rtgWorld, int x, int y, float border, float river) {
 
-            return terrainBeach(x, y, rtgWorld, river, 60f);
+            return terrainBeach(x, y, rtgWorld, river, 63f);
         }
     }
 
     public static class SurfaceBOPGravelBeach extends SurfaceBase {
 
-        private IBlockState cliffBlock1;
-        private IBlockState cliffBlock2;
-        private int cliffType;
+        private IBlockState mixBlockFill;
 
-        public SurfaceBOPGravelBeach(BiomeConfig config, IBlockState top, IBlockState filler, IBlockState cliff1, IBlockState cliff2, int cliff) {
+        public SurfaceBOPGravelBeach(BiomeConfig config, IBlockState top, IBlockState filler) {
 
             super(config, top, filler);
 
-            cliffBlock1 = cliff1;
-            cliffBlock2 = cliff2;
-            cliffType = cliff;
+            mixBlockFill = this.getConfigBlock(config.SURFACE_MIX_FILLER_BLOCK.get(), Blocks.GRAVEL.getDefaultState());
         }
 
         @Override
         public void paintTerrain(ChunkPrimer primer, int i, int j, int x, int z, int depth, RTGWorld rtgWorld, float[] noise, float river, Biome[] base) {
 
             Random rand = rtgWorld.rand();
-            SimplexNoise simplex = rtgWorld.simplexInstance(0);
-            float c = Terrain.calcCliff(x, z, noise);
-            boolean cliff = c > 1.3f;
-            boolean dirt = false;
 
             for (int k = 255; k > -1; k--) {
                 Block b = primer.getBlockState(x, k, z).getBlock();
+
                 if (b == Blocks.AIR) {
                     depth = -1;
                 }
                 else if (b == Blocks.STONE) {
                     depth++;
 
-                    if (cliff) {
-                        if (cliffType == 1) {
-                            if (depth < 6) {
-                                primer.setBlockState(x, k, z, cliffBlock1.getBlock().getDefaultState());
-                            }
-                        }
-                        else {
-                            if (depth > -1 && depth < 2) {
-                                primer.setBlockState(x, k, z, rand.nextInt(3) == 0 ? cliffBlock2 : cliffBlock1);
-                            }
-                            else if (depth < 10) {
-                                primer.setBlockState(x, k, z, cliffBlock1);
-                            }
-                        }
+                    if (depth == 0 && k > 61) {
+                        primer.setBlockState(x, k, z, topBlock);
                     }
-                    else if (depth < 6) {
-                        if (depth == 0 && k > 61) {
-                            if (simplex.noise2f(i / 12f, j / 12f) > -0.3f + ((k - 61f) / 15f)) {
-                                dirt = true;
-                                primer.setBlockState(x, k, z, topBlock);
-                            }
-                            else {
-                                primer.setBlockState(x, k, z, Blocks.SAND.getDefaultState());
-                            }
-                        }
-                        else if (depth < 4) {
-                            if (dirt) {
-                                primer.setBlockState(x, k, z, fillerBlock);
-                            }
-                            else {
-                                primer.setBlockState(x, k, z, Blocks.SAND.getDefaultState());
-                            }
-                        }
-                        else if (!dirt) {
-                            primer.setBlockState(x, k, z, Blocks.SANDSTONE.getDefaultState());
-                        }
+                    else if (k > 63 && depth > 3 && depth < 6) {
+                        primer.setBlockState(x, k, z, mixBlockFill);
+                    }
+                    else if (depth < 4) {
+                        primer.setBlockState(x, k, z, fillerBlock);
                     }
                 }
             }
